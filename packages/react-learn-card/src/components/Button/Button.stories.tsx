deb4919f8188a6831728365a3e3d41e33b0ebbdc
Taylor Beeston <beeston.taylor@gmail.com>
2022-05-03 12:02:31 -0700

    :recycle: Change Button story

--- packages/react-learn-card/src/components/Button/Button.stories.tsx
+++ packages/react-learn-card/src/components/Button/Button.stories.tsx
@@ -4,15 +4,17 @@ import { Story, Meta } from '@storybook/react';
 import Button, { ButtonProps } from './Button';
 
 export default {
-  title: 'Button',
-  component: Button,
-  argTypes: {},
+    title: 'Button',
+    component: Button,
+    argTypes: {},
 } as Meta<typeof Button>;
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+const Template: Story<ButtonProps> = args => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  onClick: () => { console.log('Nice') },
-  text: 'Primary',
+export const ButtonTest = Template.bind({});
+ButtonTest.args = {
+    onClick: () => {
+        console.log('Nice');
+    },
+    text: 'Test!',
 };
