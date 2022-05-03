3eb650430e4d72b43d17ff7967b99ba23ee66265
goblincore <withallmyhrt@gmail.com>
2022-05-03 14:41:54 -0400

    Add jest and first test

---
+++ packages/react-learn-card/src/components/Button/Button.test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '@testing-library/jest-dom'
+import {render, screen } from '@testing-library/react'
+
+import Button from "./Button";
+
+describe("Running Test for Mr. Button", () => {
+
+
+  test("Check Button Render", () => {
+    render(<Button text="Button" />)
+    expect(screen.getByRole('button',{name:"Button"})).toBeInTheDocument();
+    expect(screen.queryByRole('button',{name:"Not In The Document"})).not.toBeInTheDocument();
+  });
+
+});
