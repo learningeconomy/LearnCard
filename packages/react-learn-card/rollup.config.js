5c804b64af21e3ecc488bff053db7cc328264660
Taylor Beeston <beeston.taylor@gmail.com>
2022-05-03 13:39:09 -0700

    :bug: Fix build

--- packages/react-learn-card/rollup.config.js
+++ packages/react-learn-card/rollup.config.js
@@ -1,39 +1,45 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
+import resolve from '@rollup/plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default [
     {
-        input: "src/index.ts",
+        input: 'src/index.ts',
         output: [
             {
                 file: packageJson.main,
-                format: "cjs",
+                format: 'cjs',
                 sourcemap: true,
             },
             {
                 file: packageJson.module,
-                format: "esm",
+                format: 'esm',
                 sourcemap: true,
             },
         ],
         plugins: [
             peerDepsExternal(),
+            postcss({
+                minimize: true,
+                modules: true,
+                extract: true,
+            }),
             resolve(),
             commonjs(),
-            typescript({ tsconfig: "./tsconfig.json" }),
+            typescript({ tsconfig: './tsconfig.json' }),
             terser(),
         ],
-        external: ["react", "react-dom", "styled-components"]
+        external: ['react', 'react-dom', 'styled-components'],
     },
-    {
+    /* {
         input: "types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
-    },
+    }, */
 ];
