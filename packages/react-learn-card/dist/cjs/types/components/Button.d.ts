5e2b2dc7d1cefdcf2545c1e514adf5ecd599057e
Taylor Beeston <beeston.taylor@gmail.com>
2022-05-03 13:32:35 -0700

    :fire: Remove dist folder

--- packages/react-learn-card/dist/cjs/types/components/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import React, { MouseEventHandler } from 'react';
-declare type ButtonProps = {
-    text?: string;
-    onClick: MouseEventHandler<HTMLButtonElement>;
-};
-declare const Button: React.FC<ButtonProps>;
-export default Button;
--- packages/react-learn-card/dist/esm/index.js
+++
@@ -1,2 +0,0 @@
-import t from"react";const e=({text:e,onClick:o})=>t.createElement("button",{type:"button",onClick:o},e);export{e as default};
-//# sourceMappingURL=index.js.map
