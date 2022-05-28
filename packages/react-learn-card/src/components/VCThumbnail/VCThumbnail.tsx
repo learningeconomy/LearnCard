eb55a6e3d88d038b0b36ee1bcf86c4c8b97caf50
Gerardo Paredes <gerardparedes23@gmail.com>
2022-05-28 13:24:03 -0700

    use color pallet colors

--- packages/react-learn-card/src/components/VCThumbnail/VCThumbnail.tsx
+++ packages/react-learn-card/src/components/VCThumbnail/VCThumbnail.tsx
@@ -25,7 +25,7 @@ export const VCThumbnail: React.FC<VCThumbnailProps> = ({
     return (
         <div
             onClick={onClick}
-            className={`flex flex-col items-center justify-between relative w-60 h-60 py-6 px-3 shadow-2xl rounded-3xl vc-thumbail-container ${className}`}
+            className={`flex flex-col items-center justify-between relative w-60 h-60 py-6 px-3 rounded-3xl shadow-3xl bg-emerald-700 vc-thumbnail-container ${className}`}
         >
             <div className="flex flex-col items-center justify-center z-10 text-center w-full">
                 <h2 className="text-lg font-normal tracking-wide leading-snug text-center">
@@ -63,7 +63,7 @@ export const VCThumbnail: React.FC<VCThumbnailProps> = ({
             <div className="flex items-center justify-center w-full vc-thumbnail-footer">
                 <h3 className='text-white text-sm text-right font-normal tracking-wide leading-tight flex-1'>Verified</h3>
                 <div className="flex items-center justify-center my-0 mx-1 h-10 w-10 rounded-full bg-white overflow-hidden">
-                    <div className="flex items-center justify-center h-5/6 w-10/12 bg-white rounded-full overflow-hidden vc-thumbnail-verification-badge-wrap">
+                    <div className="flex items-center justify-center h-5/6 w-10/12 bg-white rounded-full border-solid border-4 border-emerald-700 overflow-hidden">
                         <img className='h-full w-full object-cover' src={VerifiedArrow ?? ''} alt="credential icon" />
                     </div>
                 </div>
