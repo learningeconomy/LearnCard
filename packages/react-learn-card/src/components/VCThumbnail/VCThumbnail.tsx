22e3d7441b7d39ab97dc629ca8659a3860de5f76
Gerardo Paredes <gerardparedes23@gmail.com>
2022-05-31 14:17:32 -0700

    update markup to match design

--- packages/react-learn-card/src/components/VCThumbnail/VCThumbnail.tsx
+++ packages/react-learn-card/src/components/VCThumbnail/VCThumbnail.tsx
@@ -25,16 +25,38 @@ export const VCThumbnail: React.FC<VCThumbnailProps> = ({
     return (
         <div
             onClick={onClick}
-            className={`flex flex-col items-center justify-between relative w-60 h-60 py-6 px-3 rounded-3xl shadow-3xl bg-emerald-700 vc-thumbnail-container ${className}`}
+            className={`flex flex-col items-center justify-between relative w-60 h-60 py-3 px-3 rounded-3xl shadow-3xl bg-emerald-700 vc-thumbnail-container ${className}`}
         >
             <div className="flex flex-col items-center justify-center z-10 text-center w-full">
-                <h2
-                    className="text-lg font-normal tracking-wide leading-snug text-center"
-                    data-testid="vc-thumbnail-title"
-                >
-                    {title ?? ''}
-                </h2>
-                <p className="text-base font-light text-center" data-testid="vc-thumbnail-createdAt">{createdAt ?? ''}</p>
+                <div className="flex flex-row items-start justify-between w-full">
+                    <div className="flex flex-row items-start justify-start w-4/5">
+                        <h2
+                            className="text-base tracking-wide leading-snug text-left"
+                            data-testid="vc-thumbnail-title"
+                        >
+                            {title ?? ''}
+                        </h2>
+                    </div>
+                    <div className="flex flex-row items-start justify-end w-1/5">
+                        <div className="flex items-center justify-end my-0 h-8 w-8 rounded-full bg-white overflow-hidden">
+                            <div className="flex items-center justify-center h-5/6 w-10/12 bg-white rounded-full border-solid border-2 border-emerald-700 overflow-hidden">
+                                <img
+                                    className="h-full w-full object-cover"
+                                    src={VerifiedCheck ?? ''}
+                                    alt="credential icon"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full mt-2">
+                    <p
+                        className="text-sm font-light text-center"
+                        data-testid="vc-thumbnail-createdAt"
+                    >
+                        {createdAt ?? ''}
+                    </p>
+                </div>
 
                 <div className="flex flex-row items-center justify-center mt-2 w-full">
                     <div className="flex items-center justify-center h-14 w-14 shadow-3xl rounded-full overflow-hidden bg-white">
@@ -59,22 +81,10 @@ export const VCThumbnail: React.FC<VCThumbnailProps> = ({
                 </div>
             </div>
 
-            <div className="flex items-center justify-center w-full vc-thumbnail-footer">
-                <h3 className="text-white text-sm text-right font-normal tracking-wide leading-tight flex-1">
-                    Verified
-                </h3>
-                <div className="flex items-center justify-center my-0 mx-1 h-10 w-10 rounded-full bg-white overflow-hidden">
-                    <div className="flex items-center justify-center h-5/6 w-10/12 bg-white rounded-full border-solid border-4 border-emerald-700 overflow-hidden">
-                        <img
-                            className="h-full w-full object-cover"
-                            src={VerifiedCheck ?? ''}
-                            alt="credential icon"
-                        />
-                    </div>
+            <div className="flex items-center justify-center w-full">
+                <div className="h-16 w-16 border-solid border-2 border-black bg-white">
+                    <img className="h-full w-full object-cover" src={issuerImage} alt="user image" />
                 </div>
-                <h3 className="text-white text-sm text-left font-normal tracking-wide leading-tight flex-1">
-                    Credential
-                </h3>
             </div>
         </div>
     );
