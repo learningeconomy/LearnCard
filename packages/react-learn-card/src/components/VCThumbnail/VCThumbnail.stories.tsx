ea64abad86152469522d085b4b25e5b23681139e
Gerardo Paredes <gerardparedes23@gmail.com>
2022-05-31 14:55:46 -0700

    update storybook props, add listview

--- packages/react-learn-card/src/components/VCThumbnail/VCThumbnail.stories.tsx
+++ packages/react-learn-card/src/components/VCThumbnail/VCThumbnail.stories.tsx
@@ -15,8 +15,10 @@ export const VCThumbnailTest = Template.bind({});
 VCThumbnailTest.args = {
     title: 'Our Wallet Passed JFF Plugfest #1 2022',
     createdAt: '01 May, 2022',
-    issuerImage: "https://kayaelle.github.io/vc-ed/plugfest-1-2022/images/JFF_LogoLockup.png",
-    userImage: "https://img.freepik.com/free-photo/pleasant-looking-serious-blonde-woman-with-long-hair-wears-blue-casual-sweater_273609-17050.jpg?w=2000",
+    issuerImage: 'https://kayaelle.github.io/vc-ed/plugfest-1-2022/images/JFF_LogoLockup.png',
+    userImage:
+        'https://img.freepik.com/free-photo/pleasant-looking-serious-blonde-woman-with-long-hair-wears-blue-casual-sweater_273609-17050.jpg?w=2000',
+    listView: false,
     onClick: () => {
         console.log('Hello World');
     },
