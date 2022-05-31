35c6cfcf30136c0b06512c3dc450e48f7d539c37
Gerardo Paredes <gerardparedes23@gmail.com>
2022-05-31 09:02:29 -0700

    add onClick tests

--- packages/react-learn-card/src/components/VCThumbnail/__tests__/VCThumbnail.test.tsx
+++ packages/react-learn-card/src/components/VCThumbnail/__tests__/VCThumbnail.test.tsx
@@ -1,53 +1,57 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import VCThumbnail from '../VCThumbnail';
 
 describe('Running Tests for VCThumbnail', () => {
     test('Checks VCThumbnail renders with props', () => {
-        render(
+        const handleOnClick = jest.fn();
+
+        const { getByTestId, getByAltText } = render(
             <VCThumbnail
                 title="This is a title!"
                 createdAt="05/01/2022"
                 issuerImage="https://issuerimage.png"
                 userImage="https://userimage.png"
-                onClick={() => {
-                    console.log('Hello World');
-                }}
+                onClick={handleOnClick}
             />
         );
 
-        const title = screen.getByTestId('vc-thumbnail-title');
+        fireEvent.click(getByTestId('vc-thumbnail-title'));
+        expect(handleOnClick).toHaveBeenCalledTimes(1);
+
+        const title = getByTestId('vc-thumbnail-title');
         expect(title).toHaveTextContent('This is a title!');
 
-        const createdAt = screen.getByTestId('vc-thumbnail-createdAt');
+        const createdAt = getByTestId('vc-thumbnail-createdAt');
         expect(createdAt).toHaveTextContent('05/01/2022');
 
-        const issuerImage = screen.getByAltText('issuer image');
+        const issuerImage = getByAltText('issuer image');
         expect(issuerImage).toHaveAttribute('src', 'https://issuerimage.png');
 
-        const userImage = screen.getByAltText('user image');
+        const userImage = getByAltText('user image');
         expect(userImage).toHaveAttribute('src', 'https://userimage.png');
-
-        screen.debug();
     });
 
     test('Checks VCThumbnail renders without props', () => {
-        render(<VCThumbnail />);
+        const handleOnClick = jest.fn();
 
-        const title = screen.getByTestId('vc-thumbnail-title');
+        const { getByTestId, getByAltText } = render(<VCThumbnail />);
+
+        fireEvent.click(getByTestId('vc-thumbnail-title'));
+        expect(handleOnClick).not.toHaveBeenCalledTimes(1);
+
+        const title = getByTestId('vc-thumbnail-title');
         expect(title).toHaveTextContent('');
 
-        const createdAt = screen.getByTestId('vc-thumbnail-createdAt');
+        const createdAt = getByTestId('vc-thumbnail-createdAt');
         expect(createdAt).toHaveTextContent('');
 
-        const issuerImage = screen.getByAltText('issuer image');
+        const issuerImage = getByAltText('issuer image');
         expect(issuerImage).not.toHaveAttribute('src');
 
-        const userImage = screen.getByAltText('user image');
+        const userImage = getByAltText('user image');
         expect(userImage).not.toHaveAttribute('src');
-
-        screen.debug();
     });
 });
