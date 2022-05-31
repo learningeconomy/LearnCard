84c116fd7275bd517d094bb160ae3b59d8666f11
Gerardo Paredes <gerardparedes23@gmail.com>
2022-05-31 14:55:04 -0700

    add list-view markup

--- packages/react-learn-card/src/components/VCThumbnail/VCThumbnail.tsx
+++ packages/react-learn-card/src/components/VCThumbnail/VCThumbnail.tsx
@@ -11,6 +11,7 @@ export type VCThumbnailProps = {
     issuerImage?: string;
     userImage?: string;
     className?: string;
+    listView?: boolean;
     onClick?: () => void;
 };
 
@@ -20,8 +21,63 @@ export const VCThumbnail: React.FC<VCThumbnailProps> = ({
     issuerImage,
     userImage,
     className = '',
+    listView = false,
     onClick = () => {},
 }) => {
+    if (listView) {
+        return (
+            <div
+                onClick={onClick}
+                className={`flex flex-col items-center justify-between relative py-3 px-3 rounded-3xl shadow-3xl bg-emerald-700 max-w-sm vc-thumbnail-listview-container ${className}`}
+            >
+                <div
+                    className='flex flex-row items-center justify-between mb-3 z-10'
+                >
+                    <div className="flex flex-row items-start justify-start w-1/4 text-left">
+                        <div className="h-16 w-16 border-solid border-2 border-black bg-white">
+                            <img
+                                className="h-full w-full object-cover"
+                                src={issuerImage}
+                                alt="user image"
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-start justify-center w-3/4 text-left">
+                        <h2
+                            className="text-base tracking-wide leading-snug text-left"
+                            data-testid="vc-thumbnail-title"
+                        >
+                            {title ?? ''}
+                        </h2>
+                        <p
+                            className="text-sm font-light text-center"
+                            data-testid="vc-thumbnail-createdAt"
+                        >
+                            {createdAt ?? ''}
+                        </p>
+                    </div>
+                </div>
+                <div className="flex items-center justify-center w-full mt-3 vc-thumbnail-footer">
+                    <h3 className="text-white text-sm text-right font-normal tracking-wide leading-tight flex-1">
+                        Verified
+                    </h3>
+                    <div className="flex items-center justify-center my-0 mx-1 h-10 w-10 rounded-full bg-white overflow-hidden">
+                        <div className="flex items-center justify-center h-5/6 w-10/12 bg-white rounded-full border-solid border-4 border-emerald-700 overflow-hidden">
+                            <img
+                                className="h-full w-full object-cover"
+                                src={VerifiedCheck ?? ''}
+                                alt="credential icon"
+                            />
+                        </div>
+                    </div>
+                    <h3 className="text-white text-sm text-left font-normal tracking-wide leading-tight flex-1">
+                        Credential
+                    </h3>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div
             onClick={onClick}
@@ -83,7 +139,11 @@ export const VCThumbnail: React.FC<VCThumbnailProps> = ({
 
             <div className="flex items-center justify-center w-full">
                 <div className="h-16 w-16 border-solid border-2 border-black bg-white">
-                    <img className="h-full w-full object-cover" src={issuerImage} alt="user image" />
+                    <img
+                        className="h-full w-full object-cover"
+                        src={issuerImage}
+                        alt="user image"
+                    />
                 </div>
             </div>
         </div>
