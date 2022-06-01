e973683572eeeec55f7da3e6e215b51177f64878
goblincore <withallmyhrt@gmail.com>
2022-05-31 22:49:47 -0400

    Update story with dummy data shape from plugfest page

--- packages/react-learn-card/src/components/VCDisplayCard/VCDisplayCard.stories.tsx
+++ packages/react-learn-card/src/components/VCDisplayCard/VCDisplayCard.stories.tsx
@@ -18,9 +18,24 @@ export const VCDisplayCardTest = Template.bind({});
 // For example data structure for plugfest
 
 VCDisplayCardTest.args = {
+    "@context": [
+        "https://www.w3.org/2018/credentials/v1",
+        "https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/jff-vc-edu-plugfest-1-context.json"
+      ],
     title: 'Our Wallet Passed JFF Plugfest #1 2022',
     createdAt: '01 May, 2022',
-    issuerImage: "https://kayaelle.github.io/vc-ed/plugfest-1-2022/images/JFF_LogoLockup.png",
+    type: [
+        "VerifiableCredential",
+        "OpenBadgeCredential",
+    ],
+    issuanceDate: "2022-05-01T00:00:00Z",
+    issuer: {
+        "type": "Profile",
+        "id": "did:key:z6MkrHKzgsahxBLyNAbLQyB1pcWNYC9GmywiWPgkrvntAZcj",
+        "name": "Jobs for the Future (JFF)",
+        "url": "https://www.jff.org/",
+        "image": "https://kayaelle.github.io/vc-ed/plugfest-1-2022/images/JFF_LogoLockup.png"
+      },
     userImage: "https://img.freepik.com/free-photo/pleasant-looking-serious-blonde-woman-with-long-hair-wears-blue-casual-sweater_273609-17050.jpg?w=2000",
     credentialSubject: {
         "type": "AchievementSubject",
@@ -31,9 +46,17 @@ VCDisplayCardTest.args = {
           "description": "This wallet can display this Open Badge 3.0",
           "criteria": {
             "type": "Criteria",
-            "narrative": "The first cohort of the JFF Plugfest 1 in May/June of 2021 collaborated to push interoperability of VCs in education forward."
+            "narrative": "The first cohort of the JFF Plugfest 1 in May/June of 2021 collaborated to push interoperability of VCs in education forward.",
           },
           "image": "https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/images/plugfest-1-badge-image.png",
+        },
+    proof: {
+        "type": "Ed25519Signature2018",
+        "created": "2022-05-31T21:09:29Z",
+        "verificationMethod": "did:key:z6MkrHKzgsahxBLyNAbLQyB1pcWNYC9GmywiWPgkrvntAZcj#z6MkrHKzgsahxBLyNAbLQyB1pcWNYC9GmywiWPgkrvntAZcj",
+        "proofPurpose": "assertionMethod",
+        "jws": "eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..Q05Nb7ufRWi8b8MO11einZZzRgwU9iauqiKoB-tKCGRUIGWTM_-HUQMXsSE9rYm3wuv45qa5gl4TIl5lwWgvDA"
+        },    
     },
     onClick: () => {
         console.log('Hello World');
