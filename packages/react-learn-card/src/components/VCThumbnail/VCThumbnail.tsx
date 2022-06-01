fe704de1f2c7bbc42126d4488cd75850bfc31f3e
Gerardo Paredes <gerardparedes23@gmail.com>
2022-06-01 09:25:46 -0700

    make thumbnail responsive

--- packages/react-learn-card/src/components/VCThumbnail/VCThumbnail.tsx
+++ packages/react-learn-card/src/components/VCThumbnail/VCThumbnail.tsx
@@ -38,7 +38,7 @@ export type VCThumbnailProps = {
      */
     listView?: boolean;
     /**
-     * 
+     *
      * @type {function}
      */
     onClick?: () => void;
@@ -108,69 +108,72 @@ export const VCThumbnail: React.FC<VCThumbnailProps> = ({
     return (
         <div
             onClick={onClick}
-            className={`flex flex-col items-center justify-between relative w-60 h-60 py-3 px-3 rounded-3xl shadow-3xl bg-emerald-700 vc-thumbnail-container ${className}`}
+            className={`relative py-3 rounded-3xl shadow-3xl bg-emerald-700 vc-thumbnail-container ${className}`}
         >
-            <div className="flex flex-col items-center justify-center z-10 text-center w-full">
-                <div className="flex flex-row items-start justify-between w-full">
-                    <div className="flex flex-row items-start justify-start w-4/5">
-                        <h2
-                            className="text-base tracking-wide leading-snug text-left line-clamp-2"
-                            data-testid="vc-thumbnail-title"
-                        >
-                            {title ?? ''}
-                        </h2>
-                    </div>
-                    <div className="flex flex-row items-start justify-end w-1/5">
-                        <div className="flex items-center justify-end my-0 h-8 w-8 rounded-full bg-white overflow-hidden">
-                            <div className="flex items-center justify-center h-5/6 w-10/12 bg-white rounded-full border-solid border-2 border-emerald-700 overflow-hidden">
-                                <img
-                                    className="h-full w-full object-cover"
-                                    src={VerifiedCheck ?? ''}
-                                    alt="credential icon"
-                                />
+            <div className="h-full w-full absolute">
+                <div className="flex flex-col items-center justify-center z-10 text-center w-full">
+                    <div className="flex flex-row items-start justify-between w-full">
+                        <div className="flex flex-row items-start justify-start w-4/5 ml-3">
+                            <h2
+                                className="text-base tracking-wide leading-snug text-left line-clamp-2"
+                                data-testid="vc-thumbnail-title"
+                            >
+                                {title ?? ''}
+                            </h2>
+                        </div>
+                        <div className="flex flex-row items-start justify-end w-1/5 mr-3">
+                            <div className="flex items-center justify-end h-8 w-8 rounded-full bg-white overflow-hidden">
+                                <div className="flex items-center justify-center h-5/6 w-10/12 bg-white rounded-full border-solid border-2 border-emerald-700 overflow-hidden">
+                                    <img
+                                        className="h-full w-full object-cover"
+                                        src={VerifiedCheck ?? ''}
+                                        alt="credential icon"
+                                    />
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="w-full mt-2">
-                    <p
-                        className="text-sm font-light text-center line-clamp-1"
-                        data-testid="vc-thumbnail-createdAt"
-                    >
-                        {createdAt ?? ''}
-                    </p>
-                </div>
+                    <div className="w-full mt-2">
+                        <p
+                            className="text-sm font-light text-center line-clamp-1"
+                            data-testid="vc-thumbnail-createdAt"
+                        >
+                            {createdAt ?? ''}
+                        </p>
+                    </div>
 
-                <div className="flex flex-row items-center justify-center mt-2 w-full">
-                    <div className="flex items-center justify-center h-14 w-14 shadow-3xl rounded-full overflow-hidden bg-white">
-                        <div className="flex flex-row items-center justify-center rounded-full overflow-hidden bg-white w-10/12 h-5/6">
+                    <div className="flex flex-row items-center justify-center mt-3 w-full">
+                        <div className="inline-block relative overflow-hidden rounded-full shadow-3xl h-0 bg-white w-1/4 pb-25pct">
                             <img
-                                className="w-8/12 h-4/6 object-cover"
+                                className="h-5/6 w-10/12 left-1 top-1 absolute rounded-full object-cover"
                                 src={issuerImage}
                                 alt="issuer image"
                             />
                         </div>
-                    </div>
-                    <img className="h-8 w-8 my-0 mx-4" src={FatArrow ?? ''} alt="fat arrow icon" />
-                    <div className="flex items-center justify-center h-14 w-14 shadow-3xl rounded-full overflow-hidden bg-white">
-                        <div className="flex flex-row items-center justify-center h-full w-full rounded-full border-solid border-4 border-white overflow-hidden bg-white">
+
+                        <img
+                            className="h-8 w-8 my-0 mx-4"
+                            src={FatArrow ?? ''}
+                            alt="fat arrow icon"
+                        />
+                        <div className="inline-block relative overflow-hidden rounded-full shadow-3xl h-0 bg-white w-1/4 pb-25pct">
                             <img
-                                className="h-full w-full object-cover"
+                                className="w-full h-full absolute rounded-full object-cover border-solid border-4 border-white"
                                 src={userImage}
                                 alt="user image"
                             />
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="flex items-center justify-center w-full">
-                <div className="h-16 w-16 border-solid border-2 border-black bg-white">
-                    <img
-                        className="h-full w-full object-cover"
-                        src={issuerImage}
-                        alt="main image"
-                    />
+                <div className="flex items-center justify-center w-full mt-4">
+                    <div className="border-solid border-2 border-black bg-white relative w-1/4 h-0 overflow-hidden pt-25pct pb-0 px-0">
+                        <img
+                            className="h-full w-full object-cover absolute top-0 left-0"
+                            src={issuerImage}
+                            alt="main image"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
