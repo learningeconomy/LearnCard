131d7003fe1e282f0fdbf95353f495e77e0813c5
goblincore <withallmyhrt@gmail.com>
2022-06-02 02:55:19 -0400

    Fix loader spinner positioning in verification check component

--- packages/react-learn-card/src/components/Loading/CircleSpinner.tsx
+++ packages/react-learn-card/src/components/Loading/CircleSpinner.tsx
@@ -2,20 +2,26 @@ import React from 'react';
 
 import './CircleSpinner.css';
 
+enum CircleLoadingState {
+    loading,
+    finishedLoading,
+}
+
 export type CircleSpinnerProps = {
     size?: string | number;
     thickness?: number;
     color?: string;
+    loadingState?: CircleLoadingState;
 };
 
-const CircleSpinner: React.FC<CircleSpinnerProps> = ({ size = '35px', thickness = 8, color = '#ffffff' }) => {
-    const style = {
-        width: size,
-        height: size,
-        border: `${thickness}px solid ${color}`,
-    };
+const CircleSpinner: React.FC<CircleSpinnerProps> = ({
+    size = '60px',
+    thickness = 8,
+    color = '#40CBA6',
+}) => {
+    const spinnerClass = CircleLoadingState.loading ? 'lds-ring' : 'lds-ring static';
     return (
-        <div className="lds-ring" style={style}>
+        <div className={spinnerClass}>
             <div></div>
             <div></div>
             <div></div>
