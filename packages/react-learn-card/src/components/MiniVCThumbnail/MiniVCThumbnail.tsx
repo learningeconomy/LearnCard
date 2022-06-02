4899cbe29eeeb561188b6cf3ae13fdd04c0b7876
Gerardo Paredes <gerardparedes23@gmail.com>
2022-06-01 17:53:22 -0700

    handle styles for when images are not present

--- packages/react-learn-card/src/components/MiniVCThumbnail/MiniVCThumbnail.tsx
+++ packages/react-learn-card/src/components/MiniVCThumbnail/MiniVCThumbnail.tsx
@@ -14,30 +14,35 @@ export const MiniVCThumbnail: React.FC<MiniVCThumbnailProps> = ({
     className = '',
     onClick = () => {},
 }) => {
+    const titleClasses = !issuerImage ? 'w-full text-center' : 'w-3/4 text-left';
+    const createdAtClasses = !issuerImage ? 'w-full text-center' : 'text-left';
+
     return (
         <div
             onClick={onClick}
-            className={`flex flex-col items-center justify-between relative py-3 px-3 rounded-3xl shadow-3xl bg-emerald-700 max-w-sm mini-vc-thumbnail-container ${className}`}
+            className={`flex flex-col items-center justify-between relative py-3 px-3 rounded-3xl shadow-3xl bg-emerald-700 min-h-[120px] max-w-sm mini-vc-thumbnail-container ${className}`}
         >
             <div className="flex flex-row items-center justify-between mb-3 z-10">
-                <div className="flex flex-row justify-center items-center w-1/4 text-center">
-                    <div className="min-w-[48px] min-h-[48px] border-solid border-2 border-black bg-white">
-                        <img
-                            className="h-full w-full object-cover"
-                            src={issuerImage}
-                            alt="issuer image"
-                        />
+                {issuerImage && (
+                    <div className="flex flex-row justify-center items-center w-1/4 text-center">
+                        <div className="min-w-[48px] min-h-[48px] border-solid border-2 border-black bg-white">
+                            <img
+                                className="h-full w-full object-cover"
+                                src={issuerImage}
+                                alt="issuer image"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className="flex flex-col items-start justify-center w-3/4 text-left">
+                )}
+                <div className={`flex flex-col items-start justify-center ${titleClasses}`}>
                     <h2
-                        className="ml-2 text-sm tracking-wide leading-snug text-left line-clamp-2"
+                        className="ml-2 text-sm tracking-wide leading-snug line-clamp-2"
                         data-testid="vc-thumbnail-title"
                     >
                         {title ?? ''}
                     </h2>
                     <p
-                        className="ml-2 text-sm font-light text-center line-clamp-1"
+                        className={`ml-2 text-sm font-light line-clamp-1 ${createdAtClasses}`}
                         data-testid="vc-thumbnail-createdAt"
                     >
                         {createdAt ?? ''}
@@ -45,15 +50,17 @@ export const MiniVCThumbnail: React.FC<MiniVCThumbnailProps> = ({
                 </div>
             </div>
             <div className="flex items-center justify-center w-full mt-3">
-                <div className="flex item-center justify-center w-full -mt-3">
-                    <div className="h-20 w-20 border-solid border-2 border-black bg-white z-10">
-                        <img
-                            className="h-full w-full object-cover"
-                            src={badgeImage}
-                            alt="badge image"
-                        />
+                {badgeImage && (
+                    <div className="flex item-center justify-center w-full -mt-3">
+                        <div className="h-20 w-20 border-solid border-1 border-black bg-white z-10">
+                            <img
+                                className="h-full w-full object-cover"
+                                src={badgeImage}
+                                alt="badge image"
+                            />
+                        </div>
                     </div>
-                </div>
+                )}
                 <div className="flex absolute bottom-1 right-1 items-center justify-center my-0 mx-1 h-10 w-10 rounded-full bg-white overflow-hidden">
                     <div className="flex items-center justify-center h-5/6 w-10/12 bg-white rounded-full border-solid border-4 border-emerald-700 overflow-hidden">
                         <img
