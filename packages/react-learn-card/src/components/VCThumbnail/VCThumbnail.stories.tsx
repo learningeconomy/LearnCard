2df355ff28813869d0478fbe23968e76b3da20df
Gerardo Paredes <40479121+gerardopar@users.noreply.github.com>
2022-06-02 10:03:17 -0700

    [WE-2044] - VC Thumbnail && Mini VC Thumbnail (#8)
    * add icons
    
    * add VCThumnail component
    
    * add VCThumbnail stylesheet
    
    * add VCThumbnail story
    
    * run generate barrels
    
    * use color pallet colors
    
    * remove unused css
    
    * update tailwind config, add custom boxShadow
    
    * add moduleNameMapper
    
    * add jest test
    
    * add onClick tests
    
    * add assets/images dir
    
    * fix ::after border-radius, fix image import naming
    
    * fix assets folder placement
    
    * update markup to match design
    
    * add list-view markup
    
    * update storybook props, add listview
    
    * fix test failing
    
    * add line-clamp
    
    * fix text padding on mobile
    
    * add jsDocs
    
    * add tests for VCThumbnail list-view
    
    * make thumbnail responsive
    
    * add custom spacing utility class padding 25%
    
    * add extra small breakpoints
    
    * adjust media query
    
    * update markup, to match design
    
    * fix checkmark position
    
    * move types to its own file
    
    * fix types import
    
    * add sample badge png
    
    * add mini VCThumnail
    
    * handle styles for when images are not present
    
    * adjust issuer image size
    
    * adjust badge size
    
    * generate barrels
    
    * handle badge, issuer image not available
    
    * handle badge not existing on vc-thumbnail
    
    * rename inline element
    
    * fix mini-vc-thumbnail tests
    
    * update mini-vc-thumbnail test description
    
    * fix typo in test
    
    * fix VC thumbnail tests

---
+++ packages/react-learn-card/src/components/VCThumbnail/VCThumbnail.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import VCThumbnail from './VCThumbnail';
+import { VCThumbnailProps } from './types';
+
+export default {
+    title: 'VCThumbnail',
+    component: VCThumbnail,
+    argTypes: {},
+} as Meta<typeof VCThumbnail>;
+
+const Template: Story<VCThumbnailProps> = args => <VCThumbnail {...args} />;
+
+export const VCThumbnailTest = Template.bind({});
+VCThumbnailTest.args = {
+    title: 'Our Wallet Passed JFF Plugfest #1 2022',
+    createdAt: '01 May, 2022',
+    issuerImage: 'https://kayaelle.github.io/vc-ed/plugfest-1-2022/images/JFF_LogoLockup.png',
+    userImage:
+        'https://img.freepik.com/free-photo/pleasant-looking-serious-blonde-woman-with-long-hair-wears-blue-casual-sweater_273609-17050.jpg?w=2000',
+    badgeImage: 'https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/images/plugfest-1-badge-image.png',
+    listView: false,
+    onClick: () => {
+        console.log('Hello World');
+    },
+};
---
+++ packages/react-learn-card/src/components/VCThumbnail/__tests__/VCThumbnail.test.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, fireEvent } from '@testing-library/react';
+
+import VCThumbnail from '../VCThumbnail';
+
+describe('Running Tests for VCThumbnail Full View', () => {
+    test('Checks VCThumbnail renders with props', () => {
+        const handleOnClick = jest.fn();
+
+        const { getByTestId, getByAltText } = render(
+            <VCThumbnail
+                title="This is a title!"
+                createdAt="05/01/2022"
+                issuerImage="https://issuerimage.png"
+                userImage="https://userimage.png"
+                badgeImage='https://badgeimage.png'
+                listView={false}
+                onClick={handleOnClick}
+            />
+        );
+
+        fireEvent.click(getByTestId('vc-thumbnail'));
+        expect(handleOnClick).toHaveBeenCalledTimes(1);
+
+        const title = getByTestId('vc-thumbnail-title');
+        expect(title).toHaveTextContent('This is a title!');
+
+        const createdAt = getByTestId('vc-thumbnail-createdAt');
+        expect(createdAt).toHaveTextContent('05/01/2022');
+
+        const issuerImage = getByAltText('issuer image');
+        expect(issuerImage).toHaveAttribute('src', 'https://issuerimage.png');
+
+        const userImage = getByAltText('user image');
+        expect(userImage).toHaveAttribute('src', 'https://userimage.png');
+
+        const badgeImage = getByAltText('badge image');
+        expect(badgeImage).toHaveAttribute('src', 'https://badgeimage.png');
+    });
+
+    test('Checks VCThumbnail renders without props', () => {
+        const handleOnClick = jest.fn();
+
+        const { getByTestId, getByAltText, queryByTestId } = render(<VCThumbnail />);
+
+        fireEvent.click(getByTestId('vc-thumbnail'));
+        expect(handleOnClick).not.toHaveBeenCalledTimes(1);
+
+        const title = getByTestId('vc-thumbnail-title');
+        expect(title).toHaveTextContent('');
+
+        const createdAt = getByTestId('vc-thumbnail-createdAt');
+        expect(createdAt).toHaveTextContent('');
+
+        const issuerImage = getByAltText('issuer image');
+        expect(issuerImage).not.toHaveAttribute('src');
+
+        const userImage = getByAltText('user image');
+        expect(userImage).not.toHaveAttribute('src');
+
+        const imageElement = queryByTestId('vc-thumbnail-badge');
+        expect(imageElement).not.toBeInTheDocument();
+    });
+});
+
+describe('Running Tests for VCThumbnail List View', () => {
+    test('Checks VCThumbnail List View renders with props', () => {
+        const handleOnClick = jest.fn();
+
+        const { getByTestId, getByAltText } = render(
+            <VCThumbnail
+                title="This is a title!"
+                createdAt="05/01/2022"
+                issuerImage="https://issuerimage.png"
+                listView
+                onClick={handleOnClick}
+            />
+        );
+
+        fireEvent.click(getByTestId('vc-thumbnail-list-view'));
+        expect(handleOnClick).toHaveBeenCalledTimes(1);
+
+        const title = getByTestId('vc-thumbnail-title');
+        expect(title).toHaveTextContent('This is a title!');
+
+        const createdAt = getByTestId('vc-thumbnail-createdAt');
+        expect(createdAt).toHaveTextContent('05/01/2022');
+
+        const issuerImage = getByAltText('issuer image');
+        expect(issuerImage).toHaveAttribute('src', 'https://issuerimage.png');
+    });
+
+    test('Checks VCThumbnail List View renders without props', () => {
+        const handleOnClick = jest.fn();
+
+        const { getByTestId, queryByTestId } = render(<VCThumbnail listView />);
+
+        fireEvent.click(getByTestId('vc-thumbnail-list-view'));
+        expect(handleOnClick).not.toHaveBeenCalledTimes(1);
+
+        const title = getByTestId('vc-thumbnail-title');
+        expect(title).toHaveTextContent('');
+
+        const createdAt = getByTestId('vc-thumbnail-createdAt');
+        expect(createdAt).toHaveTextContent('');
+
+        const imageElement = queryByTestId('vc-thumbnail-image');
+        expect(imageElement).not.toBeInTheDocument();
+    });
+});
