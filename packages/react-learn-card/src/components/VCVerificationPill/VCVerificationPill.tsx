8fa8d7c153a7cfc221c344070c0d57a5894a040b
goblincore <withallmyhrt@gmail.com>
2022-06-02 07:35:20 -0400

    update verification pill props and styles

--- packages/react-learn-card/src/components/VCVerificationPill/VCVerificationPill.tsx
+++ packages/react-learn-card/src/components/VCVerificationPill/VCVerificationPill.tsx
@@ -1,71 +1,87 @@
 import React from 'react';
-
+import CircleSpinner, { CircleLoadingState } from '../Loading/CircleSpinner';
 import VerificationFailedIcon from '../../assets/images/RedXCircle.svg';
 import VerificationPassedIcon from '../../assets/images/GreenCheckCircle.svg';
 import VerificationWarningIcon from '../../assets/images/YellowWarningCircle.svg';
 
 enum VerifiableItemStatus {
-    Passed,
+    Success,
     Warning,
     Error,
     Loading,
 }
 
-type ValidationItemProps = {
+type ValidationDisplayItemProps = {
     status?: VerifiableItemStatus;
-    expiryDate?: string;
-    errorMessage?: string;
+    message?: string;
+    details?: string;
 };
 
 type VerifiableStateIndicatorProps = {
     status: VerifiableItemStatus;
 };
 
-const StatusToColor = {
-    [VerifiableItemStatus.Passed]: 'bg-emerald-500',
+const StatusToBgColor = {
+    [VerifiableItemStatus.Success]: 'bg-emerald-500',
     [VerifiableItemStatus.Warning]: 'bg-yellow-400',
     [VerifiableItemStatus.Error]: 'bg-rose-600',
     [VerifiableItemStatus.Loading]: 'bg-grayscale-50',
 };
 
+const StatusToTextColor = {
+    [VerifiableItemStatus.Success]: 'text-emerald-700',
+    [VerifiableItemStatus.Warning]: 'text-yellow-400',
+    [VerifiableItemStatus.Error]: 'text-rose-600',
+    [VerifiableItemStatus.Loading]: 'text-white',
+};
+
 const StatusToIcon = {
-    [VerifiableItemStatus.Passed]: VerificationPassedIcon,
+    [VerifiableItemStatus.Success]: VerificationPassedIcon,
     [VerifiableItemStatus.Warning]: VerificationWarningIcon,
     [VerifiableItemStatus.Error]: VerificationFailedIcon,
     [VerifiableItemStatus.Loading]: null,
 };
 
-
-
 export const ValidationStateIndicator: React.FC<VerifiableStateIndicatorProps> = ({ status }) => {
-    const bgColor = StatusToColor[status];
+    const bgColor = StatusToBgColor[status];
     const source = StatusToIcon[status];
 
-
     return (
-        <div className={`rounded-full w-[30px] h-[30px] ${bgColor}`}>
-            <div className="flex items-center justify-center rounded-[50%] bg-white rounded-full overflow-hidden">
-                <img
-                    className="h-full w-full object-contain p-1"
-                    src={source ?? ''}
-                    alt="Verification Status Icon"
-                />
+        <div className={`relative rounded-full w-[30px] h-[30px] ${bgColor}`}>
+            <div className="relative w-[100%] h-[100%] flex items-center justify-center rounded-[50%] bg-white rounded-full overflow-hidden">
+                {status !== VerifiableItemStatus.Loading && (
+                    <img
+                        className="h-full w-full object-contain p-1"
+                        src={source ?? ''}
+                        alt="Verification Status Icon"
+                    />
+                )}
+                {status === VerifiableItemStatus.Loading && (
+                    <CircleSpinner size={30} marginOffset={1} thickness={3} color={'#41CEF2'} />
+                )}
             </div>
         </div>
     );
 };
 
-const VCVerificationPill: React.FC<ValidationItemProps> = ({
-    type,
-    status = VerifiableItemStatus.Warning,
-    expiryDate,
-    errorMessage,
+const VCVerificationPill: React.FC<ValidationDisplayItemProps> = ({
+    message = 'Verification in progress....',
+    details,
+    status = VerifiableItemStatus.Loading,
 }) => {
+    const statusColor = StatusToTextColor[status];
     return (
         <section className="flex width-full bg-white p-2 px-5 my-3.5 justify-between items-center relative vc-verification-checklist-pill rounded-[22px]">
             <div className="vc-pill-left">
-                <p className="status-text">Verification Success</p>
-                <p className="vc-pill-text">Issued to Janet Yoon</p>
+                {status !== VerifiableItemStatus.Loading && (
+                    <p
+                        className={`text-[10px] text-gray-900 font-bold status-text uppercase ${statusColor}`}
+                    >
+                        Verification Success
+                    </p>
+                )}
+                <p className="text-[12px] text-grayscale-900 vc-pill-text">{message}</p>
+                {details && <p className="text-[12px] text-gray-600"> {details}</p>}
             </div>
             <div className="vc-pill-right">
                 <ValidationStateIndicator status={status} />
