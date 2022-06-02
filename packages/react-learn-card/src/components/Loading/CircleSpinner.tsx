faefe4911a091a01bac7b6e70e16b253bbeaa4ab
goblincore <withallmyhrt@gmail.com>
2022-06-02 07:16:49 -0400

    Update circle spinner props

--- packages/react-learn-card/src/components/Loading/CircleSpinner.tsx
+++ packages/react-learn-card/src/components/Loading/CircleSpinner.tsx
@@ -8,24 +8,42 @@ export enum CircleLoadingState {
 }
 
 export type CircleSpinnerProps = {
-    size?: string | number;
+    size?: number; //in pixels
     thickness?: number;
     color?: string;
     loadingState?: CircleLoadingState;
+    marginOffset?: number;
 };
 
 const CircleSpinner: React.FC<CircleSpinnerProps> = ({
-    size = '60px',
-    thickness = 8,
+    size = 60,
+    thickness = 4,
     color = '#40CBA6',
+    marginOffset = 3,
 }) => {
     const spinnerClass = CircleLoadingState.stop ? 'lds-ring' : 'lds-ring static';
+
+    // overall circumference height and width
+    const outerStyle = {
+        width: `${size}px`,
+        height: `${size}px`,
+    };
+
+    // inner circle circumference is a ratio of the outer size
+    // this is the actual spinning thing you see
+    const spinnerStyles = {
+        height: `${Math.floor(size / 1.1111111)}px`,
+        width: `${Math.floor(size / 1.11111111)}px`,
+        border: `${thickness}px solid #fff`,
+        margin: `${marginOffset}px`,
+        borderColor: `${color} transparent transparent transparent`,
+    };
     return (
-        <div className={spinnerClass}>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+        <div className={spinnerClass} style={outerStyle}>
+            <div style={spinnerStyles}></div>
+            <div style={spinnerStyles}></div>
+            <div style={spinnerStyles}></div>
+            <div style={spinnerStyles}></div>
         </div>
     );
 };
