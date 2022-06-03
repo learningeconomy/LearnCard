1f539e5199f5289a65ed73296b181e394abec76b
Taylor Beeston <beeston.taylor@gmail.com>
2022-06-02 20:52:54 -0700

    :bug: Actually fix rollup stuff!

--- packages/react-learn-card/rollup.config.js
+++ packages/react-learn-card/rollup.config.js
@@ -3,7 +3,6 @@ import postcss from 'rollup-plugin-postcss';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import image from 'rollup-plugin-img';
-import dts from 'rollup-plugin-dts';
 import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
@@ -13,28 +12,13 @@ export default [
     {
         input: 'src/index.ts',
         output: [
-            {
-                file: packageJson.main,
-                format: 'cjs',
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: 'esm',
-                sourcemap: true,
-            },
+            { file: packageJson.main, format: 'cjs', sourcemap: true },
+            { file: packageJson.module, format: 'esm', sourcemap: true },
         ],
         plugins: [
+            postcss({ minimize: true }),
             peerDepsExternal(),
-            postcss({
-                minimize: true,
-                modules: true,
-                extract: true,
-            }),
-            image({
-                extensions: /\.(png|jpg|jpeg|gif|svg)$/,
-                limit: 10000,
-            }),
+            image(),
             resolve(),
             commonjs(),
             typescript({ tsconfig: './tsconfig.json' }),
@@ -42,9 +26,4 @@ export default [
         ],
         external: ['react', 'react-dom', 'styled-components'],
     },
-    /* {
-        input: "types/index.d.ts",
-        output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts()],
-    }, */
 ];
