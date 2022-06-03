c6b06021d83ee25ccaa59730dca2c6e62c638a18
Taylor Beeston <beeston.taylor@gmail.com>
2022-06-03 11:25:13 -0700

    :bug: gitignore dist and update types in learn-card-types

--- packages/learn-card-types/dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-if(process.env.NODE_ENV === 'production') {
-    module.exports = require('./learn-card-types.cjs.production.min.js');
-} else {
-    module.exports = require('./learn-card-types.cjs.development.js');
-}
--- packages/learn-card-types/src/index.ts
+++ packages/learn-card-types/src/index.ts
@@ -11,6 +11,37 @@ export type VerificationItem = {
     details?: string;
 };
 
+export type AchievementCriteria = {
+    type?: string;
+    narrative?: string;
+};
+
+export type CredentialSubjectAchievement = {
+    type?: string;
+    name?: string;
+    description?: string;
+    criteria?: AchievementCriteria;
+    image?: string;
+};
+
+export type CredentialSubject = {
+    type?: string;
+    id?: string;
+    achievement?: CredentialSubjectAchievement;
+};
+
+export type Issuer =
+    | string
+    | { type?: string; id?: string; name?: string; url?: string; image?: string };
+
+export type CredentialInfo = {
+    title?: string;
+    createdAt?: string;
+    issuer?: Issuer;
+    issuee?: Issuer;
+    credentialSubject?: CredentialSubject;
+};
+
 export type Proof = {
     type: string;
     proofPurpose: string;
@@ -22,11 +53,11 @@ export type Proof = {
 export type UnsignedVC = {
     [key: string]: any;
     '@context': string | string[];
-    credentialSubject: { id: string };
+    credentialSubject: CredentialSubject;
     id: string;
     issuanceDate: string;
     expirationDate?: string;
-    issuer: string;
+    issuer: Issuer;
     type: string[];
 };
 
