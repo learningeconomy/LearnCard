e522943c790f9e484cd02968ddde4256b24597a8
Gerardo Paredes <gerardparedes23@gmail.com>
2022-06-15 14:39:33 -0700

    add dummy QR code

--- packages/react-learn-card/src/components/LearnCardCreditCard/LearnCardCreditCardFrontFace.tsx
+++ packages/react-learn-card/src/components/LearnCardCreditCard/LearnCardCreditCardFrontFace.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { QRCodeSVG } from 'qrcode.react';
 
 import CardChip from '../../assets/images/card-chip.svg';
 
 export const LearnCardCreditCardFrontFace: React.FC<{ userImage: string }> = ({ userImage }) => {
     return (
         <div className="flex flex-row justify-center items-start pt-9 max-w-xs max-h-[200px] min-h-[200px] rounded-[20px] shadow-3xl bg-white relative overflow-hidden">
-            <div className="absolute h-[400px] w-[400px] rounded-full bg-cyan-900 top-[-50%] left-[-65%]" />
+            <div className="absolute h-[400px] w-[400px] rounded-full bg-cyan-900 top-[-50%] left-[-68%]" />
             {/* <div className="absolute h-full w-[60%] rounded-br-[20px] rounded-tr-[20px] bg-cyan-200 top-0 left-0" /> */}
             <div className="flex flex-row justify-between items-start w-full h-full relative">
                 <div className="flex flex-col">
@@ -24,8 +25,14 @@ export const LearnCardCreditCardFrontFace: React.FC<{ userImage: string }> = ({ 
                         />
                     </div>
                 </div>
-                <div className="flex flex-col justify-start items-start">
-                    <img src="" alt="qr code" />
+                <div className="flex justify-center items-center pr-4">
+                    <div className="max-w-[110px] h-auto">
+                        <QRCodeSVG
+                            className="h-full w-full"
+                            value="https://www.npmjs.com/package/@learncard/react"
+                        />
+                    </div>
+
                     {/* <img src="" alt="mastercard icon" /> */}
                 </div>
             </div>
