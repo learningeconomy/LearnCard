f3c537a04ebf72572ba66073df6534444cd4c1d9
Gerardo Paredes <gerardparedes23@gmail.com>
2022-06-23 21:43:55 -0700

    remove hard coded icons by path

--- packages/react-learn-card/src/constants/icons.ts
+++ packages/react-learn-card/src/constants/icons.ts
@@ -1,29 +1,18 @@
 import coins from '../assets/images/coins.svg';
-import coinsWhiteIcon from '../assets/images/coins.icon.white.svg';
 import user from '../assets/images/user.svg';
-import userWhiteIcon from '../assets/images/user.icon.white.svg';
 import trophy from '../assets/images/trophy.svg';
-import trophyWhiteIcon from '../assets/images/trophy.icon.white.svg';
 import briefcase from '../assets/images/briefcase.svg';
-import briefcaseWhiteIcon from '../assets/images/briefcase.icon.white.svg';
 import graduation from '../assets/images/graduation.svg';
-import graduationWhiteIcon from '../assets/images/graduation.icon.white.svg';
 import lightbulb from '../assets/images/lightbulb.svg';
-import lightbulbWhiteIcon from '../assets/images/lightbulb.icon.white.svg';
+
 import { Icons } from '../types';
 export { coins, user, trophy, briefcase, graduation, lightbulb };
 
 export const ICONS_TO_SOURCE = {
     [Icons.sheckelsIcon]: coins,
-    [Icons.coinsWhiteIcon]: coinsWhiteIcon,
     [Icons.userIcon]: user,
-    [Icons.userWhiteIcon]: userWhiteIcon,
     [Icons.trophyIcon]: trophy,
-    [Icons.trophyWhiteIcon]: trophyWhiteIcon,
     [Icons.briefcaseIcon]: briefcase,
-    [Icons.briefcaseWhiteIcon]: briefcaseWhiteIcon,
     [Icons.graduationIcon]: graduation,
-    [Icons.graduationWhiteIcon]: graduationWhiteIcon,
     [Icons.lightbulbIcon]: lightbulb,
-    [Icons.lightbulbWhiteIcon]: lightbulbWhiteIcon,
 };
