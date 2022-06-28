3f731bccdbcfbb70da008d9b4bb4ce408cc19661
Taylor Beeston <beeston.taylor@gmail.com>
2022-06-28 14:03:43 -0700

    :white_check_mark: Add tests for RoundedSquare

--- packages/react-learn-card/src/components/RoundedSquare/RoundedSquare.tsx
+++ packages/react-learn-card/src/components/RoundedSquare/RoundedSquare.tsx
@@ -21,19 +21,21 @@ export const RoundedSquare: React.FC<RoundedSquareProps> = ({
     bgColor = 'bg-cyan-200',
 }) => {
     return (
-        <section
+        <button
             onClick={onClick}
-            className={`relative ${bgColor}  py-[15px] px-[15px] w-[170px] h-[170px] rounded-[40px] rounded-square-card-container`}
+            className={`flex relative ${bgColor}  py-[15px] px-[15px] w-[170px] h-[170px] rounded-[40px] rounded-square-card-container`}
         >
             <div className="w-full py-[10px] relative">
                 <h3 className="line-clamp-2 font-bold text-[17px] text-grayscale-900">{title}</h3>
-                <p className="line-clamp-2 text-[10px] text-grayscale-900 text-opacity-60">{description}</p>
+                <p className="line-clamp-2 text-[10px] text-grayscale-900 text-opacity-60">
+                    {description}
+                </p>
             </div>
 
             <div className="flex w-full justify-end icon-display absolute right-[20px] bottom-[15px]">
                 <CircleIcon iconSrc={iconSrc} count={count} />
             </div>
-        </section>
+        </button>
     );
 };
 
