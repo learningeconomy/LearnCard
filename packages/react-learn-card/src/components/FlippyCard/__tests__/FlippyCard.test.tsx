69bf70db3ae816fc1443a8e23b23cc1e775726b1
EC2 Default User <ec2-user@ip-172-31-5-76.ec2.internal>
2022-06-29 17:43:51 +0000

    Update tests to user userEvent, and screen, resolving @smurflo2 comments

--- packages/react-learn-card/src/components/FlippyCard/__tests__/FlippyCard.test.tsx
+++ packages/react-learn-card/src/components/FlippyCard/__tests__/FlippyCard.test.tsx
@@ -1,42 +1,46 @@
 import React from 'react';
 
 import '@testing-library/jest-dom';
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import FlippyCard from '../FlippyCard';
 
 describe('FlippyCard', () => {
     it('should not render without children', () => {
-        const { queryByTestId } = render(<FlippyCard></FlippyCard>);
-        expect(queryByTestId('flippy-card-wrapper')).not.toBe();
+        render(<FlippyCard></FlippyCard>);
+        expect(screen.queryByTestId('flippy-card-wrapper')).not.toBe();
     });
 
-    it('should flip when clicked', () => {
-        const { getByTestId } = render(<FlippyCard><div>Front Side</div><div>Back Side</div></FlippyCard>)
+    it('should flip when clicked', async () => {
+        const user = userEvent.setup();
+        render(<FlippyCard><div>Front Side</div><div>Back Side</div></FlippyCard>)
 
-        expect(getByTestId('flippy-card')).not.toHaveClass('is-flipped');
+        const flippyCard = screen.getByTestId('flippy-card');
+        expect(flippyCard).not.toHaveClass('is-flipped');
 
-        fireEvent.click(getByTestId('flippy-card'));
-        expect(getByTestId('flippy-card')).toHaveClass('is-flipped');
+        await user.click(flippyCard);
+        expect(flippyCard).toHaveClass('is-flipped');
 
-        fireEvent.click(getByTestId('flippy-card'));
-        expect(getByTestId('flippy-card')).not.toHaveClass('is-flipped');
+        await user.click(flippyCard);
+        expect(flippyCard).not.toHaveClass('is-flipped');
 
     });
 
-    it('should render the first and second child as the front and back face', () => {
-        const { getByTestId } = render(<FlippyCard><div>Front Side</div><div>Back Side</div></FlippyCard>);
+    it('should render the first and second child as the front and back face', async () => {
+        render(<FlippyCard><div>Front Side</div><div>Back Side</div></FlippyCard>);
 
-        expect(getByTestId('flippy-card-front')).toContainHTML('<div>Front Side</div>');
-        expect(getByTestId('flippy-card-back')).toContainHTML('<div>Back Side</div>');
+        expect(screen.getByTestId('flippy-card-front')).toContainHTML('<div>Front Side</div>');
+        expect(screen.getByTestId('flippy-card-back')).toContainHTML('<div>Back Side</div>');
     });
 
 
-    it('should not render extra children components', () => {
-        const { getByTestId } = render(<FlippyCard><div>Front Side</div><div>Back Side</div><div>Extra Child</div></FlippyCard>);
+    it('should not render extra children components', async () => {
+        render(<FlippyCard><div>Front Side</div><div>Back Side</div><div>Extra Child</div></FlippyCard>);
 
-        expect(getByTestId('flippy-card-front')).not.toContainHTML('<div>Extra Child</div>');
-        expect(getByTestId('flippy-card-back')).not.toContainHTML('<div>Extra Child</div>');
+        expect(screen.getByTestId('flippy-card-front')).not.toContainHTML('<div>Extra Child</div>');
+        expect(screen.getByTestId('flippy-card-back')).not.toContainHTML('<div>Extra Child</div>');
+        expect(screen.getByTestId('flippy-card-wrapper')).not.toContainHTML('<div>Extra Child</div>');
     });
 
 });
