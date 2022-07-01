1c9813ceb1d4d90f83ebe78d6bc284bdb91ce149
Gerardo Paredes <40479121+gerardopar@users.noreply.github.com>
2022-07-01 13:16:59 -0700

    WE-2112 - Learn Card Notification component (#12)
    * add white icons
    
    * update icons enum, add white icons
    
    * fix icon import types
    
    * add notification component
    
    * move notification enums to constants dir
    
    * add/update notification types
    
    * use props, update storybook args
    
    * add claimed & unclaimed buttons styles
    
    * set claim state
    
    * update rollup config, add svgr plugin
    
    * add checkmark svg component
    
    * adjust svg size
    
    * remove hard coded icons by path
    
    * convert svgs to react components
    
    * update notification icons, use svg components 🚀
    
    * clean up
    
    * remove unused icon types
    
    * remove unused type
    
    * add tests for notifications
    
    * update notification of type ID
    
    * run generate-barrels
    
    * use getByRole for buttons, replace test with it
    
    * use getByRole
    
    * clean up tests

---
+++ packages/react-learn-card/src/components/Notification/__tests__/Notification.test.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, fireEvent, screen } from '@testing-library/react';
+
+import Notification from '../Notification';
+import { NotificationTypeEnum } from '../../../constants/notifications';
+
+describe('Notification', () => {
+    it('Renders Notification of Type: Currency with props', () => {
+        const handleOnClick = jest.fn();
+
+        render(
+            <Notification
+                title="Title of Credential"
+                issuerImage="https://issuerimage.png"
+                issuerName="Jack Daniel's"
+                issueDate="04 Apr 22"
+                notificationType={NotificationTypeEnum.Currency}
+                onClick={handleOnClick}
+            />
+        );
+
+        const title = screen.getByTestId('notification-title');
+        expect(title).toHaveTextContent('Title of Credential');
+
+        const issuerName = screen.getByTestId('notification-issuer-name');
+        expect(issuerName).toHaveTextContent("Jack Daniel's");
+
+        const credIssueDate = screen.getByTestId('notification-cred-issue-date');
+        expect(credIssueDate).toHaveTextContent('04 Apr 22');
+
+        const issuerImage = screen.getByAltText('issuer image');
+        expect(issuerImage).toHaveAttribute('src', 'https://issuerimage.png');
+
+        const notificationType = screen.getByTestId('notification-type');
+        expect(notificationType).toHaveTextContent(NotificationTypeEnum.Currency);
+
+        const iconComponent = screen.getByTestId(`${NotificationTypeEnum.Currency}-icon`);
+        expect(iconComponent).toBeInTheDocument();
+
+        fireEvent.click(screen.getByRole('button', { name: /view/i }));
+        expect(handleOnClick).toHaveBeenCalledTimes(1);
+
+        const claimButton = screen.getByRole('button', { name: /claim/i });
+        expect(claimButton).toHaveTextContent('Claim');
+        expect(screen.queryByTestId('checkmark-icon')).not.toBeInTheDocument();
+
+        fireEvent.click(claimButton);
+        expect(handleOnClick).toHaveBeenCalledTimes(1);
+
+        const claimedButton = screen.getByRole('button', { name: /claimed/i });
+        expect(claimedButton).toHaveTextContent('Claimed');
+        expect(screen.queryByTestId('checkmark-icon')).toBeInTheDocument();
+    });
+
+    it('Renders Notification of Type: Identification', () => {
+        render(<Notification notificationType={NotificationTypeEnum.ID} />);
+
+        const notificationType = screen.getByTestId('notification-type');
+        expect(notificationType).toHaveTextContent(NotificationTypeEnum.ID);
+
+        const iconComponent = screen.getByTestId(`${NotificationTypeEnum.ID}-icon`);
+        expect(iconComponent).toBeInTheDocument();
+    });
+
+    it('Renders Notification of Type: Achievement', () => {
+        render(<Notification notificationType={NotificationTypeEnum.Achievement} />);
+
+        const notificationType = screen.getByTestId('notification-type');
+        expect(notificationType).toHaveTextContent(NotificationTypeEnum.Achievement);
+
+        const iconComponent = screen.getByTestId(`${NotificationTypeEnum.Achievement}-icon`);
+        expect(iconComponent).toBeInTheDocument();
+    });
+
+    it('Renders Notification of Type: Skill', () => {
+        render(<Notification notificationType={NotificationTypeEnum.Skill} />);
+
+        const notificationType = screen.getByTestId('notification-type');
+        expect(notificationType).toHaveTextContent(NotificationTypeEnum.Skill);
+
+        const iconComponent = screen.getByTestId(`${NotificationTypeEnum.Skill}-icon`);
+        expect(iconComponent).toBeInTheDocument();
+    });
+
+    it('Renders Notification of Type: Job', () => {
+        render(<Notification notificationType={NotificationTypeEnum.Job} />);
+
+        const notificationType = screen.getByTestId('notification-type');
+        expect(notificationType).toHaveTextContent(NotificationTypeEnum.Job);
+
+        const iconComponent = screen.getByTestId(`${NotificationTypeEnum.Job}-icon`);
+        expect(iconComponent).toBeInTheDocument();
+    });
+
+    it('Renders Notification of Type: Learning', () => {
+        render(<Notification notificationType={NotificationTypeEnum.Learning} />);
+
+        const notificationType = screen.getByTestId('notification-type');
+        expect(notificationType).toHaveTextContent(NotificationTypeEnum.Learning);
+
+        const iconComponent = screen.getByTestId(`${NotificationTypeEnum.Learning}-icon`);
+        expect(iconComponent).toBeInTheDocument();
+    });
+});
---
+++ packages/react-learn-card/src/components/Notification/types.ts
@@ -0,0 +1,69 @@
+import { NotificationTypeEnum } from '../../constants/notifications';
+
+import Trophy from '../svgs/Trophy';
+import Coins from '../svgs/Coins';
+import User from '../svgs/User';
+import Briefcase from '../svgs/Briefcase';
+import Lightbulb from '../svgs/Lightbulb';
+import Graduation from '../svgs/Graduation';
+
+export type NotificationProps = {
+    notificationType: NotificationTypeEnum;
+    title: string;
+    issuerImage?: string;
+    issuerName: string;
+    issueDate: string;
+    className?: string;
+    onClick: () => void;
+};
+
+export const NotificationTypeStyles = {
+    [NotificationTypeEnum.Currency]: {
+        viewButtonStyles: 'border-cyan-400 text-cyan-400',
+        unclaimedButtonStyles: 'text-white bg-cyan-400 border-cyan-400',
+        claimedButtonStyles: 'text-cyan-400 bg-cyan-50 border-cyan-50',
+        textStyles: 'text-cyan-400 capitalize',
+        iconCircleStyles: 'bg-cyan-400',
+        IconComponent: Coins,
+    },
+    [NotificationTypeEnum.ID]: {
+        viewButtonStyles: 'border-yellow-400 text-yellow-400',
+        unclaimedButtonStyles: 'text-white bg-yellow-400 border-yellow-400',
+        claimedButtonStyles: 'text-yellow-400 bg-yellow-50 border-yellow-50',
+        textStyles: 'text-yellow-400 uppercase',
+        iconCircleStyles: 'bg-yellow-400',
+        IconComponent: User,
+    },
+    [NotificationTypeEnum.Achievement]: {
+        viewButtonStyles: 'border-spice-400 text-spice-400',
+        unclaimedButtonStyles: 'text-white bg-spice-400 border-spice-400',
+        claimedButtonStyles: 'text-spice-400 bg-spice-50 border-spice-50',
+        textStyles: 'text-spice-400 capitalize',
+        iconCircleStyles: 'bg-spice-400',
+        IconComponent: Trophy,
+    },
+    [NotificationTypeEnum.Skill]: {
+        viewButtonStyles: 'border-indigo-400 text-indigo-400',
+        unclaimedButtonStyles: 'text-white bg-indigo-400 border-indigo-400',
+        claimedButtonStyles: 'text-indigo-400 bg-indigo-50 border-indigo-50',
+        textStyles: 'text-indigo-400 capitalize',
+        iconCircleStyles: 'bg-indigo-400',
+        IconComponent: Lightbulb,
+    },
+    [NotificationTypeEnum.Job]: {
+        viewButtonStyles: 'border-emerald-400 text-emerald-400',
+        unclaimedButtonStyles: 'text-white bg-emerald-400 border-emerald-400',
+        claimedButtonStyles: 'text-emerald-400 bg-emerald-50 border-emerald-50',
+        textStyles: 'text-emerald-400 capitalize',
+        iconCircleStyles: 'bg-emerald-400',
+        IconComponent: Briefcase,
+    },
+    [NotificationTypeEnum.Learning]: {
+        viewButtonStyles: 'border-rose-400 text-rose-400',
+        unclaimedButtonStyles: 'text-white bg-rose-400 border-rose-400',
+        claimedButtonStyles: 'text-rose-400 bg-rose-50 border-rose-50',
+        textStyles: 'text-rose-400 capitalize',
+        iconCircleStyles: 'bg-rose-400',
+        IconComponent: Graduation,
+    },
+};
