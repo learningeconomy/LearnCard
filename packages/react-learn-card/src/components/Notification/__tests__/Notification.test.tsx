1dcdec3b595cd857ffafefd1be417bf53476cfe3
Gerardo Paredes <40479121+gerardopar@users.noreply.github.com>
2022-07-01 12:06:47 -0700

    clean up tests

--- packages/react-learn-card/src/components/Notification/__tests__/Notification.test.tsx
+++ packages/react-learn-card/src/components/Notification/__tests__/Notification.test.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 
 import Notification from '../Notification';
 import { NotificationTypeEnum } from '../../../constants/notifications';
 
-describe('Running Tests for Notification', () => {
+describe('Notification', () => {
     it('Renders Notification of Type: Currency with props', () => {
         const handleOnClick = jest.fn();
 
-        const { getByTestId, getByAltText, queryByTestId, getByRole } = render(
+        render(
             <Notification
                 title="Title of Credential"
                 issuerImage="https://issuerimage.png"
@@ -20,94 +20,86 @@ describe('Running Tests for Notification', () => {
             />
         );
 
-        const title = getByTestId('notification-title');
+        const title = screen.getByTestId('notification-title');
         expect(title).toHaveTextContent('Title of Credential');
 
-        const issuerName = getByTestId('notification-issuer-name');
+        const issuerName = screen.getByTestId('notification-issuer-name');
         expect(issuerName).toHaveTextContent("Jack Daniel's");
 
-        const credIssueDate = getByTestId('notification-cred-issue-date');
+        const credIssueDate = screen.getByTestId('notification-cred-issue-date');
         expect(credIssueDate).toHaveTextContent('04 Apr 22');
 
-        const issuerImage = getByAltText('issuer image');
+        const issuerImage = screen.getByAltText('issuer image');
         expect(issuerImage).toHaveAttribute('src', 'https://issuerimage.png');
 
-        const notificationType = getByTestId('notification-type');
+        const notificationType = screen.getByTestId('notification-type');
         expect(notificationType).toHaveTextContent(NotificationTypeEnum.Currency);
 
-        const iconComponent = getByTestId(`${NotificationTypeEnum.Currency}-icon`);
+        const iconComponent = screen.getByTestId(`${NotificationTypeEnum.Currency}-icon`);
         expect(iconComponent).toBeInTheDocument();
 
-        fireEvent.click(getByRole('button', { name: /view/i }));
+        fireEvent.click(screen.getByRole('button', { name: /view/i }));
         expect(handleOnClick).toHaveBeenCalledTimes(1);
 
-        const claimButton = getByRole('button', { name: /claim/i });
+        const claimButton = screen.getByRole('button', { name: /claim/i });
         expect(claimButton).toHaveTextContent('Claim');
-        expect(queryByTestId('checkmark-icon')).not.toBeInTheDocument();
+        expect(screen.queryByTestId('checkmark-icon')).not.toBeInTheDocument();
 
         fireEvent.click(claimButton);
         expect(handleOnClick).toHaveBeenCalledTimes(1);
 
-        const claimedButton = getByRole('button', { name: /claimed/i });
+        const claimedButton = screen.getByRole('button', { name: /claimed/i });
         expect(claimedButton).toHaveTextContent('Claimed');
-        expect(queryByTestId('checkmark-icon')).toBeInTheDocument();
+        expect(screen.queryByTestId('checkmark-icon')).toBeInTheDocument();
     });
 
     it('Renders Notification of Type: Identification', () => {
-        const { getByTestId } = render(<Notification notificationType={NotificationTypeEnum.ID} />);
+        render(<Notification notificationType={NotificationTypeEnum.ID} />);
 
-        const notificationType = getByTestId('notification-type');
+        const notificationType = screen.getByTestId('notification-type');
         expect(notificationType).toHaveTextContent(NotificationTypeEnum.ID);
 
-        const iconComponent = getByTestId(`${NotificationTypeEnum.ID}-icon`);
+        const iconComponent = screen.getByTestId(`${NotificationTypeEnum.ID}-icon`);
         expect(iconComponent).toBeInTheDocument();
     });
 
     it('Renders Notification of Type: Achievement', () => {
-        const { getByTestId } = render(
-            <Notification notificationType={NotificationTypeEnum.Achievement} />
-        );
+        render(<Notification notificationType={NotificationTypeEnum.Achievement} />);
 
-        const notificationType = getByTestId('notification-type');
+        const notificationType = screen.getByTestId('notification-type');
         expect(notificationType).toHaveTextContent(NotificationTypeEnum.Achievement);
 
-        const iconComponent = getByTestId(`${NotificationTypeEnum.Achievement}-icon`);
+        const iconComponent = screen.getByTestId(`${NotificationTypeEnum.Achievement}-icon`);
         expect(iconComponent).toBeInTheDocument();
     });
 
     it('Renders Notification of Type: Skill', () => {
-        const { getByTestId } = render(
-            <Notification notificationType={NotificationTypeEnum.Skill} />
-        );
+        render(<Notification notificationType={NotificationTypeEnum.Skill} />);
 
-        const notificationType = getByTestId('notification-type');
+        const notificationType = screen.getByTestId('notification-type');
         expect(notificationType).toHaveTextContent(NotificationTypeEnum.Skill);
 
-        const iconComponent = getByTestId(`${NotificationTypeEnum.Skill}-icon`);
+        const iconComponent = screen.getByTestId(`${NotificationTypeEnum.Skill}-icon`);
         expect(iconComponent).toBeInTheDocument();
     });
 
     it('Renders Notification of Type: Job', () => {
-        const { getByTestId } = render(
-            <Notification notificationType={NotificationTypeEnum.Job} />
-        );
+        render(<Notification notificationType={NotificationTypeEnum.Job} />);
 
-        const notificationType = getByTestId('notification-type');
+        const notificationType = screen.getByTestId('notification-type');
         expect(notificationType).toHaveTextContent(NotificationTypeEnum.Job);
 
-        const iconComponent = getByTestId(`${NotificationTypeEnum.Job}-icon`);
+        const iconComponent = screen.getByTestId(`${NotificationTypeEnum.Job}-icon`);
         expect(iconComponent).toBeInTheDocument();
     });
 
     it('Renders Notification of Type: Learning', () => {
-        const { getByTestId } = render(
-            <Notification notificationType={NotificationTypeEnum.Learning} />
-        );
+        render(<Notification notificationType={NotificationTypeEnum.Learning} />);
 
-        const notificationType = getByTestId('notification-type');
+        const notificationType = screen.getByTestId('notification-type');
         expect(notificationType).toHaveTextContent(NotificationTypeEnum.Learning);
 
-        const iconComponent = getByTestId(`${NotificationTypeEnum.Learning}-icon`);
+        const iconComponent = screen.getByTestId(`${NotificationTypeEnum.Learning}-icon`);
         expect(iconComponent).toBeInTheDocument();
     });
 });
