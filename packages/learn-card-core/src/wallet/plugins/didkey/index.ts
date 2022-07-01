73716d4eccdbfd6bf58bb3a323088deb91b003bc
Taylor Beeston <beeston.taylor@gmail.com>
2022-07-01 09:37:03 -0700

    :goal_net: Validate that passed in key is a valid hex string

--- packages/learn-card-core/src/wallet/plugins/didkey/index.ts
+++ packages/learn-card-core/src/wallet/plugins/didkey/index.ts
@@ -4,9 +4,13 @@ import { toUint8Array } from 'hex-lite';
 import { DidKeyPluginMethods } from './types';
 import { Plugin } from 'types/wallet';
 
+const isHex = (str: string) => /^[0-9a-f]+$/i.test(str);
+
 export const getDidKeyPlugin = async (
     key: string
 ): Promise<Plugin<'DID Key', DidKeyPluginMethods>> => {
+    if (!isHex(key)) throw new Error('Key must be a hexadecimal string!');
+
     const keypair = JSON.parse(generateEd25519KeyFromBytes(toUint8Array(key.padStart(64, '0'))));
     const did = keyToDID('key', JSON.stringify(keypair));
 
