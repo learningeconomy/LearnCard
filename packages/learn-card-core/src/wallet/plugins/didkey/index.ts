0b9897d59aa68032c1a934c4282a992d8922e0cd
Taylor Beeston <beeston.taylor@gmail.com>
2022-07-01 09:38:16 -0700

    :goal_net: Validate key length

--- packages/learn-card-core/src/wallet/plugins/didkey/index.ts
+++ packages/learn-card-core/src/wallet/plugins/didkey/index.ts
@@ -9,7 +9,9 @@ const isHex = (str: string) => /^[0-9a-f]+$/i.test(str);
 export const getDidKeyPlugin = async (
     key: string
 ): Promise<Plugin<'DID Key', DidKeyPluginMethods>> => {
+    if (key.length === 0) throw new Error("Please don't use an empty string for a key!");
     if (!isHex(key)) throw new Error('Key must be a hexadecimal string!');
+    if (key.length > 64) throw new Error('Key must be less than 64 characters');
 
     const keypair = JSON.parse(generateEd25519KeyFromBytes(toUint8Array(key.padStart(64, '0'))));
     const did = keyToDID('key', JSON.stringify(keypair));
