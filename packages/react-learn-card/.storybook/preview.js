eec6f576e5d383a9220db9562cff93e1ccfb4167
Taylor Beeston <beeston.taylor@gmail.com>
2022-07-07 10:00:55 -0700

    :bug: Fix storybook

--- packages/react-learn-card/.storybook/preview.js
+++ packages/react-learn-card/.storybook/preview.js
@@ -1,11 +1,12 @@
-import '../src/tailwind.css';
+import '../src/assets/styles/base.css';
+import '../src/assets/styles/main.css';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+};
