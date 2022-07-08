109b3901b9511f900726e65682d3ab499cd1b3e1
Taylor Beeston <beeston.taylor@gmail.com>
2022-07-07 19:16:20 -0700

    :bug: Fix bad shebang

--- packages/learn-card-cli/rollup.config.js
+++ packages/learn-card-cli/rollup.config.js
@@ -6,7 +6,7 @@ const packageJson = require('./package.json');
 export default [
     {
         input: ['src/index.tsx'],
-        output: [{ file: packageJson.bin, format: 'cjs', banner: '#!/user/bin/env node' }],
+        output: [{ file: packageJson.bin, format: 'cjs', banner: '#!/usr/bin/env node' }],
         plugins: [json(), esbuild()],
     },
 ];
