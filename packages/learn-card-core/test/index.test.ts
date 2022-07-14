9b9fb82e3bcf6c0642c563a0f5a314f07c4833fe
Taylor Beeston <beeston.taylor@gmail.com>
2022-07-13 19:13:14 -0700

    :white_check_mark: Add rudimentary tests

--- packages/learn-card-core/test/index.test.ts
+++ packages/learn-card-core/test/index.test.ts
@@ -1,7 +1,26 @@
-import { main } from '../src';
+import { walletFromKey } from '../src';
 
-describe('main', () => {
-  it('should not throw', () => {
-    expect(() => main()).not.toThrow();
-  });
+const getWallet = async () => walletFromKey('a'.repeat(64));
+
+describe('walletFromKey', () => {
+    it('should work', async () => {
+        await expect(getWallet()).resolves.toBeDefined();
+    });
+
+    it('should determinstically create a did', async () => {
+        const wallet = await getWallet();
+
+        expect(wallet.did).toEqual('did:key:z6Mkv1o2GEgtXjFdEMfLtupcKhGRydM8V7VHzii7Uh4aHoqH');
+    });
+
+    it('should determinstically create a keypair', async () => {
+        const wallet = await getWallet();
+
+        expect(wallet.keypair).toEqual({
+            kty: 'OKP',
+            crv: 'Ed25519',
+            x: '5zTqbCtiV95yNV5HKqBaTEh-a0Y8Ap7TBt8vAbVja1g',
+            d: 'qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqo',
+        });
+    });
 });
