bd885b5a81a19b078908545cd030c90fed5eca78
Taylor Beeston <beeston.taylor@gmail.com>
2022-07-14 10:12:10 -0700

    :bug: Fix presentations not issuing

--- packages/learn-card-core/src/wallet/plugins/didkit/index.ts
+++ packages/learn-card-core/src/wallet/plugins/didkit/index.ts
@@ -53,11 +53,13 @@ export const getDidKitPlugin = async (
             verifyCredential: async (_wallet, credential) =>
                 JSON.parse(await verifyCredential(JSON.stringify(credential), '{}')),
 
-            issuePresentation: async (presentation, options, keypair) =>
-                issuePresentation(
-                    JSON.stringify(presentation),
-                    JSON.stringify(options),
-                    JSON.stringify(keypair)
+            issuePresentation: async (_wallet, presentation, options, keypair) =>
+                JSON.parse(
+                    await issuePresentation(
+                        JSON.stringify(presentation),
+                        JSON.stringify(options),
+                        JSON.stringify(keypair)
+                    )
                 ),
 
             verifyPresentation: async (_wallet, presentation) =>
