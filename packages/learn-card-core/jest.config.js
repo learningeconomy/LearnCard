98d0d067dd250fb127f6c0cbe4d55f9b237a202d
Taylor Beeston <beeston.taylor@gmail.com>
2022-07-14 09:57:37 -0700

    :recycle: Consolidate transform regex

--- packages/learn-card-core/jest.config.js
+++ packages/learn-card-core/jest.config.js
@@ -177,8 +177,7 @@ module.exports = {
 
     // A map from regular expressions to paths to transformers
     transform: {
-        '^.+\\.jsx?$': 'esbuild-jest',
-        '^.+\\.tsx?$': 'esbuild-jest',
+        '^.+\\.[jt]sx?$': 'esbuild-jest',
     },
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
