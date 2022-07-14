080ebfb3e66e6773407df249c0bc305e29f1d0b1
Taylor Beeston <beeston.taylor@gmail.com>
2022-07-14 10:16:37 -0700

    :bug: Get better error messages by having esbuild emit a sourcemap for jest

--- packages/learn-card-core/jest.config.js
+++ packages/learn-card-core/jest.config.js
@@ -177,7 +177,7 @@ module.exports = {
 
     // A map from regular expressions to paths to transformers
     transform: {
-        '^.+\\.[jt]sx?$': 'esbuild-jest',
+        '^.+\\.[jt]sx?$': ['esbuild-jest', { sourcemap: true }],
     },
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
