5bdf42ec52db7a4ca82a47307cedaf16785e8272
Taylor Beeston <beeston.taylor@gmail.com>
2022-08-08 12:57:35 -0700

    :recycle: Refactor handler to enforce handling all methods via TS

--- services/meta-mask-snap/src/snap.ts
+++ services/meta-mask-snap/src/snap.ts
@@ -3,10 +3,10 @@ import { walletFromKey, LearnCardWallet, DidMethod } from '@learncard/core';
 import { OnRpcRequestHandler } from '@metamask/snap-types';
 import { MetaMaskInpageProvider } from '@metamask/providers';
 
-import { LearnCardRPCAPI } from './index.ts';
+import { LearnCardRPCAPI, LearnCardRPCAPITypes } from './';
 
 import didkit from './didkit_wasm_bg.wasm';
-import { RPCMethod } from '../src/types/helpers';
+import { RPCMethod } from './types/helpers';
 
 const serializeResponse = async <Serializer extends RPCMethod>(
     serializer: Serializer,
@@ -24,41 +24,21 @@ declare global {
     const wallet: MetaMaskInpageProvider;
 }
 
-let memoizedWallet: LearnCardWallet;
-
-const getWallet = async () => {
-    if (memoizedWallet) return memoizedWallet;
-
-    const entropy = await wallet.request<{ publicKey: string; privateKey: string }>({
-        method: 'snap_getBip44Entropy_60',
-    });
-
-    const newWallet = await walletFromKey(entropy.privateKey, {
-        didkit: Uint8Array.from(atob(didkit), c => c.charCodeAt(0)),
-    });
-
-    memoizedWallet = newWallet;
-
-    return memoizedWallet;
-};
-
-export const onRpcRequest: OnRpcRequestHandler = async ({ request: _request }) => {
-    const result = await LearnCardRPCAPI.deserializer.spa(_request);
-
-    if (!result.success) throw new Error('Invalid Request');
-
-    const request = result.data;
-
-    const lcWallet = await getWallet();
-
-    if (request.method === 'did') {
+const HANDLERS: {
+    [Method in LearnCardRPCAPITypes[keyof LearnCardRPCAPITypes]['method']]: (
+        lcWallet: LearnCardWallet,
+        request: LearnCardRPCAPITypes[Method]['arguments']['deserializer']
+    ) => Promise<LearnCardRPCAPITypes[Method]['returnValue']['serializer']>;
+} = {
+    did: async (lcWallet, request) => {
         return serializeResponse(LearnCardRPCAPI.did, lcWallet.did(request.didMethod as DidMethod));
-    }
+    },
 
-    if (request.method === 'getTestVc')
+    getTestVc: async lcWallet => {
         return serializeResponse(LearnCardRPCAPI.getTestVc, lcWallet.getTestVc());
+    },
 
-    if (request.method === 'issueCredential') {
+    issueCredential: async (lcWallet, request) => {
         const permission = await wallet.request({
             method: 'snap_confirm',
             params: [
@@ -81,16 +61,16 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request: _request }) =
         }
 
         throw new Error('Did not get permission to issue credential');
-    }
+    },
 
-    if (request.method === 'verifyCredential') {
+    verifyCredential: async (lcWallet, request) => {
         return serializeResponse(
             LearnCardRPCAPI.verifyCredential,
             await lcWallet.verifyCredential(request.credential)
         );
-    }
+    },
 
-    if (request.method === 'issuePresentation') {
+    issuePresentation: async (lcWallet, request) => {
         const permission = await wallet.request({
             method: 'snap_confirm',
             params: [
@@ -113,14 +93,47 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request: _request }) =
         }
 
         throw new Error('Did not get permission to issue credential');
-    }
+    },
 
-    if (request.method === 'verifyPresentation') {
+    verifyPresentation: async (lcWallet, request) => {
         return serializeResponse(
             LearnCardRPCAPI.verifyPresentation,
             await lcWallet.verifyPresentation(request.presentation)
         );
-    }
+    },
+};
 
-    throw new Error('Method not found.');
+let memoizedWallet: LearnCardWallet;
+
+const getWallet = async () => {
+    if (memoizedWallet) return memoizedWallet;
+
+    const entropy = await wallet.request<{ publicKey: string; privateKey: string }>({
+        method: 'snap_getBip44Entropy_60',
+    });
+
+    if (!entropy?.privateKey) throw new Error('Could not get wallet entropy');
+
+    const newWallet = await walletFromKey(entropy.privateKey, {
+        didkit: Uint8Array.from(window.atob(didkit), c => c.charCodeAt(0)),
+    });
+
+    memoizedWallet = newWallet;
+
+    return memoizedWallet;
+};
+
+export const onRpcRequest: OnRpcRequestHandler = async ({ request: _request }) => {
+    const result = await LearnCardRPCAPI.deserializer.spa(_request);
+
+    if (!result.success) throw new Error('Invalid Request');
+
+    const request = result.data;
+
+    const lcWallet = await getWallet();
+
+    // TS can't tell that the request object is the same as the handler (because they both get typed
+    // as unions with all valid methods), so we simply cast to any to defeat that warning since we
+    // know that we have correctly selected the correct request handler for the request's method
+    return HANDLERS[request.method](lcWallet, request as any);
 };
