5f6995f3b5ec0352af57536a6b8a7528d6df6c4e
goblincore <withallmyhrt@gmail.com>
2022-08-09 22:57:02 -0400

    Add additional styles for achievement mini card

--- packages/react-learn-card/src/components/SmallVCCard/SmallVCCard.tsx
+++ packages/react-learn-card/src/components/SmallVCCard/SmallVCCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SmallVCCardProps } from '../../types';
-
+import { Trophy } from '../svgs';
 export const SmallVCCard: React.FC<SmallVCCardProps> = ({
     title = 'Title Lorem Ipsum',
     thumbImgSrc,
@@ -10,9 +10,28 @@ export const SmallVCCard: React.FC<SmallVCCardProps> = ({
     return (
         <button
             onClick={onClick}
-            className={`flex shadow-[0_0_8px_0px_rgba(0,0,0,0.2)] relative $ py-[15px] px-[15px] w-[160px] h-[209px] rounded-[20px] rounded-square-card-container`}
+            className={`flex-col shadow-[0_0_8px_0px_rgba(0,0,0,0.2)] relative $ py-[8px] px-[8px] w-[160px] h-[209px] rounded-[20px] rounded-square-card-container`}
         >
-            <p>{title}</p>
+            <section className="h-[110px] bg-indigo-200 flex-col justify-center items-center w-full rounded-[20px]">
+                {thumbImgSrc && (
+                    <img
+                        className="h-full w-full object-cover"
+                        src={thumbImgSrc ?? ''}
+                        alt="Credential Achievement Image"
+                    />
+                )}
+                {(!thumbImgSrc || thumbImgSrc === '') && (
+                    <Trophy className="achievement-placeholder-trophy p-[15px]" color="#ffffff" />
+                )}
+            </section>
+
+            <section className="achievement-title w-full flex justify-center line-clamp-2">
+                <p className="text-sm text-grayscale-900 font-bold mt-[10px] text-center">
+                    {title}
+                </p>
+            </section>
+            <p className="achievement-card-date mt-[5px] text-grayscale-500 text-[12px]">{date}</p>
+            <section className="achievement-card-footer"></section>
         </button>
     );
 };
