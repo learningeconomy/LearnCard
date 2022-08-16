e8f8f4834662d5480daf535bf90e1e4bd547ba35
Taylor Beeston <beeston.taylor@gmail.com>
2022-08-15 21:39:11 -0700

    :lipstick: Update nav

--- examples/snap-example-dapp/src/components/SnapInfo.tsx
+++ examples/snap-example-dapp/src/components/SnapInfo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Did from '@components/Did';
 import AddCredential from '@components/AddCredential';
@@ -16,6 +16,27 @@ import GetCredentialsList from '@components/GetCredentialsList';
 import { useIsSnapReady } from '@state/snapState';
 import { modalComponent, useModalStore } from '@state/modal';
 
+const Tabs = {
+    'View Dids': <Did />,
+    'Add Credential': <AddCredential />,
+    'Get Credential': <GetCredential />,
+    'Get Credentials': <GetCredentials />,
+    'Read From Ceramic': <ReadFromCeramic />,
+    'Issue Credential': <IssueCredential />,
+    'Verify Credential': <VerifyCredential />,
+    'Remove Credential': <RemoveCredential />,
+    'Publish Credential': <PublishCredential />,
+    'Issue Presentation': <IssuePresentation />,
+    'Verify Presentation': <VerifyPresentation />,
+    'Get Credentials List': <GetCredentialsList />,
+};
+
+const options = Object.keys(Tabs).map(option => (
+    <option key={option} value={option}>
+        {option}
+    </option>
+));
+
 const Button: React.FC<{ component: JSX.Element }> = ({ component, children }) => (
     <button
         className="border rounded bg-green-300 p-4"
@@ -26,27 +47,25 @@ const Button: React.FC<{ component: JSX.Element }> = ({ component, children }) =
     </button>
 );
 const SnapInfo: React.FC = () => {
+    const [tab, setTab] = useState<keyof typeof Tabs>('View Dids');
+
     const isSnapReady = useIsSnapReady();
     const modal = useModalStore();
+
+    useEffect(() => {
+        if (tab) modalComponent.set(Tabs[tab]);
+    }, [tab]);
 
     if (!isSnapReady) return <></>;
 
     return (
         <section className="h-full flex flex-col gap-2">
-            <header className="p-4 flex gap-4">
-                <Button component={<Did />}>View Dids</Button>
-                <Button component={<AddCredential />}>Add Credential</Button>
-                <Button component={<GetCredential />}>Get Credential</Button>
-                <Button component={<GetCredentials />}>Get Credentials</Button>
-                <Button component={<ReadFromCeramic />}>Read From Ceramic</Button>
-                <Button component={<IssueCredential />}>Issue Credential</Button>
-                <Button component={<VerifyCredential />}>Verify Credential</Button>
-                <Button component={<RemoveCredential />}>Remove Credential</Button>
-                <Button component={<PublishCredential />}>Publish Credential</Button>
-                <Button component={<IssuePresentation />}>Issue Presentation</Button>
-                <Button component={<VerifyPresentation />}>Verify Presentation</Button>
-                <Button component={<GetCredentialsList />}>Get Credentials List</Button>
-            </header>
+            <section className="flex p-2">
+                <select onChange={e => setTab(e.target.value as keyof typeof Tabs)} value={tab}>
+                    {options}
+                </select>
+                <output className="pl-2 ml-2 border-l">{tab}</output>
+            </section>
 
             <section className="flex-grow border-t overflow-auto">{modal}</section>
         </section>
