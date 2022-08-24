82dfcc48b1cac45012e54e4b25dcc39553e3aeb6
Taylor Beeston <beeston.taylor@gmail.com>
2022-08-24 12:37:23 -0700

    :label: Add better types for @digitalbazaar/vpqr

--- packages/learn-card-core/src/types/global.d.ts
+++ packages/learn-card-core/src/types/global.d.ts
@@ -5,4 +5,24 @@ declare module 'hex-lite' {
     function fromBuffer(buffer: ArrayBuffer): string;
 }
 
-declare module '@digitalbazaar/vpqr';
+declare module '@digitalbazaar/vpqr' {
+    import { VP } from '@learncard/types';
+
+    function toQrCode(args: {
+        vp: VP;
+        documentLoader: (url: string) => Promise<{ document: Record<string, any> }>;
+        size?: number;
+        diagnose?: typeof console.log;
+    }): Promise<{
+        payload: string;
+        imageDataUrl: string;
+        encodedCborld: string;
+        rawCborldBytes: Uint8Array;
+        version: number;
+    }>;
+    function fromQrCode(args: {
+        text: string;
+        documentLoader: (url: string) => Promise<{ document: Record<string, any> }>;
+        diagnose?: typeof console.log;
+    }): Promise<{ vp: VP }>;
+}
