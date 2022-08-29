1e71d2d5b37a7e595e49978b3f794a96c3bb208e
Gerardo Paredes <40479121+gerardopar@users.noreply.github.com>
2022-08-29 15:00:16 -0700

    Student ID Card component (#74)
    * add schoolCardID component
    
    * add react-barcode pkg
    
    * add assets
    
    * add idCard styles
    
    * add idCard props
    
    * generate barrels
    
    * Create flat-guests-attack.md
    
    * :bug: Fix build errors
    
    Co-authored-by: Taylor Beeston <beeston.taylor@gmail.com>

---
+++ packages/react-learn-card/src/components/SchoolIdCard/SchoolIDCard.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import Barcode from 'react-barcode';
+
+import { SchoolIDCardProps } from './types';
+
+export const SchoolIDCard: React.FC<SchoolIDCardProps> = ({
+    userImage,
+    userName,
+    text = null,
+    extraText,
+    backgroundImage,
+    className,
+    containerClassName,
+}) => {
+    let textEl: React.ReactNode | string | null = null;
+    if (typeof text === 'string') {
+        textEl = <h1>{text}</h1>;
+    } else {
+        textEl = text;
+    }
+
+    return (
+        <div
+            style={{ backgroundImage: `url(${backgroundImage})` }}
+            className={`h-[200px] w-[320px] rounded-[20px] overflow-hidden ${className}`}
+        >
+            <div
+                className={`h-full w-full flex items-center justify-start relative ${containerClassName}`}
+            >
+                <div className="w-full flex justify-start items-center pl-3 mt-10">
+                    <div
+                        className="h-[100px] w-[80px] rounded-full overflow-hidden bg-no-repeat bg-cover bg-center border-solid border-[2px] border-white mr-2"
+                        style={{ backgroundImage: `url(${userImage})` }}
+                    />
+                    <div className="h-full flex items-start justify-center flex-col mt-7">
+                        {userName && <p className="text-sm text-black font-light">{userName}</p>}
+                        {textEl && textEl}
+                    </div>
+                </div>
+
+                <div className="barcode-container">
+                    <div className="barcode-wrap">
+                        <Barcode value="barcode-example" height="22px" width="1px" fontSize={0} />
+                    </div>
+                </div>
+
+                {extraText && (
+                    <div>
+                        <p className="absolute bottom-3 left-0 w-full flex items-center justify-center text-white text-xs font-semibold">
+                            {extraText}
+                        </p>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default SchoolIDCard;
--- packages/react-learn-card/src/declaration.d.ts
+++ packages/react-learn-card/src/declaration.d.ts
@@ -17,3 +17,5 @@ declare module '*.svg' {
     const value: any;
     export default value;
 }
+
+declare module 'react-barcode';
