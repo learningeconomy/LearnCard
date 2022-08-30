2808d37a4889d9fafd341b472e4c7dbb68eef40a
Donny <withallmyhrt@gmail.com>
2022-08-29 20:46:03 -0400

    Add Course Card, Skill Cards (#75)

---
+++ packages/react-learn-card/src/components/CourseCard/CourseCard.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import CourseCard from './CourseCard';
+import { CourseCardProps } from '../../types';
+
+export default {
+    title: 'Course Card',
+    component: CourseCard,
+    argTypes: {},
+} as Meta<typeof CourseCard>;
+
+const Template: Story<CourseCardProps> = args => <CourseCard {...args} />;
+
+export const CourseCardTest = Template.bind({});
+CourseCardTest.args = {
+    status: 'Enrolled',
+    title: 'MECH 1340 Digital Fundamentals and Programmable Logic Controllers',
+    semester: 'Fall 2022',
+    jobCount: 1,
+    achievementCount: 4,
+    skillCount: 9,
+};
