5eca699c6e8bceadee95e3bd5ba26a7a3655b41c
Taylor Beeston <39720479+TaylorBeeston@users.noreply.github.com>
2022-09-28 09:15:34 -0700

    :bug: Fix fixBuild.sh script on BSD/OSX (#86)
    I learned a thing about sed today!
    
    Co-authored-by: Taylor Beeston <taylor@Taylors-iMac-Pro.local>

--- services/meta-mask-snap/fixBundle.sh
+++ services/meta-mask-snap/fixBundle.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-sed -i 's/var root = freeGlobal || freeSelf || Function("return this")()/var root = { Uint8Array }/g' dist/snap.js
-sed -i 's/exports.onRpcRequest = onRpcRequest/module.exports = { onRpcRequest }/g' dist/snap.js
-sed -i 's/var isEdge = .*;/var isEdge = false;/g' dist/snap.js
-sed -i 's/fetch2 = .*;/fetch2 = fetch;/g' dist/snap.js
-sed -i 's/{ signal: abortSignal }/{}/g' dist/snap.js
-sed -i 's/g\.AbortSignal/AbortSignal/g' dist/snap.js
-sed -i 's/g\.AbortController/AbortController/g' dist/snap.js
-sed -i 's/g\.AbortSignal/AbortSignal/g' dist/snap.js
+sed -i.bak 's/var root = freeGlobal || freeSelf || Function("return this")()/var root = { Uint8Array }/g' dist/snap.js
+sed -i.bak 's/exports.onRpcRequest = onRpcRequest/module.exports = { onRpcRequest }/g' dist/snap.js
+sed -i.bak 's/var isEdge = .*;/var isEdge = false;/g' dist/snap.js
+sed -i.bak 's/fetch2 = .*;/fetch2 = fetch;/g' dist/snap.js
+sed -i.bak 's/{ signal: abortSignal }/{}/g' dist/snap.js
+sed -i.bak 's/g\.AbortSignal/AbortSignal/g' dist/snap.js
+sed -i.bak 's/g\.AbortController/AbortController/g' dist/snap.js
+sed -i.bak 's/g\.AbortSignal/AbortSignal/g' dist/snap.js
+rm dist/snap.js.bak
