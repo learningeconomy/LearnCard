7f9a93298acafd102c391b9f37269dd581c4e319
goblincore <withallmyhrt@gmail.com>
2022-09-30 02:40:45 -0400

    rename checkmark to checkmarksvg and add click handler

--- packages/react-learn-card/src/components/CircleCheckButton/CircleButton.tsx
+++ packages/react-learn-card/src/components/CircleCheckButton/CircleButton.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { CircleButtonProps } from '../../types';
+import { CheckmarkSvg } from '../RoundedPill';
 
 export const CircleCheckButton: React.FC<CircleButtonProps> = ({
     checked,
     onClick,
     className = '',
 }) => {
+    const bgColor = checked ? 'bg-emerald-700' : 'bg-gray-50';
+
+    const handleClick = () => {
+        onClick?.();
+    };
+
     return (
         <button
             type="button"
-            onClick={onClick}
-            className={`p-4 border rounded text-gray-700 ${className}`}
-        ></button>
+            onClick={handleClick}
+            className={`w-[32px] h-[32px] flex items-center justify-center border-4 border-gray-50 rounded-full text-gray-700  ${bgColor} ${className}`}
+        >
+            <CheckmarkSvg color="#ffffff" />
+        </button>
     );
 };
 
