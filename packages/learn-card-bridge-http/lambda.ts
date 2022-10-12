99ed90b71f6e6e4e13103e965b18e9e997dde400
Taylor Beeston <beeston.taylor@gmail.com>
2022-10-12 14:01:23 -0700

    :zap: Keep Bridge Warm!

--- packages/learn-card-bridge-http/lambda.ts
+++ packages/learn-card-bridge-http/lambda.ts
@@ -1,4 +1,16 @@
+import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
 import serverlessHttp from 'serverless-http';
-import app from './src/app';
 
-export const handler = serverlessHttp(app);
+import app from './src/app';
+import { getWallet } from './src/wallet';
+
+const _handler = serverlessHttp(app);
+
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
+    if ((event as any).source === 'serverless-plugin-warmup') {
+        getWallet(); // Intentionally don't wait for the wallet to init!
+        return 'Warming lambda...';
+    }
+
+    return _handler(event, context);
+};
