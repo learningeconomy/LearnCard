18995f3df45bdde8333f78541c56ead535c62f31
EC2 Default User <ec2-user@ip-172-31-5-76.ec2.internal>
2022-10-20 20:29:49 +0000

    Add support for registering new issuers

--- services/learn-card-discord-bot/src/Commands.ts
+++ services/learn-card-discord-bot/src/Commands.ts
@@ -2,8 +2,13 @@ import { Command } from './Command';
 import { IssueCredential } from './commands/IssueCredential';
 import { AddCredential, AddCredentialModal } from './commands/AddCredential';
 import { ListCredentials } from './commands/ListCredentials';
-import { SendCredential, SendCredentialSelection } from './commands/SendCredential';
+import {
+    SendCredential,
+    SendCredentialSelection,
+    PickIssuerSelection,
+} from './commands/SendCredential';
 import { RegisterDID } from './commands/RegisterDID';
+import { ConfigureIssuer, ConfigureIssuerModal } from './commands/ConfigureIssuer';
 
 export const Commands: Command[] = [
     IssueCredential,
@@ -11,6 +16,7 @@ export const Commands: Command[] = [
     ListCredentials,
     SendCredential,
     RegisterDID,
+    ConfigureIssuer,
 ];
-export const Modals: object[] = [AddCredentialModal];
-export const MessageComponents: object[] = [SendCredentialSelection];
+export const Modals: object[] = [AddCredentialModal, ConfigureIssuerModal];
+export const MessageComponents: object[] = [SendCredentialSelection, PickIssuerSelection];
--- services/learn-card-discord-bot/src/wallet/learncard.ts
+++ services/learn-card-discord-bot/src/wallet/learncard.ts
@@ -1,5 +1,8 @@
-import { walletFromKey } from '@learncard/core';
+import crypto from 'crypto';
+import { initLearnCard } from '@learncard/core';
 
 export const getWallet = async (seed: string) => {
-    return walletFromKey(seed);
+    return initLearnCard({ seed });
 };
+
+export const generateRandomSeed = () => crypto.randomBytes(32).toString('hex');
