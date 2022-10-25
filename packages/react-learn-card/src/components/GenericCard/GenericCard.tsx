b42ab74ae91e920fab65e68402bb53413ed269d8
goblincore <withallmyhrt@gmail.com>
2022-10-25 17:19:35 -0400

    Update generic card styles to match figma

--- packages/react-learn-card/src/components/GenericCard/GenericCard.tsx
+++ packages/react-learn-card/src/components/GenericCard/GenericCard.tsx
@@ -12,14 +12,19 @@ export const GenericCard: React.FC<GenericCardProps> = ({
     return (
         <button
             onClick={onClick}
-            className={`flex-col shadow-[0_0_8px_0px_rgba(0,0,0,0.2)] relative $ py-[8px] px-[8px] w-[160px] h-[209px] rounded-[20px] rounded-square-card-container`}
+            className={`flex flex-col overflow-hidden shadow-[0_0_8px_0px_rgba(0,0,0,0.2)] relative $ py-[0px] px-[0px] w-[160px] h-[209px] rounded-[20px] rounded-square-card-container`}
         >
+            <section className="flex generic-card-title w-full flex justify-center items-center  h-[76px]  bg-grayscale-900">
+                <p className="text-[14px] font-bold mt-[10px] text-center text-white line-clamp-2">
+                    {title}
+                </p>
+            </section>
             <section
-                className={`flex h-[110px] ${thumbClass} flex-col justify-center items-center w-full rounded-[20px]`}
+                className={`flex h-[110px] m-auto ${thumbClass} w-[140px] overflow-hidden p-[15px] flex-col justify-center items-center w-full rounded-[20px]`}
             >
                 {thumbImgSrc && (
                     <img
-                        className="h-full w-full object-contain"
+                        className="h-full w-full object-contain overflow-hidden"
                         src={thumbImgSrc ?? ''}
                         alt="Credential Achievement Image"
                     />
@@ -30,12 +35,6 @@ export const GenericCard: React.FC<GenericCardProps> = ({
                         color="#ffffff"
                     />
                 )}
-            </section>
-
-            <section className="generic-card-title w-full flex justify-center line-clamp-2">
-                <p className="text-sm text-grayscale-900 font-bold mt-[10px] text-center">
-                    {title}
-                </p>
             </section>
 
             <section className="generic-card-footer"></section>
