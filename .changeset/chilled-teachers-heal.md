e67119e978d4df324aa71235c3b202b3e8bdde7c
Donny <withallmyhrt@gmail.com>
2022-10-25 17:45:07 -0400

    Small UI updates (#128)

---
+++ .changeset/chilled-teachers-heal.md
@@ -0,0 +1,5 @@
+---
+"@learncard/react": patch
+---
+
+Small UI updates - add generic display card, job history card, update job listing card
---
+++ packages/react-learn-card/src/components/JobHistoryCard/JobHistoryCard.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import JobHistoryCard from './JobHistoryCard';
+import { JobHistoryCardProps } from '../../types';
+
+export default {
+    title: 'Job History Card',
+    component: JobHistoryCard,
+    argTypes: {},
+} as Meta<typeof JobHistoryCard>;
+
+const Template: Story<JobHistoryCardProps> = args => <JobHistoryCard {...args} />;
+
+export const JobHistoryCardTest = Template.bind({});
+JobHistoryCardTest.args = {
+    className: 'job-history-card',
+    title: 'Title of Job',
+    company: 'Taco Bell',
+    jobType: 'Full-Time',
+    showArrow: false,
+    dateRange: 'JUL 2021 - JAN 2022',
+    description: 'Job description history lorum ipsum potato tomato wee',
+    onClick: () => {
+        console.log('clicked');
+    },
+};
