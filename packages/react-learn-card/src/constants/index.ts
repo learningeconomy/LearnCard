5d934bd829a52cc9dd9e52e282c9686f3710379b
goblincore <withallmyhrt@gmail.com>
2022-10-26 23:44:21 -0400

    Unify components to use WalletCategoryTypes

--- packages/react-learn-card/src/constants/index.ts
+++ packages/react-learn-card/src/constants/index.ts
@@ -8,7 +8,7 @@ import achievementsGraphic from '../assets/images/walletTrophy.webp';
 import MiniTrophyIcon from '../assets/images/minitrophycolored.svg';
 import MiniGradIcon from '../assets/images/minigradcapcolored.svg';
 import MiniPuzzleIcon from '../assets/images/minipuzzlecolored.svg';
-import { LCSubtypes } from '../types';
+import { LCSubtypes, WalletCategoryTypes } from '../types';
 
 export const TYPE_TO_MINI_ICON: any = {
     [LCSubtypes.course]: MiniGradIcon,
@@ -16,29 +16,38 @@ export const TYPE_TO_MINI_ICON: any = {
     [LCSubtypes.skill]: MiniPuzzleIcon,
 };
 
-export const WALLET_SUBTYPES = {
-    ACHIEVEMENTS: 'achievements',
-    IDS: 'ids',
-    JOB_HISTORY: 'jobHistory',
-    CURRENCIES: 'currency',
-    LEARNING_HISTORY: 'learningHistory',
-    SKILLS: 'skills',
-};
-
 export const TYPE_TO_IMG_SRC = {
-    [WALLET_SUBTYPES.ACHIEVEMENTS]: achievementsGraphic,
-    [WALLET_SUBTYPES.IDS]: idsGraphic,
-    [WALLET_SUBTYPES.JOB_HISTORY]: jobhistoryGraphic,
-    [WALLET_SUBTYPES.LEARNING_HISTORY]: learningHistoryGraphic,
-    [WALLET_SUBTYPES.CURRENCIES]: coinsGraphic,
-    [WALLET_SUBTYPES.SKILLS]: skillsGraphic,
+    [WalletCategoryTypes.achievements]: achievementsGraphic,
+    [WalletCategoryTypes.ids]: idsGraphic,
+    [WalletCategoryTypes.jobHistory]: jobhistoryGraphic,
+    [WalletCategoryTypes.learningHistory]: learningHistoryGraphic,
+    [WalletCategoryTypes.currency]: coinsGraphic,
+    [WalletCategoryTypes.skills]: skillsGraphic,
 };
 
 export const TYPE_TO_WALLET_COLOR = {
-    [WALLET_SUBTYPES.ACHIEVEMENTS]: 'spice-300',
-    [WALLET_SUBTYPES.IDS]: 'yellow-300',
-    [WALLET_SUBTYPES.JOB_HISTORY]: 'rose-300',
-    [WALLET_SUBTYPES.LEARNING_HISTORY]: 'emerald-300',
-    [WALLET_SUBTYPES.CURRENCIES]: 'cyan-200',
-    [WALLET_SUBTYPES.SKILLS]: 'indigo-300',
+    [WalletCategoryTypes.achievements]: 'spice-300',
+    [WalletCategoryTypes.ids]: 'yellow-300',
+    [WalletCategoryTypes.jobHistory]: 'rose-300',
+    [WalletCategoryTypes.learningHistory]: 'emerald-300',
+    [WalletCategoryTypes.currency]: 'cyan-200',
+    [WalletCategoryTypes.skills]: 'indigo-300',
+};
+
+export const TYPE_TO_WALLET_LIGHT_COLOR = {
+    [WalletCategoryTypes.achievements]: 'spice-200',
+    [WalletCategoryTypes.ids]: 'yellow-200',
+    [WalletCategoryTypes.jobHistory]: 'rose-200',
+    [WalletCategoryTypes.learningHistory]: 'emerald-200',
+    [WalletCategoryTypes.currency]: 'cyan-200',
+    [WalletCategoryTypes.skills]: 'indigo-200',
+};
+
+export const TYPE_TO_WALLET_DARK_COLOR = {
+    [WalletCategoryTypes.achievements]: 'spice-500',
+    [WalletCategoryTypes.ids]: 'yellow-400',
+    [WalletCategoryTypes.jobHistory]: 'rose-400',
+    [WalletCategoryTypes.learningHistory]: 'emerald-500',
+    [WalletCategoryTypes.currency]: 'cyan-200',
+    [WalletCategoryTypes.skills]: 'indigo-400',
 };
