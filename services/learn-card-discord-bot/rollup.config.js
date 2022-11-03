64d2105b64ab1f552c7788e2d6230981ac536a15
Jacksón Smith <custard7@gmail.com>
2022-11-03 15:13:32 -0400

    Update services/learn-card-discord-bot/rollup.config.js
    Co-authored-by: Taylor Beeston <39720479+TaylorBeeston@users.noreply.github.com>

--- services/learn-card-discord-bot/rollup.config.js
+++ services/learn-card-discord-bot/rollup.config.js
@@ -1,7 +1,7 @@
 import json from '@rollup/plugin-json';
 import esbuild from 'rollup-plugin-esbuild';
 
-const packageJson = require('./package.json');
+import packageJson from './package.json';
 
 export default [
     {
