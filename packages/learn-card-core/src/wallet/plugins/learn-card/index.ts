74203f4f0cf70b260a769d4d3af6bd0d075a4515
Taylor Beeston <beeston.taylor@gmail.com>
2022-11-07 22:14:59 -0800

    :sparkles: Move context in LearnCard plugin

--- packages/learn-card-core/src/wallet/plugins/learn-card/index.ts
+++ packages/learn-card-core/src/wallet/plugins/learn-card/index.ts
@@ -14,6 +14,50 @@ export const getLearnCardPlugin = (
     displayName: 'LearnCard',
     description: 'Adds opinionated logic to a LearnCard',
     methods: {
+        issueCredential: async (_learnCard, credential, options) => {
+            try {
+                const vc = await learnCard.invoke.issueCredential(
+                    credential,
+                    options ?? {},
+                    {} as any // intentionally throw if wrong signature
+                );
+
+                // move proof context to outer context array
+                if (!Array.isArray(vc.proof)) {
+                    vc['@context'] = Array.from(
+                        new Set([...vc['@context'], ...vc.proof['@context']])
+                    );
+
+                    delete vc.proof['@context'];
+                }
+
+                return vc;
+            } catch (error) {
+                throw new Error(`Error issuing VC: ${error}`);
+            }
+        },
         verifyCredential: verifyCredential(learnCard) as any,
+        issuePresentation: async (_learnCard, presentation, options) => {
+            try {
+                const vp = await learnCard.invoke.issuePresentation(
+                    presentation,
+                    options ?? {},
+                    {} as any // intentionally throw if wrong signature
+                );
+
+                // move proof context to outer context array
+                if (!Array.isArray(vp.proof)) {
+                    vp['@context'] = Array.from(
+                        new Set([...vp['@context'], ...vp.proof['@context']])
+                    );
+
+                    delete vp.proof['@context'];
+                }
+
+                return vp;
+            } catch (error) {
+                throw new Error(`Error issuing VP: ${error}`);
+            }
+        },
     },
 });
--- packages/learn-card-core/src/wallet/plugins/learn-card/types.ts
+++ packages/learn-card-core/src/wallet/plugins/learn-card/types.ts
@@ -1,4 +1,12 @@
-import { VC, VerificationCheck, VerificationItem } from '@learncard/types';
+import {
+    JWK,
+    UnsignedVC,
+    VC,
+    UnsignedVP,
+    VP,
+    VerificationCheck,
+    VerificationItem,
+} from '@learncard/types';
 import { Plugin } from 'types/wallet';
 import { ProofOptions } from '../didkit';
 
@@ -9,12 +17,26 @@ export type VerifyCredential = {
 
 /** @group LearnCard Plugin */
 export type LearnCardPluginDependentMethods = {
+    issueCredential:
+    | ((credential: UnsignedVC, signingOptions?: Partial<ProofOptions>) => Promise<VC>)
+    | ((credential: UnsignedVC, options: Partial<ProofOptions>, keypair: JWK) => Promise<VC>);
     verifyCredential: (vc: VC, options?: Partial<ProofOptions>) => Promise<VerificationCheck>;
+    issuePresentation:
+    | ((presentation: UnsignedVP, signingOptions?: Partial<ProofOptions>) => Promise<VP>)
+    | ((presentation: UnsignedVP, options: Partial<ProofOptions>, keypair: JWK) => Promise<VP>);
 };
 
 /** @group LearnCard Plugin */
 export type LearnCardPluginMethods = {
+    issueCredential: (
+        credential: UnsignedVC,
+        signingOptions?: Partial<ProofOptions>
+    ) => Promise<VC>;
     verifyCredential: VerifyCredential;
+    issuePresentation: (
+        presentation: UnsignedVP,
+        signingOptions?: Partial<ProofOptions>
+    ) => Promise<VP>;
 };
 
 /** @group LearnCard Plugin */
@@ -24,4 +46,13 @@ export type LearnCardPlugin = Plugin<
     LearnCardPluginMethods,
     any,
     LearnCardPluginDependentMethods
+>;
+
+/** @group LearnCard Plugin */
+export type LearnCardPluginNoIssuance = Plugin<
+    'LearnCard',
+    any,
+    Omit<LearnCardPluginMethods, 'issueCredential' | 'issuePresentation'>,
+    any,
+    Omit<LearnCardPluginDependentMethods, 'issueCredential' | 'issuePresentation'>
 >;
