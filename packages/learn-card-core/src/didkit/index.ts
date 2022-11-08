54c18155b71dc3ff1bd2ddb4ea71dbb0612e365b
Taylor Beeston <beeston.taylor@gmail.com>
2022-11-07 20:43:55 -0800

    :goal_net: Update hosted wasm file

--- packages/learn-card-core/src/didkit/index.ts
+++ packages/learn-card-core/src/didkit/index.ts
@@ -5,7 +5,7 @@ export * from './pkg/didkit_wasm';
 let initialized = false;
 
 export const init = async (
-    arg: InitInput | Promise<InitInput> = 'https://cdn.filestackcontent.com/haoSPuEQnqrKwBACgH0D'
+    arg: InitInput | Promise<InitInput> = 'https://cdn.filestackcontent.com/vnAYGRmCRsOhZDUrWrUc'
 ) => {
     // allow calling multiple times without reinitializing
     if (initialized) return;
