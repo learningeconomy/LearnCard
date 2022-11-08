86f35413e6006a17a596d71ea3f186f915e90f28
Taylor Beeston <39720479+TaylorBeeston@users.noreply.github.com>
2022-11-08 07:57:52 -0800

    [WE-2547] Issue to LCW (#166)
    * :sparkles: Add Issue to LCW button in CHAPI example site
    
    * :sparkles: Allow making jff2 credentials with vc-templates plugin
    
    * :sparkles: Use JFF2 VC for LCW button
    
    * :bookmark: Changeset
    
    * :green_heart: Update snap.manifest.json
    
    Co-authored-by: Manifest Destiny <beestontaylor@learningeconomy.io>

---
+++ .changeset/weak-ligers-impress.md
@@ -0,0 +1,5 @@
+---
+'@learncard/core': patch
+---
+
+Allow making a JFF 2 VC in VC-Templates plugin
--- packages/learn-card-core/src/wallet/plugins/vc-templates/templates.ts
+++ packages/learn-card-core/src/wallet/plugins/vc-templates/templates.ts
@@ -47,4 +47,47 @@ export const VC_TEMPLATES: { [Key in keyof VcTemplates]: (args: VcTemplates[Key]
                 },
             },
         }),
+        jff2: ({
+            did = 'did:example:d23dd687a7dc6787646f2eb98d0',
+            subject = 'did:example:d23dd687a7dc6787646f2eb98d0',
+            issuanceDate = '2020-08-19T21:41:50Z',
+        } = {}) => ({
+            '@context': [
+                'https://www.w3.org/2018/credentials/v1',
+                'https://purl.imsglobal.org/spec/ob/v3p0/context.json',
+                'https://w3id.org/security/suites/ed25519-2020/v1',
+            ],
+            id: 'urn:uuid:a63a60be-f4af-491c-87fc-2c8fd3007a58',
+            type: ['VerifiableCredential', 'OpenBadgeCredential'],
+            name: 'JFF x vc-edu PlugFest 2 Interoperability',
+            issuer: {
+                type: ['Profile'],
+                id: did,
+                name: 'Jobs for the Future (JFF)',
+                image: {
+                    id: 'https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/images/JFF_LogoLockup.png',
+                    type: 'Image',
+                },
+            },
+            issuanceDate: issuanceDate,
+            credentialSubject: {
+                type: ['AchievementSubject'],
+                id: subject,
+                achievement: {
+                    id: 'urn:uuid:bd6d9316-f7ae-4073-a1e5-2f7f5bd22922',
+                    type: ['Achievement'],
+                    name: 'JFF x vc-edu PlugFest 2 Interoperability',
+                    description:
+                        'This credential solution supports the use of OBv3 and w3c Verifiable Credentials and is interoperable with at least two other solutions.  This was demonstrated successfully during JFF x vc-edu PlugFest 2.',
+                    criteria: {
+                        narrative:
+                            'Solutions providers earned this badge by demonstrating interoperability between multiple providers based on the OBv3 candidate final standard, with some additional required fields. Credential issuers earning this badge successfully issued a credential into at least two wallets.  Wallet implementers earning this badge successfully displayed credentials issued by at least two different credential issuers.',
+                    },
+                    image: {
+                        id: 'https://w3c-ccg.github.io/vc-ed/plugfest-2-2022/images/JFF-VC-EDU-PLUGFEST2-badge-image.png',
+                        type: 'Image',
+                    },
+                },
+            },
+        }),
     };
