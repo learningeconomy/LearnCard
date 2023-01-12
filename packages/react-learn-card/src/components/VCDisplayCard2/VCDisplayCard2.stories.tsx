70c5fc4b3c4c1e07bf4e12650c2b5c830b1a71e5
Kyle <Smurflo2@gmail.com>
2023-01-12 15:13:19 -0700

    Start on new VC display

---
+++ packages/react-learn-card/src/components/VCDisplayCard2/VCDisplayCard2.stories.tsx
@@ -0,0 +1,141 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import VCDisplayCard2, { VCDisplayCard2Props } from './VCDisplayCard2';
+import { VerificationStatusEnum } from '@learncard/types';
+
+export default {
+    title: 'VCDisplayCard2',
+    component: VCDisplayCard2,
+    argTypes: {},
+} as Meta<typeof VCDisplayCard2>;
+
+const Template: Story<VCDisplayCard2Props> = args => <VCDisplayCard2 {...args} />;
+
+const cardImgOverride = (
+    <img
+        className="h-full w-full object-cover"
+        src={
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Lunar_eclipse_of_2022_November_8_-_cropped.jpg/440px-Lunar_eclipse_of_2022_November_8_-_cropped.jpg'
+        }
+        alt="Credential Achievement Image"
+    />
+);
+
+const DetailsOverrideTest = (
+    <>
+        <div className="width-full">
+            <h6 className="line-clamp-1 text-grayscale-900 font-bold uppercase text-xs tracking-wider subpixel-antialiased">
+                Description
+            </h6>
+            <p className="line-clamp-3 subpixel-antialiased text-grayscale-600 text-[14px] lc-line-clamp">
+                override test description
+            </p>
+        </div>
+
+        <div className="width-full mt-[10px]">
+            <h6 className="line-clamp-1 text-grayscale-900 font-bold uppercase text-xs tracking-wider  subpixel-antialiased">
+                Criteria
+            </h6>
+            <p className="line-clamp-3 subpixel-antialiased text-grayscale-600 text-[14px] lc-line-clamp">
+                override critera text
+            </p>
+        </div>
+
+        <div className="width-full mt-[10px] line-clamp-1 overflow-hidden vc-issuer-name-info">
+            <h6 className="line-clamp-1 text-grayscale-900 font-bold uppercase text-xs tracking-wider subpixel-antialiased">
+                Issuer
+            </h6>
+            <p className="max-w-[344px] line-clamp-1 subpixel-antialiased text-grayscale-600 text-[14px] block overflow-ellipsis break-all">
+                override details issuer override details issuer override details issuer override
+                details issuer override details issuer override details issuer
+            </p>
+        </div>
+
+        <div className="width-full mt-[10px] line-clamp-1 overflow-hidden vc-issuer-name-info">
+            <h6 className="line-clamp-1 text-grayscale-900 font-bold uppercase text-xs tracking-wider subpixel-antialiased">
+                other field
+            </h6>
+            <p className="max-w-[344px] line-clamp-1 subpixel-antialiased text-grayscale-600 text-[14px] block overflow-ellipsis break-all">
+                override details
+            </p>
+        </div>
+    </>
+);
+
+const customHeaderTest = () => (
+    <div
+        onClick={() => console.log('test')}
+        className="rounded-[40px] bg-emerald-700 max-w-[200px] p-[5px] m-auto"
+    >
+        CUSTOM CONTENT
+    </div>
+);
+
+export const VCDisplayCardTest = Template.bind({});
+
+// See https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/
+// For example data structure for plugfest
+
+VCDisplayCardTest.args = {
+    // customHeaderComponent: customHeaderTest(),
+    // overrideCardImageComponent: cardImgOverride,
+    // overrideDetailsComponent: DetailsOverrideTest,
+    // overrideCardTitle: 'Override Card Title test',
+    // subjectImageComponent: (
+    //     <div className="flex flex-row items-center justify-center h-full w-full overflow-hidden bg-emerald-700 text-white font-medium text-3xl">
+    //         LC
+    //     </div>
+    // ),
+    // issuerImageComponent: (
+    //     <div className="flex flex-row items-center justify-center h-full w-full overflow-hidden bg-emerald-700 text-white font-medium text-3xl">
+    //         DC
+    //     </div>
+    // ),
+    credential: {
+        '@context': [
+            'https://www.w3.org/2018/credentials/v1',
+            'https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/jff-vc-edu-plugfest-1-context.json',
+        ],
+        type: ['VerifiableCredential', 'OpenBadgeCredential'],
+        issuer: {
+            type: 'Profile',
+            id: 'did:example:123',
+            name: 'Jobs for the Future (JFF)',
+            url: 'https://www.jff.org/',
+            image: 'https://kayaelle.github.io/vc-ed/plugfest-1-2022/images/JFF_LogoLockup.png',
+        },
+        issuanceDate: '2022-07-27T19:57:31.512Z',
+        credentialSubject: {
+            type: 'AchievementSubject',
+            id: 'did:example:123',
+            achievement: {
+                type: 'Achievement',
+                name: 'Our Wallet Passed JFF Plugfest #1 2022',
+                description: 'This wallet can display this Open Badge 3.0',
+                criteria: {
+                    type: 'Criteria',
+                    narrative:
+                        'The first cohort of the JFF Plugfest 1 in May/June of 2021 collaborated to push interoperability of VCs in education forward.',
+                },
+                image: 'https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/images/plugfest-1-badge-image.png',
+            },
+        },
+    },
+    // issueeOverride: {
+    //     name: 'Test Person',
+    // },
+    // issuerOverride: {
+    //     name: 'Dilbert Charles',
+    // },
+    // loading: false,
+    // className: 'bg-indigo-700',
+    // TODO test with Failed and Error ones too
+    verification: [
+        {
+            check: 'proof',
+            status: VerificationStatusEnum.Success,
+            message: 'Valid • Does Not Expire',
+        },
+    ],
+};
---
+++ packages/react-learn-card/src/components/VCDisplayCard2/VCDisplayCard2.tsx
@@ -0,0 +1,231 @@
+import React, { useLayoutEffect, useState, useEffect, useRef } from 'react';
+import fitty, { FittyInstance, FittyOptions } from 'fitty';
+
+import { VC, VerificationItem } from '@learncard/types';
+// import { initLearnCard } from '@learncard/core';
+
+type VerifiableCredentialInfo = {
+    title: string;
+    description: string;
+    image?: string;
+    skills: { name: string; subskills?: string[] };
+    // skills: SkillType[];
+    // subskills: SubskillType[];
+    issuedToUserThumb: React.ReactNode;
+    issuedToUserName: string;
+    issuedByName: string;
+};
+
+export type VCDisplayCard2Props = {
+    credentialInfo?: VerifiableCredentialInfo;
+    credential?: VC;
+    verification: VerificationItem[];
+};
+
+export const VCDisplayCard2: React.FC<VCDisplayCard2Props> = ({
+    credentialInfo,
+    credential,
+    verification,
+}) => {
+    const isPreview = !credential;
+
+    const [isFront, setIsFront] = useState(true);
+    const [statusText, setStatusText] = useState(isPreview ? 'Sample Preview' : '...');
+    const [verificationResults, setVerificationResults] = useState(isPreview ? undefined : []);
+    const [headerHeight, setHeaderHeight] = useState(44); // 44 is the height if the header is one line
+    const [headerWidth, setHeaderWidth] = useState(0);
+
+    const headerRef = useRef<HTMLHeadingElement>(null);
+
+    useLayoutEffect(() => {
+        // Needs a small setTimeout otherwise it'll be wrong sometimes with multiline header.
+        //   Probably because of the interaction with FitText
+        setTimeout(() => {
+            setHeaderHeight(headerRef.current?.clientHeight ?? 44);
+            setHeaderWidth(headerRef.current?.clientWidth ?? 0);
+        }, 10);
+    });
+
+    let statusColor = '';
+    switch (statusText) {
+        case '...':
+        case 'Sample Preview':
+            statusColor = '#828282';
+            break;
+        case 'Success':
+            statusColor = '#39B54A';
+            break;
+        case 'Failed':
+            statusColor = '#D01012';
+            break;
+        case 'Error':
+            statusColor = '#FFBD06';
+            break;
+        default:
+            statusColor = '#000000';
+    }
+
+    return (
+        <section className="font-mouse flex flex-col items-center border-solid border-[5px] border-white h-full rounded-[30px] overflow-visible z-10 max-w-[400px] relative">
+            <RibbonEnd
+                side="left"
+                className="absolute left-[-30px] top-[50px] z-0"
+                height={(headerHeight + 10).toString()}
+            />
+            <RibbonEnd
+                side="right"
+                className="absolute right-[-30px] top-[50px] z-0"
+                height={(headerHeight + 10).toString()}
+            />
+
+            <h1
+                ref={headerRef}
+                className="leading-[100%] px-[20px] py-[10px] overflow-visible mt-[40px] absolute text-center text-white bg-[#ffbd06] border-y-[5px] border-[#ffcf00] shadow-bottom text-shadow w-[calc(100%_+_16px)] rounded-t-[8px] text-[32px]"
+                style={{ wordBreak: 'break-word' }}
+            >
+                {!isFront && (
+                    <button
+                        className="absolute left-[-22px]"
+                        style={{ top: `${headerHeight / 2 - 18}px` }}
+                        role="button"
+                        onClick={() => setIsFront(true)}
+                    >
+                        {/*
+                        <img
+                            src={LeftArrow}
+                            alt="Back"
+                            className="h-[36px] w-[36px] bg-[#2F80ED] rounded-[40px] px-[4px] border-white border-[3px] border-solid"
+                        />
+                        */}
+                    </button>
+                )}
+                <FitText
+                    // text={credentialInfo.title}
+                    text={'Fitty title what whaaaaaaaaaaaaaaaat'}
+                    width={((headerWidth ?? 290) - 40).toString()}
+                    options={{ maxSize: 32, minSize: 20, multiLine: true }}
+                />
+            </h1>
+            <div
+                className="flex flex-col items-center gap-[20px] bg-[#2f80ed] grow w-full rounded-t-[30px] rounded-b-[20px] pb-[30px] overflow-scroll"
+                style={{ paddingTop: headerHeight + 70 }}
+            >
+                vc info content
+                {/*  
+                {isFront && <VerifiableCredentialFrontDetails credential={credentialInfo} />}
+                {!isFront && (
+                    <VerifiableCredentialBackDetails
+                        credentialInfo={credentialInfo}
+                        verificationResults={verificationResults}
+                    />
+                )}*/}
+            </div>
+            <footer className="w-full flex justify-between p-[5px] mt-[5px]">
+                {/* <img src={VcCheckmark} alt="checkmark" /> */}
+                <div className="font-montserrat flex flex-col items-center justify-center">
+                    <span>Verified Credential</span>
+                    <span className="vc-status-text" style={{ color: statusColor }}>
+                        {statusText}
+                    </span>
+                </div>
+                <button
+                    role="button"
+                    className={`footer-switch-button ${isFront ? 'info' : 'award'}`}
+                    onClick={() => setIsFront(!isFront)}
+                />
+            </footer>
+        </section>
+    );
+};
+
+const RibbonEnd: React.FC<{ side: 'left' | 'right'; className?: string; height?: string }> = ({
+    side,
+    className = '',
+    height = '64',
+}) => {
+    const halfHeight = parseInt(height) / 2;
+    return (
+        <svg
+            className={className}
+            width="30"
+            height={height}
+            viewBox={`0 0 30 ${height}`}
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ transform: `scaleX(${side === 'left' ? '1' : '-1'})` }}
+        >
+            <g filter="url(#filter0_d_4620_22659)">
+                <path d={`M0 0H30V${height}H0L6.36364 ${halfHeight}L0 0Z`} fill="#FFBD06" />
+                <path
+                    d={`M3.08593 2.5H27.5V${height}H3.08593L8.80922 ${halfHeight}L8.91926 30L8.80922 29.4812L3.08593 2.5Z`}
+                    stroke="#FFCF00"
+                    strokeWidth="5"
+                />
+            </g>
+            <defs>
+                <filter
+                    id="filter0_d_4620_22659"
+                    x="0"
+                    y="0"
+                    width="30"
+                    height={height}
+                    filterUnits="userSpaceOnUse"
+                    colorInterpolationFilters="sRGB"
+                >
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feColorMatrix
+                        in="SourceAlpha"
+                        type="matrix"
+                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                        result="hardAlpha"
+                    />
+                    <feOffset dy="4" />
+                    <feComposite in2="hardAlpha" operator="out" />
+                    <feColorMatrix
+                        type="matrix"
+                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+                    />
+                    <feBlend
+                        mode="normal"
+                        in2="BackgroundImageFix"
+                        result="effect1_dropShadow_4620_22659"
+                    />
+                    <feBlend
+                        mode="normal"
+                        in="SourceGraphic"
+                        in2="effect1_dropShadow_4620_22659"
+                        result="shape"
+                    />
+                </filter>
+            </defs>
+        </svg>
+    );
+};
+
+type FitTextProps = {
+    text: string;
+    width: string; // Must be set for this to work
+    options?: FittyOptions;
+    textClassName?: string;
+};
+
+const FitText: React.FC<FitTextProps> = ({ text, width, options, textClassName = '' }) => {
+    const textRef = useRef<HTMLSpanElement>(null);
+
+    useEffect(() => {
+        let fit: FittyInstance;
+        if (textRef.current) fit = fitty(textRef.current, options);
+
+        return () => fit?.unsubscribe();
+    }, [textRef.current]);
+
+    return (
+        <div style={{ width }} className="text-center">
+            <span ref={textRef} className={textClassName}>
+                {text}
+            </span>
+        </div>
+    );
+};
+
+export default VCDisplayCard2;
