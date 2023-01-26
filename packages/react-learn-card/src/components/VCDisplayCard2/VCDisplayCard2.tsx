077e4ad5c14a3104751b9e0d42415b8a9daa0434
Kyle <Smurflo2@gmail.com>
2023-01-26 14:16:31 -0700

    black opaque details/back buttons to handle custom colors

--- packages/react-learn-card/src/components/VCDisplayCard2/VCDisplayCard2.tsx
+++ packages/react-learn-card/src/components/VCDisplayCard2/VCDisplayCard2.tsx
@@ -202,7 +202,7 @@ export const VCDisplayCard2: React.FC<VCDisplayCard2Props> = ({
                     )}
                     <button
                         type="button"
-                        className="text-white shadow-bottom bg-grayscale-900 px-[30px] py-[8px] rounded-[40px] text-[28px] tracking-[0.75px] uppercase leading-[28px] mt-[40px] w-fit"
+                        className="text-white shadow-bottom bg-[#00000099] px-[30px] py-[8px] rounded-[40px] text-[28px] tracking-[0.75px] uppercase leading-[28px] mt-[40px] w-fit"
                         onClick={() => setIsFront(!isFront)}
                     >
                         {isFront && 'Details'}
