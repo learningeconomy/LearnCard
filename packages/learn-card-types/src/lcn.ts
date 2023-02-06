6a754080b13983eabc3b9fdc59ce9521ac3b00f0
Taylor Beeston <beeston.taylor@gmail.com>
2023-02-06 13:48:01 -0800

    :coffin: Remove unused LCNProfileID code

--- packages/learn-card-types/src/lcn.ts
+++ packages/learn-card-types/src/lcn.ts
@@ -7,10 +7,3 @@ export const LCNProfileValidator = z.object({
     image: z.string().optional(),
 });
 export type LCNProfile = z.infer<typeof LCNProfileValidator>;
-
-export const LCNProfileIDValidator = z.union([
-    z.object({ handle: z.string() }),
-    z.object({ did: z.string() }),
-    z.object({ email: z.string() }),
-]);
-export type LCNProfileID = z.infer<typeof LCNProfileIDValidator>;
