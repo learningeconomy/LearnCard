69bf3c7b4dfd9f7a9c73287b49a13c87ab575476
Taylor Beeston <beeston.taylor@gmail.com>
2023-02-09 12:38:45 -0800

    :sparkles: Export NetworkLearnCard type

--- packages/plugins/learn-card-network/src/learnCard.ts
+++ packages/plugins/learn-card-network/src/learnCard.ts
@@ -2,9 +2,11 @@ import { initLearnCard, LearnCardFromSeed, AddPlugin } from '@learncard/core';
 import { getLearnCardNetworkPlugin } from './plugin';
 import { LearnCardNetworkPlugin } from './types';
 
+export type NetworkLearnCard = AddPlugin<LearnCardFromSeed['returnValue'], LearnCardNetworkPlugin>;
+
 export const initNetworkLearnCard = async (
     _config: LearnCardFromSeed['args'] & { network?: string }
-): Promise<AddPlugin<LearnCardFromSeed['returnValue'], LearnCardNetworkPlugin>> => {
+): Promise<NetworkLearnCard> => {
     const { network = 'https://network.learncard.com/trpc', seed, ...config } = _config;
 
     const baseLearnCard = await initLearnCard({ seed, ...config });
