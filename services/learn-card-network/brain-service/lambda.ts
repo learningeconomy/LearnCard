2cfb494564e9a110f3d110f779ff4e398250aab5
Taylor Beeston <beeston.taylor@gmail.com>
2023-02-09 13:18:34 -0800

    :bug: Truly fix CORS issues

--- services/learn-card-network/brain-service/lambda.ts
+++ services/learn-card-network/brain-service/lambda.ts
@@ -1,4 +1,5 @@
 import serverlessHttp from 'serverless-http';
+import type { Context, APIGatewayProxyResultV2, APIGatewayProxyEventV2 } from 'aws-lambda';
 import { awsLambdaRequestHandler } from '@trpc/server/adapters/aws-lambda';
 import { createOpenApiAwsLambdaHandler } from 'trpc-openapi';
 
@@ -9,12 +10,12 @@ import { appRouter, createContext } from './src/app';
 export const swaggerUiHandler = serverlessHttp(app, { basePath: '/docs' });
 export const didWebHandler = serverlessHttp(didWebApp, { basePath: '/users' });
 
-export const openApiHandler = createOpenApiAwsLambdaHandler({
+export const _openApiHandler = createOpenApiAwsLambdaHandler({
     router: appRouter,
     createContext,
 });
 
-export const trpcHandler = awsLambdaRequestHandler({
+export const _trpcHandler = awsLambdaRequestHandler({
     router: appRouter,
     createContext,
     responseMeta: () => {
@@ -27,3 +28,39 @@ export const trpcHandler = awsLambdaRequestHandler({
         };
     },
 });
+
+export const openApiHandler = async (
+    event: APIGatewayProxyEventV2,
+    context: Context
+): Promise<APIGatewayProxyResultV2> => {
+    if (event.requestContext.http.method === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Methods': '*',
+            },
+        };
+    }
+
+    return _openApiHandler(event, context);
+};
+
+export const trpcHandler = async (
+    event: APIGatewayProxyEventV2,
+    context: Context
+): Promise<APIGatewayProxyResultV2> => {
+    if (event.requestContext.http.method === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Methods': '*',
+            },
+        };
+    }
+
+    return _trpcHandler(event, context);
+};
