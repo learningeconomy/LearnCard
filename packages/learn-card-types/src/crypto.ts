4610e8c83cd387f1cd2e48766c9f8e9dd57b569a
Taylor Beeston <beeston.taylor@gmail.com>
2023-02-17 12:08:16 -0800

    :sparkles: Add JWE types

--- packages/learn-card-types/src/crypto.ts
+++ packages/learn-card-types/src/crypto.ts
@@ -5,6 +5,34 @@ export const JWKValidator = z.object({
     crv: z.string(),
     x: z.string(),
     y: z.string().optional(),
+    n: z.string().optional(),
     d: z.string(),
 });
 export type JWK = z.infer<typeof JWKValidator>;
+
+export const JWERecipientHeaderValidator = z.object({
+    alg: z.string(),
+    iv: z.string(),
+    tag: z.string(),
+    epk: JWKValidator.partial().optional(),
+    kid: z.string().optional(),
+    apv: z.string().optional(),
+    apu: z.string().optional(),
+});
+export type JWERecipientHeader = z.infer<typeof JWERecipientHeaderValidator>;
+
+export const JWERecipientValidator = z.object({
+    header: JWERecipientHeaderValidator,
+    encrypted_key: z.string(),
+});
+export type JWERecipient = z.infer<typeof JWERecipientValidator>;
+
+export const JWEValidator = z.object({
+    protected: z.string(),
+    iv: z.string(),
+    ciphertext: z.string(),
+    tag: z.string(),
+    aad: z.string().optional(),
+    recipients: JWERecipientValidator.array().optional(),
+});
+export type JWE = z.infer<typeof JWEValidator>;
