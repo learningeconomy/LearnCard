260d163e62502f693b17f8301b0813c63a001f0b
Taylor Beeston <beeston.taylor@gmail.com>
2023-02-22 12:01:54 -0800

    :white_check_mark: Add tests for updateBoost

--- services/learn-card-network/brain-service/test/boosts.spec.ts
+++ services/learn-card-network/brain-service/test/boosts.spec.ts
@@ -54,4 +54,55 @@ describe('Boosts', () => {
             expect(boosts).toHaveLength(1);
         });
     });
+
+    describe('updateBoost', () => {
+        beforeEach(async () => {
+            await Profile.delete({ detach: true, where: {} });
+            await Credential.delete({ detach: true, where: {} });
+            await Boost.delete({ detach: true, where: {} });
+
+            await userA.clients.fullAuth.profile.createProfile({ profileId: 'usera' });
+            await userB.clients.fullAuth.profile.createProfile({ profileId: 'userb' });
+
+            await sendCredential(
+                { profileId: 'usera', user: userA },
+                { profileId: 'userb', user: userB }
+            );
+        });
+
+        afterAll(async () => {
+            await Profile.delete({ detach: true, where: {} });
+            await Profile.delete({ detach: true, where: {} });
+            await Credential.delete({ detach: true, where: {} });
+        });
+
+        it('should require full auth to update a boost', async () => {
+            const boosts = await userA.clients.fullAuth.boost.getBoosts();
+            const uri = boosts[0]!.uri;
+
+            await expect(
+                noAuthClient.boost.updateBoost({ uri, updates: { name: 'nice' } })
+            ).rejects.toMatchObject({ code: 'UNAUTHORIZED' });
+            await expect(
+                userA.clients.partialAuth.boost.updateBoost({ uri, updates: { name: 'nice' } })
+            ).rejects.toMatchObject({ code: 'UNAUTHORIZED' });
+        });
+
+        it('should allow you to update boost name', async () => {
+            const boosts = await userA.clients.fullAuth.boost.getBoosts();
+            const boost = boosts[0]!;
+            const uri = boost.uri;
+
+            expect(boost.name).toBeUndefined();
+
+            await expect(
+                userA.clients.fullAuth.boost.updateBoost({ uri, updates: { name: 'nice' } })
+            ).resolves.not.toThrow();
+
+            const newBoosts = await userA.clients.fullAuth.boost.getBoosts();
+            const newBoost = newBoosts[0]!;
+
+            expect(newBoost.name).toEqual('nice');
+        });
+    });
 });
