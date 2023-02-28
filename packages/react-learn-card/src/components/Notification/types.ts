3f9c4740f411c051acf72c2cd59e34576b4a458c
gerardopar <gerardparedes23@gmail.com>
2023-02-28 13:38:48 -0800

    render notification type text

--- packages/react-learn-card/src/components/Notification/types.ts
+++ packages/react-learn-card/src/components/Notification/types.ts
@@ -20,7 +20,17 @@ export type NotificationProps = {
     loadingState: boolean;
 };
 
-export const NotificationTypeStyles = {
+export const NotificationTypeStyles: {
+    [key: NotificationTypeEnum | string]: {
+        viewButtonStyles: string;
+        unclaimedButtonStyles: string;
+        claimedButtonStyles: string;
+        textStyles: string;
+        iconCircleStyles: string;
+        IconComponent: any;
+        typeText: string;
+    };
+} = {
     [NotificationTypeEnum.Currency]: {
         viewButtonStyles: 'border-cyan-600 text-cyan-600',
         unclaimedButtonStyles: 'text-white bg-cyan-600 border-cyan-600',
@@ -28,6 +38,7 @@ export const NotificationTypeStyles = {
         textStyles: 'text-cyan-600 capitalize',
         iconCircleStyles: 'bg-cyan-600',
         IconComponent: Coins,
+        typeText: 'Curreny',
     },
     [NotificationTypeEnum.SocialBadges]: {
         viewButtonStyles: 'border-cyan-600 text-cyan-600',
@@ -36,6 +47,7 @@ export const NotificationTypeStyles = {
         textStyles: 'text-cyan-600 capitalize',
         iconCircleStyles: 'bg-cyan-600',
         IconComponent: AwardRibbon,
+        typeText: 'Social Badge',
     },
     [NotificationTypeEnum.ID]: {
         viewButtonStyles: 'border-yellow-400 text-yellow-400',
@@ -44,6 +56,7 @@ export const NotificationTypeStyles = {
         textStyles: 'text-yellow-400 uppercase',
         iconCircleStyles: 'bg-yellow-400',
         IconComponent: User,
+        typeText: 'ID',
     },
     [NotificationTypeEnum.Achievement]: {
         viewButtonStyles: 'border-spice-400 text-spice-400',
@@ -52,6 +65,7 @@ export const NotificationTypeStyles = {
         textStyles: 'text-spice-400 capitalize',
         iconCircleStyles: 'bg-spice-400',
         IconComponent: Trophy,
+        typeText: 'Achievement',
     },
     [NotificationTypeEnum.Skill]: {
         viewButtonStyles: 'border-indigo-400 text-indigo-400',
@@ -60,6 +74,7 @@ export const NotificationTypeStyles = {
         textStyles: 'text-indigo-400 capitalize',
         iconCircleStyles: 'bg-indigo-400',
         IconComponent: Lightbulb,
+        typeText: 'Skill',
     },
     [NotificationTypeEnum.Job]: {
         viewButtonStyles: 'border-emerald-400 text-emerald-400',
@@ -68,6 +83,7 @@ export const NotificationTypeStyles = {
         textStyles: 'text-emerald-400 capitalize',
         iconCircleStyles: 'bg-emerald-400',
         IconComponent: Briefcase,
+        typeText: 'Job',
     },
     [NotificationTypeEnum.Learning]: {
         viewButtonStyles: 'border-rose-400 text-rose-400',
@@ -76,5 +92,6 @@ export const NotificationTypeStyles = {
         textStyles: 'text-rose-400 capitalize',
         iconCircleStyles: 'bg-rose-400',
         IconComponent: Graduation,
+        typeText: 'Learning',
     },
 };
