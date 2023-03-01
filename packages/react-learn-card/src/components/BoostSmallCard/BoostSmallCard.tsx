2731f6a41564d9169c814b8b4bfd1d3718b09ad8
goblincore <withallmyhrt@gmail.com>
2023-03-01 09:42:15 -0500

    update styles for small boost card

--- packages/react-learn-card/src/components/BoostSmallCard/BoostSmallCard.tsx
+++ packages/react-learn-card/src/components/BoostSmallCard/BoostSmallCard.tsx
@@ -17,41 +17,45 @@ export const BoostSmallCard: React.FC<BoostSmallCardProps> = ({
     subHeaderComponent,
 }) => {
     const thumbClass = `bg-${TYPE_TO_WALLET_DARK_COLOR[type]}` ?? 'bg-grayscale-50';
-    const defaultThumbClass = `flex h-[110px] m-auto ${thumbClass} w-[140px] overflow-hidden flex-col justify-center items-center w-full rounded-[20px] ${customThumbClass}`;
+    const defaultThumbClass = `flex h-[110px] my-[10px] mx-auto ${thumbClass} w-[110px] overflow-hidden flex-col justify-center items-center w-full rounded-full ${customThumbClass}`;
     const imgSrc = thumbImgSrc?.trim() !== '' ? thumbImgSrc : TYPE_TO_IMG_SRC[type];
     const headerBgColor = `bg-${TYPE_TO_WALLET_DARK_COLOR[type]}` ?? 'bg-grayscale-900';
     const checkBtnClass = checkStatus ? 'generic-vc-card checked' : 'generic-vc-card unchecked';
-    const defaultHeaderClass = `flex generic-card-title w-full flex justify-center items-center  h-[76px] ${headerBgColor} ${customHeaderClass}`;
-    const flippedClass = flipped ? 'flex-col-reverse' : 'flex-col';
+    const defaultHeaderClass = `flex generic-card-title w-full justify-center ${customHeaderClass}`;
+
 
     return (
         <div
             onClick={onClick}
-            className={`flex generic-display-card-simple bg-white ${flippedClass} shadow-[0_0_8px_0px_rgba(0,0,0,0.2)] relative $ py-[0px] px-[0px] w-[160px] h-[215px] rounded-[20px] overflow-hidden ${className}`}
+            className={`flex generic-display-card-simple bg-white flex-col shadow-[0_0_8px_0px_rgba(0,0,0,0.2)] relative $ py-[0px] px-[0px] w-[160px] h-[215px] rounded-[20px] overflow-hidden ${className}`}
         >
-            <section className={defaultHeaderClass}>
-                <p className="relative z-[100] text-[14px] px-[10px] py-[0px] font-bold mt-[10px] text-center text-white line-clamp-2">
-                    {title}
-                </p>
-            </section>
+          
 
             <section className={defaultThumbClass}>
                 {thumbImgSrc && thumbImgSrc?.trim() !== '' && (
                     <img
-                        className="generic-display-card-img h-full w-full  w-[140px] h-[119px] rounded-[20px] object-cover overflow-hidden"
+                        className="generic-display-card-img h-full w-full  w-[110px] h-[110px] rounded-full object-cover overflow-hidden"
                         src={thumbImgSrc ?? ''}
                         alt="Credential Achievement Image"
                     />
                 )}
                 {(!thumbImgSrc || thumbImgSrc?.trim() === '') && (
                     <img
-                        className="max-w-[130px] w-full h-full p-[10px] object-contain"
+                        className="max-w-[110px] w-full h-full p-[0px] object-cover rounded-full"
                         src={imgSrc}
                     />
                 )}
             </section>
 
-            <section className="generic-card-footer"></section>
+            <section className={defaultHeaderClass}>
+                <p className="relative z-[100] small-boost-title text-[14px] p-[0px] font-bold text-center line-clamp-2">
+                    {title}
+                </p>
+            </section>
+
+            <section className="small-boost-card-footer flex justify-center items-center">
+                <div className="boost-btn-click">Boost</div>
+            </section>
             {showChecked && (
                 <div className="check-btn-overlay absolute top-[5px] left-[5px]">
                     <CircleCheckButton
