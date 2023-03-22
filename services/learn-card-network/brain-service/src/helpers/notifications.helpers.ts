12e860b72409dbafa69fd8f6dac8f603a87b988d
Jacksón Smith <custard7@gmail.com>
2023-03-22 11:10:06 -0400

    Wire up did:web plugin for sending notifications

--- services/learn-card-network/brain-service/src/helpers/notifications.helpers.ts
+++ services/learn-card-network/brain-service/src/helpers/notifications.helpers.ts
@@ -1,28 +1,10 @@
-import { getLearnCard } from '@helpers/learnCard.helpers';
+import { getDidWebLearnCard } from '@helpers/learnCard.helpers';
 import { LCNNotification } from '@learncard/types';
 
 export async function sendNotification(notification: LCNNotification) {
     try {
         if (process.env.NOTIFICATIONS_SERVICE_WEBHOOK_URL) {
-            const learnCard = await getLearnCard();
-
-            // TODO: use did:web instead of did:key
-            // const domainName = 'network.learncard.com';
-            // const domain =
-            //     !domainName || process.env.IS_OFFLINE
-            //         ? `localhost%3A${process.env.PORT || 3000}`
-            //         : domainName;
-            // const did = `did:web:${domain}`;
-            // const unsignedVP: UnsignedVP = {
-            //     '@context': ['https://www.w3.org/2018/credentials/v1'],
-            //     type: ['VerifiablePresentation'],
-            //     holder: did,
-            // };
-
-            // const didJwt = learnCard.invoke.issuePresentation(unsignedVP, {
-            //     proofPurpose: 'authentication',
-            //     proofFormat: 'jwt'
-            // });
+            const learnCard = await getDidWebLearnCard();
 
             const didJwt = await learnCard.invoke.getDidAuthVp({ proofFormat: 'jwt' });
 
