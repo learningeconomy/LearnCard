4d9f45036f4ec33b628217c5d8973848d808e57c
Taylor Beeston <beeston.taylor@gmail.com>
2023-04-12 10:54:25 -0700

    :lipstick: Use button instead of div

--- packages/react-learn-card/src/components/VCDisplayCard2/VCDisplayCard2.tsx
+++ packages/react-learn-card/src/components/VCDisplayCard2/VCDisplayCard2.tsx
@@ -203,7 +203,8 @@ export const VCDisplayCard2: React.FC<VCDisplayCard2Props> = ({
                             showFrontFace={() => setIsFront(true)}
                         />
                     )}
-                    <div
+                    <button
+                        type="button"
                         className="vc-toggle-side-button text-white shadow-bottom bg-[#00000099] px-[30px] py-[8px] rounded-[40px] text-[28px] tracking-[0.75px] uppercase leading-[28px] mt-[40px] w-fit"
                         onClick={() => setIsFront(!isFront)}
                     >
@@ -214,7 +215,7 @@ export const VCDisplayCard2: React.FC<VCDisplayCard2Props> = ({
                                 Back
                             </span>
                         )}
-                    </div>
+                    </button>
                 </div>
             </div>
             <footer className="vc-card-footer w-full flex justify-between p-[5px] mt-[5px]">
