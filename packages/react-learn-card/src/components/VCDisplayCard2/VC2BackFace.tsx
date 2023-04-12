b6e6988c3040f58120b3655ca681d7aacfb39021
Taylor Beeston <beeston.taylor@gmail.com>
2023-04-12 13:55:00 -0700

    :bug: Update expired text when VC expires

--- packages/react-learn-card/src/components/VCDisplayCard2/VC2BackFace.tsx
+++ packages/react-learn-card/src/components/VCDisplayCard2/VC2BackFace.tsx
@@ -46,11 +46,16 @@ const VC2BackFace: React.FC<VC2BackFaceProps> = ({
         ? format(new Date(credential.expirationDate), 'MMM dd, yyyy')
         : undefined;
 
+    const isExpired =
+        credential.expirationDate &&
+        Number(new Date(credential.expirationDate)) < Number(new Date());
+
     const achievement =
         'achievement' in credential.credentialSubject
             ? credential.credentialSubject.achievement
             : undefined;
     const criteria = achievement?.criteria?.narrative;
+    const description = achievement?.description;
 
     /* 
     const tags = credential.credentialSubject.achievement?.tag;
@@ -71,17 +76,15 @@ const VC2BackFace: React.FC<VC2BackFaceProps> = ({
                 </div>
             )}
 
-            <TruncateTextBox
-                headerText="About"
-                text={achievement?.description}
-                className="description-box"
-            >
-                {expiration && (
-                    <p className="text-grayscale-800 font-poppins font-[600] text-[12px] leading-[18px] mb-0">
-                        Expires on {expiration}
-                    </p>
-                )}
-            </TruncateTextBox>
+            {(description || expiration) && (
+                <TruncateTextBox headerText="About" text={description} className="description-box">
+                    {expiration && (
+                        <p className="text-grayscale-800 font-poppins font-[600] text-[12px] leading-[18px] mb-0">
+                            Expire{isExpired ? 'd' : 's'} on {expiration}
+                        </p>
+                    )}
+                </TruncateTextBox>
+            )}
             {criteria && (
                 <TruncateTextBox headerText="Criteria" text={criteria} className="criteria-box" />
             )}
