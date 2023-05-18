1b75b538b67d44616ffdcc7838cf8ff7b4819c42
Taylor Beeston <beeston.taylor@gmail.com>
2023-05-18 12:33:53 -0700

    :bug: Fix chromatic

--- packages/plugins/didkit/src/didkit/pkg/didkit_wasm.js
+++ packages/plugins/didkit/src/didkit/pkg/didkit_wasm.js
@@ -1116,7 +1116,7 @@ function initSync(bytes) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('didkit_wasm_bg.wasm', import.meta.url);
+        // input = new URL('didkit_wasm_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
