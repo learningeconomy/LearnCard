b3cc41f07129c86f9aa80c4324f3709b4ddb19c4
Taylor Beeston <beeston.taylor@gmail.com>
2023-05-21 23:12:30 -0700

    :bug: Try changing jest-mongo-config

--- services/learn-card-network/learn-cloud-service/jest-mongodb-config.js
+++ services/learn-card-network/learn-cloud-service/jest-mongodb-config.js
@@ -1,12 +1,10 @@
 module.exports = {
     mongodbMemoryServerOptions: {
-      binary: {
-        version: '5.1.0',
-        skipMD5: true,
-      },
-      instance: {
-        dbName: 'main',
-      },
-      autoStart: false,
+        binary: {
+            version: '5.1.0',
+            skipMD5: true,
+        },
+        instance: {},
+        autoStart: false,
     },
-  };
+};
