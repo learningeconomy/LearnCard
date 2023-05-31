d41b82cdd7660ef4722784490b06e3efd064aaaa
Taylor Beeston <beeston.taylor@gmail.com>
2023-05-31 10:48:59 -0700

    :bug: Remove warning

--- packages/react-learn-card/.storybook/main.ts
+++ packages/react-learn-card/.storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
-    stories: ['../src/**/**/*.stories.mdx', '../src/**/**/*.stories.@(js|jsx|ts|tsx)'],
+    stories: ['../src/**/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-styling'],
     framework: { name: '@storybook/react-vite', options: {} },
 };
