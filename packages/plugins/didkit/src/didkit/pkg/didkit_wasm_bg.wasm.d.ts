0b15dfc3d658268c15caff21c5cea5892c7b2608
Taylor Beeston <beeston.taylor@gmail.com>
2023-07-11 19:23:30 -0700

    :sparkles: Add CLR context

--- packages/plugins/didkit/src/didkit/pkg/didkit_wasm_bg.wasm.d.ts
+++ packages/plugins/didkit/src/didkit/pkg/didkit_wasm_bg.wasm.d.ts
@@ -33,11 +33,11 @@ export function verifyInvocation(a: number, b: number, c: number, d: number): nu
 export function contextLoader(a: number, b: number): number;
 export function didkit_error_message(): number;
 export function didkit_error_code(): number;
-export function __wbindgen_malloc(a: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
 export const __wbindgen_export_2: WebAssembly.Table;
-export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf6251ee6687a20b8(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h7955a8bde413354d(a: number, b: number, c: number): void;
 export function __wbindgen_add_to_stack_pointer(a: number): number;
-export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
 export function __wbindgen_exn_store(a: number): void;
-export function wasm_bindgen__convert__closures__invoke2_mut__h63ffd509f6690aab(a: number, b: number, c: number, d: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h787c66a4f6d2c392(a: number, b: number, c: number, d: number): void;
