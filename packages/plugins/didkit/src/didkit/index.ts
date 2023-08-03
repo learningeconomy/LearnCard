8b22a1ab0ed91d17e38117473089617bba87ae71
Taylor Beeston <beeston.taylor@gmail.com>
2023-08-02 17:44:51 -0700

    :bug: Fix issues when instantiating two LearnCards concurrently

--- packages/plugins/didkit/src/didkit/index.ts
+++ packages/plugins/didkit/src/didkit/index.ts
@@ -3,16 +3,22 @@ import _init, { InitInput } from './pkg/didkit_wasm';
 export * from './pkg/didkit_wasm';
 
 let initialized = false;
+let generating = false;
 
 export const init = async (
     arg: InitInput | Promise<InitInput> = 'https://cdn.filestackcontent.com/NXWgZbAoRVSr3oVsHXpX'
 ) => {
+    while (generating) await new Promise(res => setTimeout(res, 250));
+
     // allow calling multiple times without reinitializing
     if (initialized) return;
 
-    initialized = true;
+    generating = true;
 
-    return _init(arg);
+    await _init(arg);
+
+    generating = false;
+    initialized = true;
 };
 
 export default init;
