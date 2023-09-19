cb2ab5411337c157f8c916901297f01cb5173e24
Taylor Beeston <beeston.taylor@gmail.com>
2023-09-19 13:33:45 -0700

    :sparkles: Create Dynamic Loader plugin

---
+++ packages/plugins/dynamic-loader/src/index.ts
@@ -0,0 +1,29 @@
+import { DynamicLoaderPluginType } from './types';
+
+export * from './types';
+
+/**
+ * @group Plugins
+ */
+export const DynamicLoaderPlugin: DynamicLoaderPluginType = {
+    name: 'Dynamic Loader',
+    displayName: 'Dynamic Loader',
+    description: 'Resolve JSON-LD Contexts dynamically via fetch',
+    context: {
+        resolveStaticDocument: async () => undefined,
+        resolveRemoteDocument: async (_learnCard, uri) => {
+            try {
+                const response = await fetch(uri);
+
+                if (response.status !== 200) throw new Error('Status Code not 200!');
+
+                return await response.json();
+            } catch (error) {
+                _learnCard.debug?.('Error fetching remote context!', error);
+
+                return undefined;
+            }
+        },
+    },
+    methods: {},
+};
