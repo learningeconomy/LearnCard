e0c2e3cb202697b6875c729e35326d75bbae4f65
Taylor Beeston <39720479+TaylorBeeston@users.noreply.github.com>
2023-10-03 19:47:40 -0700

    P-256 Support (#378)
    * :sparkles: Support P256
    
    * :bookmark: Changeset

--- packages/plugins/didkit/src/didkit/index.ts
+++ packages/plugins/didkit/src/didkit/index.ts
@@ -6,7 +6,7 @@ let initialized = false;
 let generating = false; // Mutex flag to allow first init call to acquire a lock
 
 export const init = async (
-    arg: InitInput | Promise<InitInput> = 'https://cdn.filestackcontent.com/FScqkl2ETTSnK2K351TA'
+    arg: InitInput | Promise<InitInput> = 'https://cdn.filestackcontent.com/OaJFEB9fRhO8SnuxHsGl'
 ) => {
     // Do not return until we are done generating!
     while (generating) await new Promise(res => setTimeout(res, 250));
