3b1d2687c035297da8b34ca2ffbd33ad49687faf
Taylor Beeston <39720479+TaylorBeeston@users.noreply.github.com>
2023-12-01 20:22:13 +0000

    [LC-36] Fix Text Resizing Weirdness (#396)
    * :bug: Fix text resizing weirdness
    
    * :bookmark: Changeset

---
+++ .changeset/tiny-lemons-raise.md
@@ -0,0 +1,5 @@
+---
+'@learncard/react': patch
+---
+
+Fix text resizing issues
--- packages/react-learn-card/src/components/VCDisplayCard2/FitText.tsx
+++ packages/react-learn-card/src/components/VCDisplayCard2/FitText.tsx
@@ -1,38 +1,81 @@
-import React, { useLayoutEffect, useRef } from 'react';
-import fitty, { FittyInstance, FittyOptions } from 'fitty';
+import React, { useEffect, useRef } from 'react';
 
-type FitTextProps = {
+interface FitTextProps {
     text: string;
-    width: string; // Must be set for this to work
-    options?: FittyOptions;
+    width: string;
     className?: string;
-    textClassName?: string;
-};
+    minFontSize?: number;
+    maxFontSize?: number;
+}
 
 const FitText: React.FC<FitTextProps> = ({
     text,
     width,
-    options,
     className = '',
-    textClassName = '',
+    minFontSize = 10,
+    maxFontSize = 100,
 }) => {
-    const textRef = useRef<HTMLSpanElement>(null);
+    const textRef = useRef<HTMLDivElement>(null);
+    let animationFrameId: number | null = null;
 
-    useLayoutEffect(() => {
-        let fit: FittyInstance;
-
+    const adjustFontSize = () => {
         if (textRef.current) {
-            fit = fitty(textRef.current, options);
+            const currentFontSize = parseFloat(
+                window.getComputedStyle(textRef.current).getPropertyValue('font-size')
+            );
 
-            setTimeout(fit.fit, 0);
+            // Need to calculate spacing based on nowrap to prevent thrashing
+            textRef.current.style.whiteSpace = 'nowrap';
+            const parentWidth = (textRef.current.parentNode as any)?.clientWidth;
+            const scrollWidth = textRef.current.scrollWidth || textRef.current.offsetWidth;
+
+            // Sometimes scrollWidth can temporarily be 0. If so, just try again next frame
+            if (scrollWidth === 0) {
+                if (animationFrameId !== null) {
+                    cancelAnimationFrame(animationFrameId);
+                }
+
+                animationFrameId = requestAnimationFrame(adjustFontSize);
+
+                return;
+            }
+
+            const newFontSize = Math.min(
+                Math.max((parentWidth / scrollWidth) * currentFontSize, minFontSize),
+                maxFontSize
+            );
+
+            textRef.current.style.fontSize = `${newFontSize}px`;
+            textRef.current.style.whiteSpace = newFontSize === minFontSize ? 'normal' : 'nowrap';
+        }
+    };
+
+    const handleResize = () => {
+        if (animationFrameId !== null) {
+            cancelAnimationFrame(animationFrameId);
         }
 
-        return () => fit?.unsubscribe();
-    }, [textRef.current]);
+        animationFrameId = requestAnimationFrame(adjustFontSize);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        adjustFontSize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            if (animationFrameId !== null) {
+                cancelAnimationFrame(animationFrameId);
+            }
+        };
+    }, [text]);
 
     return (
         <div style={{ width }} className={`text-center ${className}`}>
-            <span ref={textRef} className={textClassName}>
+            <span
+                className={`text-[${minFontSize}px] transition-[font-size] whitespace-nowrap`}
+                ref={textRef}
+            >
                 {text}
             </span>
         </div>
