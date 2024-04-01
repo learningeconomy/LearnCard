eac8d7c3b1e73aad1e15b9e379c82a6c232b76ab
gerardopar <gerardparedes23@gmail.com>
2024-04-01 08:15:53 -0700

    add plural text

--- packages/react-learn-card/src/components/VCDisplayCard2/VCDisplayCardSkillsCount.tsx
+++ packages/react-learn-card/src/components/VCDisplayCard2/VCDisplayCardSkillsCount.tsx
@@ -11,10 +11,12 @@ export const VCDisplayCardSkillsCount: React.FC<{
 
     if (skillsCount === 0) return <></>;
 
+    const text = skillsCount === 1 ? 'Skill' : 'Skills';
+
     return (
         <div className="flex items-center justify-center mt-8">
-            <p className="text-violet-500 text-3xl flex items-center justify-center tracking-[0.75px]">
-                +{skillsCount} Skills{' '}
+            <p className="text-violet-500 text-xl flex items-center justify-center tracking-[0.75px] font-poppins font-semibold">
+                +{skillsCount} {text}{' '}
                 <div className="bg-violet-500 rounded-full flex items-center justify-center ml-2 h-[30px] w-[30px] p-1">
                     <PuzzlePiece className="text-white" fill="#fff" />
                 </div>
