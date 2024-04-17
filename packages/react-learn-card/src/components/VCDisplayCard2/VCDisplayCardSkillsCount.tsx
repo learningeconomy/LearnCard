bba499d3d74821831d0a575332a09c71b0ade3b3
Taylor Beeston <beeston.taylor@gmail.com>
2024-04-17 11:27:04 -0700

    :lipstick: Make skills count easier to see

--- packages/react-learn-card/src/components/VCDisplayCard2/VCDisplayCardSkillsCount.tsx
+++ packages/react-learn-card/src/components/VCDisplayCard2/VCDisplayCardSkillsCount.tsx
@@ -29,7 +29,10 @@ export const VCDisplayCardSkillsCount: React.FC<{
     const text: string = skillsCount === 1 ? 'Skill' : 'Skills';
 
     return (
-        <div className="flex items-center justify-center mt-8 cursor-pointer" onClick={onClick}>
+        <div
+            className="flex items-center justify-center mt-8 cursor-pointer px-4 py-2 bg-white rounded-[20px] shadow-bottom"
+            onClick={onClick}
+        >
             <p className="text-violet-500 text-xl flex items-center justify-center tracking-[0.75px] font-poppins font-semibold">
                 +{total} {text}{' '}
                 <div className="bg-violet-500 rounded-full flex items-center justify-center ml-2 h-[30px] w-[30px] p-1">
