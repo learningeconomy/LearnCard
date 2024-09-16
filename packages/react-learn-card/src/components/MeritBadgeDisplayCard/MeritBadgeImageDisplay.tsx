708331effbd9fd79f017c79248be83f48da6c1d4
Kyle <Smurflo2@gmail.com>
2024-09-16 16:04:47 -0600

    use small z-index numbers

--- packages/react-learn-card/src/components/MeritBadgeDisplayCard/MeritBadgeImageDisplay.tsx
+++ packages/react-learn-card/src/components/MeritBadgeDisplayCard/MeritBadgeImageDisplay.tsx
@@ -14,11 +14,11 @@ const MeritBadgeImageDisplay: React.FC<MeritBadgeImageDisplayProps> = ({
         <div
             className={`flex items-center justify-center w-[120px] h-[120px] rounded-full bg-white z-50 ${className}`}
         >
-            <div className="relative left-[1px] top-[1px] w-full h-full p-[10px] flex items-center justify-center rounded-full overflow-hidden object-contain z-[10000]">
+            <div className="relative left-[1px] top-[1px] w-full h-full p-[10px] flex items-center justify-center rounded-full overflow-hidden object-contain z-[2]">
                 <img src={imageUrl} alt="merit badge thumbnail" />
             </div>
 
-            <MeritBadgeRibbon className={`absolute z-[9999]`} />
+            <MeritBadgeRibbon className="absolute z-[1]" />
         </div>
     );
 };
