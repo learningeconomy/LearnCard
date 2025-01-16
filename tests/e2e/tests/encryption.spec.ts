8133e4caa510914216827c04405c1ee3b1ae27ee
Taylor Beeston <beeston.taylor@gmail.com>
2025-01-16 10:32:23 -0800

    ✅ Add test for basic encryption

---
+++ tests/e2e/tests/encryption.spec.ts
@@ -0,0 +1,23 @@
+import { describe, test, expect } from 'vitest';
+
+import { NetworkLearnCardFromSeed } from '@learncard/init';
+
+import { getLearnCardForUser } from './helpers/learncard.helpers';
+
+let a: NetworkLearnCardFromSeed['returnValue'];
+let b: NetworkLearnCardFromSeed['returnValue'];
+
+describe('Encryption', () => {
+    beforeEach(async () => {
+        a = await getLearnCardForUser('a');
+        b = await getLearnCardForUser('b');
+    });
+
+    test('Users can encrypt and decrypt VCs', async () => {
+        const vc = await a.invoke.issueCredential(a.invoke.getTestVc(b.id.did()));
+        const jwe = await a.invoke.createDagJwe(vc, [a.id.did(), b.id.did()]);
+
+        expect(await b.invoke.decryptDagJwe(jwe, [b.id.keypair()])).toEqual(vc);
+        expect(await a.invoke.decryptDagJwe(jwe, [a.id.keypair()])).toEqual(vc);
+    });
+});
