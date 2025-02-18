60090c28bbd2af3b659e0e6a6f648cb1ddf5efaa
Taylor Beeston <39720479+TaylorBeeston@users.noreply.github.com>
2025-02-18 14:48:26 -0800

    Fix OIDC key changing (#613)
    * 🚑️ Fix XAPI/OIDC key id changing
    
    * 🔖 Changeset

--- services/learn-card-network/learn-cloud-service/src/helpers/auth.helpers.ts
+++ services/learn-card-network/learn-cloud-service/src/helpers/auth.helpers.ts
@@ -3,28 +3,62 @@ import crypto from 'crypto';
 import jwt from 'jsonwebtoken';
 
 import { isTest } from './test.helpers';
-import { generateDeterministicRsaKeyPair } from './crypto.helpers';
+import { generateDeterministicRSAKeyPair, KeyPair } from './crypto.helpers';
 import { XAPI_ENDPOINT } from '../constants/xapi';
+import { getRsaInfo, setRsaInfo } from '@cache/rsa';
 
 const seed = isTest ? 'a'.repeat(64) : process.env.LEARN_CLOUD_SEED;
 
-const { privateKey, publicKey } = generateDeterministicRsaKeyPair(Buffer.from(seed ?? '', 'hex'));
+let keyInfo: { keypair: KeyPair; kid: string };
+
+const getKeyInfo = async () => {
+    if (!keyInfo) {
+        const cachedKeyInfo = await getRsaInfo();
+
+        if (cachedKeyInfo) keyInfo = cachedKeyInfo;
+        else {
+            const keypair = generateDeterministicRSAKeyPair(Buffer.from(seed ?? '', 'hex'));
+
+            keyInfo = {
+                keypair,
+                kid: crypto
+                    .createHash('sha256')
+                    .update(keypair.publicKey)
+                    .digest('hex')
+                    .slice(0, 16),
+            };
+
+            setRsaInfo(keyInfo);
+        }
+    }
+
+    return keyInfo;
+};
 
 const ISSUER = process.env.SERVER_URL || 'http://localhost:4100';
-const KEY_ID = crypto.createHash('sha256').update(publicKey).digest('hex').slice(0, 16);
 
-export const generateToken = (did: string, scope = 'lrs:all'): string => {
+export const generateToken = async (did: string, scope = 'lrs:all'): Promise<string> => {
+    const {
+        keypair: { privateKey },
+        kid,
+    } = await getKeyInfo();
+
     const token = jwt.sign({ iss: ISSUER, sub: did, aud: XAPI_ENDPOINT, scope, did }, privateKey, {
         expiresIn: '1h',
         algorithm: 'RS256',
-        keyid: KEY_ID,
+        keyid: kid,
     });
 
     return token;
 };
 
-export const generateJwk = () => {
+export const generateJwk = async () => {
+    const {
+        keypair: { publicKey },
+        kid,
+    } = await getKeyInfo();
+
     const keyComponents = crypto.createPublicKey(publicKey).export({ format: 'jwk' });
 
-    return { ...keyComponents, use: 'sig', kid: KEY_ID, alg: 'RS256' };
+    return { ...keyComponents, use: 'sig', kid, alg: 'RS256' };
 };
