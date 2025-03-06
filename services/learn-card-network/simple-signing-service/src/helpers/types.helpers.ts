74fadee83567c4cb0700f14d413c2fbddf8840ae
Taylor Beeston <beeston.taylor@gmail.com>
2025-03-06 11:40:51 -0800

    ✨ Create Simple Signing Service to use SAs in end to end tests

---
+++ services/learn-card-network/simple-signing-service/src/helpers/types.helpers.ts
@@ -0,0 +1,15 @@
+import Express from 'express';
+import { UnsignedVC, VC, UnsignedVP, VP, JWE } from '@learncard/types';
+
+export interface TypedRequest<
+    Body extends Record<string, any> = {},
+    Query extends Record<string, any> = {},
+    Params extends Record<string, any> = {}
+> extends Express.Request {
+    body: Body;
+    query: Query;
+    params: Params;
+}
+
+export const isEncrypted = (item: UnsignedVC | VC | UnsignedVP | VP | JWE): item is JWE =>
+    'ciphertext' in item;
