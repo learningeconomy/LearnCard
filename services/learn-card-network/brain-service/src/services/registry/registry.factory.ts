71bae0371d5b94352fae0576d1b5aacfc2c870e5
Jacksón Smith <jackson@Jacksons-2025-Macbook.local>
2025-07-07 16:28:13 -0400

    IS_CI flag to registries service

--- services/learn-card-network/brain-service/src/services/registry/registry.factory.ts
+++ services/learn-card-network/brain-service/src/services/registry/registry.factory.ts
@@ -3,7 +3,7 @@ import { WhitelistAdapter } from './adapters/whitelist.adapter';
 import { DccIssuerRegistryAdapter } from './adapters/dcc-issuer-registry.adapter';
 
 export const getRegistryService = (): RegistryService => {
-    if (process.env.NODE_ENV === 'test' || process.env.TRUSTED_ISSUERS_WHITELIST) {
+    if (process.env.NODE_ENV === 'test' || process.env.TRUSTED_ISSUERS_WHITELIST || process.env.IS_CI) {
         return new WhitelistAdapter();
     }
 
