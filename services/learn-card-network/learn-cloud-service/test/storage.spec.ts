b66774f28e307a09c0dbf0af7dfd85a331321db8
Kai <Smurflo2@gmail.com>
2025-07-21 15:24:01 -0600

    update test to not expect full auth

--- services/learn-card-network/learn-cloud-service/test/storage.spec.ts
+++ services/learn-card-network/learn-cloud-service/test/storage.spec.ts
@@ -100,18 +100,14 @@ describe('Storage', () => {
             await Credentials.deleteMany({});
         });
 
-        it('should require full auth to get a credential', async () => {
+        it('should not require full auth to get a credential', async () => {
             const unsignedVc = userA.learnCard.invoke.getTestVc(userB.learnCard.id.did());
             const vc = await userA.learnCard.invoke.issueCredential(unsignedVc);
 
             const uri = await userA.clients.fullAuth.storage.store({ item: vc });
 
-            await expect(noAuthClient.storage.resolve({ uri })).rejects.toMatchObject({
-                code: 'UNAUTHORIZED',
-            });
-            await expect(userB.clients.partialAuth.storage.resolve({ uri })).rejects.toMatchObject({
-                code: 'UNAUTHORIZED',
-            });
+            await expect(noAuthClient.storage.resolve({ uri })).resolves.not.toThrow();
+            await expect(userB.clients.partialAuth.storage.resolve({ uri })).resolves.not.toThrow();
         });
 
         it('should allow resolving a credential/presentation', async () => {
