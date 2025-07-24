3b954d3308d82d438cc8f8412e2109257f1c98d1
Kai <Smurflo2@gmail.com>
2025-07-24 14:39:15 -0600

    No more DefaultFace for NotificationBoostCard

--- packages/react-learn-card/src/components/NotificationBoostCard/NotificationBoostCard.stories.tsx
+++ packages/react-learn-card/src/components/NotificationBoostCard/NotificationBoostCard.stories.tsx
@@ -32,3 +32,23 @@ NotificationCardTest.args = {
     handleCancelClick: () => console.log('///CANCEL CLICK'),
     isArchived: false,
 };
+
+export const NotificationCardTestNoIssuerImage = Template.bind({});
+
+NotificationCardTestNoIssuerImage.args = {
+    title: 'Title of Credential',
+    thumbImage:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/a/a8/Jack_Daniels_Logo.svg/1200px-Jack_Daniels_Logo.svg.png',
+    issuerInfo: {
+        image: '',
+        fullName: 'Bobby Baggins',
+        displayName: 'BBaggins',
+    },
+    className: 'notification-boost-card-test',
+    issueDate: 'Apr 04 2022',
+    notificationType: NotificationTypeEnum.SocialBadges,
+    handleButtonClick: () => console.log('BUTTON CLICK'),
+    handleCardClick: () => console.log('CARD CLICK'),
+    handleCancelClick: () => console.log('///CANCEL CLICK'),
+    isArchived: false,
+};
