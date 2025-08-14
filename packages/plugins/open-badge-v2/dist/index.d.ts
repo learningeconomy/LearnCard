f7cc8b431b39289b94798dee58ef33ec7da4af1c
Jacksón Smith <jackson@mac.myfiosgateway.com>
2025-08-14 15:59:47 -0400

    Remove dist

--- packages/plugins/open-badge-v2/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { openBadgeV2Plugin } from './plugin';
-export * from './types';
-//# sourceMappingURL=index.d.ts.map
--- packages/plugins/open-badge-v2/dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./open-badge-v2-plugin.cjs.production.min.js');
-} else {
-    module.exports = require('./open-badge-v2-plugin.cjs.development.js');
-}
