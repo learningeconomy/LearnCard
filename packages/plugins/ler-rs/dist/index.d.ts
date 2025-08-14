17c6778b7904bfc75d2c11c52fcf7fe6b32a07f9
Jacksón Smith <jackson@mac.myfiosgateway.com>
2025-08-14 13:51:19 -0400

    Remove dist

--- packages/plugins/ler-rs/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { getLerRsPlugin } from './ler-rs';
-export * from './types';
-//# sourceMappingURL=index.d.ts.map
--- packages/plugins/ler-rs/dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./ler-rs-plugin.cjs.production.min.js');
-} else {
-    module.exports = require('./ler-rs-plugin.cjs.development.js');
-}
