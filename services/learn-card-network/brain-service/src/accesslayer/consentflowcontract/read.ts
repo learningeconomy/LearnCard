a7581dff9a21e40f53a50794fb76e1b34ec10937
Gerardo Paredes <gerardparedes23@gmail.com>
2025-12-02 11:06:46 -0800

    add getConsentFlowContractById, getRequestedForList, getRequestedForByStatus

--- services/learn-card-network/brain-service/src/accesslayer/consentflowcontract/read.ts
+++ services/learn-card-network/brain-service/src/accesslayer/consentflowcontract/read.ts
@@ -1,7 +1,7 @@
 import { QueryBuilder } from 'neogma';
 
 import { getIdFromUri } from '@helpers/uri.helpers';
-import { ConsentFlowContract } from '@models';
+import { ConsentFlowContract, ConsentFlowInstance } from '@models';
 import { DbContractType } from 'types/consentflowcontract';
 import { inflateObject } from '@helpers/objects.helpers';
 
@@ -19,4 +19,67 @@ export const getContractById = async (id: string): Promise<DbContractType | null
 
 export const getContractByUri = async (uri: string): Promise<DbContractType | null> => {
     return getContractById(getIdFromUri(uri));
+};
+
+export const getConsentFlowContractById = async (
+    id: string
+): Promise<ConsentFlowInstance | null> => {
+    const result = await new QueryBuilder()
+        .match({ model: ConsentFlowContract, where: { id }, identifier: 'contract' })
+        .return('contract')
+        .limit(1)
+        .run();
+
+    const record = result.records[0];
+    if (!record) return null;
+
+    return record.toObject().contract;
+};
+
+export const getRequestedForList = async (id: string) => {
+    const result = await new QueryBuilder()
+        .match({
+            model: ConsentFlowContract,
+            identifier: 'c',
+            where: { id },
+        })
+        .match('(c)-[r:REQUESTED_FOR]->(p:Profile)')
+        .return(['p', 'r'])
+        .run();
+
+    return result.records.map(rec => {
+        const { p, r } = rec.toObject();
+
+        return {
+            profile: p.properties,
+            status: r.properties?.status ?? null,
+            readStatus: r.properties?.readStatus ?? null,
+        };
+    });
+};
+
+export const getRequestedForByStatus = async (
+    id: string,
+    status: 'pending' | 'accepted' | 'denied'
+) => {
+    const result = await new QueryBuilder()
+        .match({
+            model: ConsentFlowContract,
+            identifier: 'c',
+            where: { id },
+        })
+        .match('(c)-[r:REQUESTED_FOR]->(p:Profile)')
+        .where(`r.status = '${status}'`)
+        .return(['p', 'r'])
+        .run();
+
+    return result.records.map(rec => {
+        const { p, r } = rec.toObject();
+
+        return {
+            profile: p.properties,
+            status: r.properties?.status ?? null,
+            readStatus: r.properties?.readStatus ?? null,
+        };
+    });
 };
