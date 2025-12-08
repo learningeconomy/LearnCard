98d168081cd2d73d13b96d5d505d85d660b52232
Jacksón Smith <jackson@mac.myfiosgateway.com>
2025-12-07 23:18:49 -0500

    Delete app-store-portal example

--- examples/app-store-portal/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-    integrations: [react(), tailwind()],
-});
