68718a177cf5d09ae701fe4fc1c2a831640a56a5
Gerardo Paredes <gerardparedes23@gmail.com>
2026-01-05 15:37:06 -0800

    add AiPathwayCourseDetails:

--- apps/learn-card-app/src/pages/ai-pathways/ai-pathway-courses/AiPathwayCourseItem.tsx
+++ apps/learn-card-app/src/pages/ai-pathways/ai-pathway-courses/AiPathwayCourseItem.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 
 import startRatingIcon from '../../../assets/images/star-rating.icon.png';
 import timeMachineIcon from '../../../assets/images/time-machine.icon.png';
+import AiPathwayCourseDetails from './AiPathwayCourseDetails';
 import edxLogo from '../../../assets/images/edx-logo.png';
 
+import { useModal } from 'learn-card-base';
+
+import { ModalTypes } from 'learn-card-base';
 import { AiPathwayCourse } from './ai-pathway-courses.helpers';
 
 const AiPathwayCourseItem: React.FC<{ course: AiPathwayCourse }> = ({ course }) => {
+    const { newModal } = useModal();
+
+    const openCourseDetailsModal = () => {
+        newModal(<AiPathwayCourseDetails course={course} />, undefined, {
+            desktop: ModalTypes.Right,
+            mobile: ModalTypes.Right,
+        });
+    };
+
     return (
         <div
             key={course.id}
+            onClick={openCourseDetailsModal}
             className="w-full flex flex-col items-start justify-start px-4 py-2 gap-1 border-solid border-[1px] border-grayscale-200 rounded-xl"
         >
             <p className="text-indigo-500 font-normal text-sm line-clamp-1 font-notoSans uppercase">
@@ -44,7 +58,7 @@ const AiPathwayCourseItem: React.FC<{ course: AiPathwayCourse }> = ({ course }) 
                             className="w-5 h-5 object-contain"
                         />
                     </div>
-                    <div className="flex flex-col items-end justify-end">
+                    <div className="flex flex-col items-start justify-end">
                         <p className="text-sm text-grayscale-800 font-semibold text-right">
                             {course.durationAvg}
                         </p>
