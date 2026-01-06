3e4a897f1180ca744e5735d2be0420164e737fba
Gerardo Paredes <gerardparedes23@gmail.com>
2026-01-06 11:10:14 -0800

    add ai-pathway career header

--- apps/learn-card-app/src/pages/ai-pathways/ai-pathway-careers/AiPathwaysCareerDetails.tsx
+++ apps/learn-card-app/src/pages/ai-pathways/ai-pathway-careers/AiPathwaysCareerDetails.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import { IonFooter } from '@ionic/react';
+import AiPathwayCareerHeader from './AiPathwayCareerHeader';
 import AiPathwayCareerSalaries from './AiPathwayCareerSalaries';
-import CareerLaptopIcon from '../../../assets/images/career.laptop.icon.png';
 
 import { useModal } from 'learn-card-base';
 
@@ -18,27 +18,7 @@ const AiPathwayCareerDetails: React.FC<{ career: AiPathwayCareer }> = ({ career 
         >
             <div className="h-full relative overflow-hidden bg-grayscale-200">
                 <div className="h-full overflow-y-auto pb-[150px] px-[20px] flex flex-col gap-[20px] items-center justify-start">
-                    <div className="bg-white rounded-[24px] flex flex-col overflow-y-auto shadow-box-bottom max-w-[600px] mx-auto min-w-[300px] shrink-0 mt-[60px] w-full">
-                        {/* header */}
-                        <div className="flex flex-col gap-[10px] items-center p-[20px] border-b-[1px] border-grayscale-200 border-solid">
-                            <img
-                                src={CareerLaptopIcon}
-                                alt="career icon"
-                                className="w-[80px] h-[80px]"
-                            />
-
-                            <h2 className="text-[20px] text-grayscale-900 font-poppins">
-                                {career.title}
-                            </h2>
-                        </div>
-
-                        {/* description */}
-                        <div className="flex flex-col gap-[10px] items-start justify-start p-[20px]">
-                            <p className="text-grayscale-600 font-poppins text-base tracking-[-0.25px]">
-                                {career.description}
-                            </p>
-                        </div>
-                    </div>
+                    <AiPathwayCareerHeader career={career} />
 
                     <AiPathwayCareerSalaries career={career} />
                 </div>
