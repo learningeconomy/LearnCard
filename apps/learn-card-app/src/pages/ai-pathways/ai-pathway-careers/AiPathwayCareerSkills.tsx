99cc70ec7946010413377520ffdc3d99432a3347
Gerardo Paredes <gerardparedes23@gmail.com>
2026-01-09 15:47:44 -0800

    wire up AiPathwayCareerSkills

--- apps/learn-card-app/src/pages/ai-pathways/ai-pathway-careers/AiPathwayCareerSkills.tsx
+++ apps/learn-card-app/src/pages/ai-pathways/ai-pathway-careers/AiPathwayCareerSkills.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 
-import { type AiPathwayCareer } from './ai-pathway-careers.helpers';
+import { OccupationDetailsResponse } from 'learn-card-base/types/careerOneStop';
 
-export const AiPathwayCareerSkills: React.FC<{ career: AiPathwayCareer }> = ({ career }) => {
+export const AiPathwayCareerSkills: React.FC<{ occupation: OccupationDetailsResponse }> = ({
+    occupation,
+}) => {
+    const skills = occupation?.SkillsDataList?.map(skill => ({
+        id: skill?.ElementId,
+        name: skill?.ElementName,
+        description: skill?.ElementDescription,
+        importance: skill?.Importance,
+    }))
+        .sort((a, b) => {
+            const aImp = Number(a?.importance ?? 0);
+            const bImp = Number(b?.importance ?? 0);
+            return bImp - aImp;
+        })
+        .slice(0, 5);
+
     return (
         <div className="bg-white rounded-[24px] p-[20px] flex flex-col overflow-y-auto shadow-box-bottom max-w-[600px] mx-auto min-w-[300px] shrink-0 w-full gap-2">
             <div className="w-full flex items-center justify-start">
@@ -10,7 +25,7 @@ export const AiPathwayCareerSkills: React.FC<{ career: AiPathwayCareer }> = ({ c
             </div>
 
             <div className="w-full flex flex-col items-center justify-start gap-4">
-                {career.skills.map(skill => {
+                {skills?.map(skill => {
                     return (
                         <div
                             key={skill.id}
