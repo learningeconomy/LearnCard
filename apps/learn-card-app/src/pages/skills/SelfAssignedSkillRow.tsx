fe9fbb23f1fe4691f276b7cdb3106efb3883e4a0
Kai <Smurflo2@gmail.com>
2026-01-09 11:51:41 -0700

    More progress bar progress

--- apps/learn-card-app/src/pages/skills/SelfAssignedSkillRow.tsx
+++ apps/learn-card-app/src/pages/skills/SelfAssignedSkillRow.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 
 import InfoIcon from 'learn-card-base/svgs/InfoIcon';
+import SlimCaretRight from '../../components/svgs/SlimCaretRight';
 import CompetencyIcon from '../SkillFrameworks/CompetencyIcon';
 import CircleCheckmark from 'learn-card-base/svgs/CircleCheckmark';
+import SkillProgressBar from './SkillProgressBar';
 import ViewAlignmentInfo from '../SkillFrameworks/ViewAlignmentInfo';
 
 import { ModalTypes, useModal } from 'learn-card-base';
 
 import { SkillFramework, SkillFrameworkNode } from '../../components/boost/boost';
-import SkillProgressBar from './SkillProgressBar';
-import CaretDown from '../../components/svgs/CaretDown';
 
 type SelfAssignedSkillRowProps = {
     skill: SkillFrameworkNode;
@@ -28,11 +28,11 @@ const SelfAssignedSkillRow: React.FC<SelfAssignedSkillRowProps> = ({
     const [isExpanded, setIsExpanded] = useState(true);
 
     return (
-        <div className="flex flex-col rounded-[15px] shadow-box-bottom ">
+        <div className="flex flex-col rounded-[15px] shadow-box-bottom">
             <div
                 role="button"
                 onClick={handleToggleSelect}
-                className="p-[10px] pl-[15px] flex items-center gap-[10px] text-grayscale-900 bg-white w-full"
+                className="p-[10px] pl-[15px] flex items-center gap-[10px] text-grayscale-900 bg-white w-full rounded-[15px]"
             >
                 <>
                     {isNodeSelected ? (
@@ -78,11 +78,13 @@ const SelfAssignedSkillRow: React.FC<SelfAssignedSkillRowProps> = ({
                     )}
                     <button
                         onClick={() => setIsExpanded(!isExpanded)}
-                        className="flex items-center text-grayscale-700 font-poppins text-[12px] font-[600]"
+                        className="flex items-center gap-[5px] text-grayscale-700 font-poppins text-[12px] font-[600]"
                     >
                         {isExpanded ? 'Hide options' : 'Show options'}
-                        <CaretDown
-                            className={`w-[15px] h-[15px] ${isExpanded ? 'rotate-180' : ''}`}
+                        <SlimCaretRight
+                            className={`w-[15px] h-[15px] ${
+                                isExpanded ? 'rotate-[270deg]' : 'rotate-[90deg]'
+                            }`}
                         />
                     </button>
                 </div>
--- apps/learn-card-app/src/pages/skills/SkillProgressBar.tsx
+++ apps/learn-card-app/src/pages/skills/SkillProgressBar.tsx
@@ -13,26 +13,32 @@ const SKILL_LEVEL_META = {
     [SkillLevel.Hidden]: {
         color: 'grayscale-500',
         description: 'Do not display your proficiency status.',
+        value: 0,
     },
     [SkillLevel.Novice]: {
-        color: 'grayscale-600',
+        color: 'grayscale-700',
         description: 'Just starting and needs guidance.',
+        value: 1,
     },
     [SkillLevel.Beginner]: {
         color: 'orange-400',
         description: 'Handles simple tasks without support.',
+        value: 2,
     },
     [SkillLevel.Proficient]: {
         color: 'violet-500',
         description: 'Works independently on routine tasks.',
+        value: 3,
     },
     [SkillLevel.Advanced]: {
         color: 'light-blue-500',
         description: 'Solves complex tasks efficiently.',
+        value: 4,
     },
     [SkillLevel.Expert]: {
         color: 'emerald-500',
         description: 'Deep mastery; can lead and mentor others.',
+        value: 5,
     },
 };
 
@@ -52,6 +58,23 @@ const SkillProgressBar: React.FC<SkillProgressBarProps> = ({}) => {
                 <p className="text-grayscale-700 font-poppins text-[12px]">
                     {SKILL_LEVEL_META[skillLevel].description}
                 </p>
+            </div>
+
+            <div className="flex gap-[1px] w-full rounded-[20px] overflow-hidden">
+                {Object.keys(SkillLevel).map((level: string, index: number) => (
+                    <button
+                        key={level}
+                        onClick={() => setSkillLevel(level as SkillLevel)}
+                        className={`flex-1 h-[7px] ${
+                            index <= SKILL_LEVEL_META[skillLevel].value
+                                ? `bg-${color}`
+                                : 'bg-grayscale-200'
+                        }`}
+                    />
+                ))}
+
+                {/* To make sure tailwind puts these colors in the CSS */}
+                <span className="hidden bg-orange-400 bg-light-blue-500 text-light-blue-500" />
             </div>
         </div>
     );
