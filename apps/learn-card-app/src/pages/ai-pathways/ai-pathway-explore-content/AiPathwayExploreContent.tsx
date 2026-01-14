4b49275174e14b13a00c55cad0917d1c003a14eb
Gerardo Paredes <gerardparedes23@gmail.com>
2026-01-14 14:35:53 -0800

    add content skeleton loader

--- apps/learn-card-app/src/pages/ai-pathways/ai-pathway-explore-content/AiPathwayExploreContent.tsx
+++ apps/learn-card-app/src/pages/ai-pathways/ai-pathway-explore-content/AiPathwayExploreContent.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import AiPathwayContentList from './AiPathwayContentList';
 
 import { useOccupationDetailsForKeyword } from 'learn-card-base/react-query/queries/careerOneStop';
+import AiPathwayContentListItemSkeleton from './AiPathwayContentItemSkeletonLoader';
 
 const AiPathwayExploreContent: React.FC<{ careerKeywords?: string[] }> = ({
     careerKeywords = [],
@@ -10,6 +11,28 @@ const AiPathwayExploreContent: React.FC<{ careerKeywords?: string[] }> = ({
     const { data: occupations, isLoading } = useOccupationDetailsForKeyword(
         careerKeywords?.[0] || ''
     );
+
+    if (isLoading) {
+        return (
+            <div className="w-full max-w-[600px] flex items-center justify-center flex-wrap text-center px-4">
+                <div className="w-full bg-white items-center justify-center flex flex-col shadow-bottom-2-4 p-[15px] mt-4 rounded-[15px]">
+                    <div className="w-full flex items-center justify-start">
+                        <h2 className="text-xl text-grayscale-800 font-notoSans">
+                            Explore Content
+                        </h2>
+                    </div>
+
+                    <div className="w-full flex flex-col items-start justify-start mt-4 gap-4">
+                        <div className="w-full flex flex-col items-start justify-start gap-4">
+                            {Array.from({ length: 3 }).map((_, index) => (
+                                <AiPathwayContentListItemSkeleton key={index} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div className="w-full max-w-[600px] flex items-center justify-center flex-wrap text-center px-4">
