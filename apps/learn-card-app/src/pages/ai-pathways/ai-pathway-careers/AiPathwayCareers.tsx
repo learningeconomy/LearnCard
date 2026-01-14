65a65cd8e80f6b327dbf7f69edd80909da6ba2c8
Gerardo Paredes <gerardparedes23@gmail.com>
2026-01-14 14:31:01 -0800

    add careers skeleton loader

--- apps/learn-card-app/src/pages/ai-pathways/ai-pathway-careers/AiPathwayCareers.tsx
+++ apps/learn-card-app/src/pages/ai-pathways/ai-pathway-careers/AiPathwayCareers.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import AiPathwayCareerItem from './AiPathwayCareerItem';
+import AiPathwayCareerItemSkeletonLoader from './AiPathwayCareerItemSkeletonLoader';
 
 import { useOccupationDetailsForKeyword } from 'learn-card-base/react-query/queries/careerOneStop';
 
@@ -9,14 +10,34 @@ const AiPathwayCareers: React.FC<{ careerKeywords?: string[] }> = ({ careerKeywo
         careerKeywords?.[0] || ''
     );
 
+    const titleEl = (
+        <div className="w-full flex items-center justify-start">
+            <h2 className="text-xl text-grayscale-800 font-notoSans">Explore Careers</h2>
+        </div>
+    );
+
+    if (isLoading) {
+        return (
+            <div className="w-full max-w-[600px] flex items-center justify-center flex-wrap text-center px-4">
+                <div className="w-full bg-white items-center justify-center flex flex-col shadow-bottom-2-4 p-[15px] rounded-[15px]">
+                    {titleEl}
+
+                    <div className="w-full flex flex-col items-start justify-start mt-4 gap-4">
+                        {Array.from({ length: 3 }).map((_, index) => (
+                            <AiPathwayCareerItemSkeletonLoader key={index} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     if (!isLoading && (!careerKeywords?.length || !occupations?.length)) return null;
 
     return (
         <div className="w-full max-w-[600px] flex items-center justify-center flex-wrap text-center px-4">
             <div className="w-full bg-white items-center justify-center flex flex-col shadow-bottom-2-4 p-[15px] rounded-[15px]">
-                <div className="w-full flex items-center justify-start">
-                    <h2 className="text-xl text-grayscale-800 font-notoSans">Explore Careers</h2>
-                </div>
+                {titleEl}
 
                 <div className="w-full flex flex-col items-start justify-start mt-4 gap-4">
                     {occupations?.map(occupation => (
