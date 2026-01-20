bc25b1101966c9980020619d0c3d6329ed071418
Gerardo Paredes <gerardparedes23@gmail.com>
2026-01-20 09:55:35 -0800

    bump LCA defaultChannel to 1.0.3

--- apps/learn-card-app/capacitor.config.ts
+++ apps/learn-card-app/capacitor.config.ts
@@ -36,7 +36,7 @@ const config: CapacitorConfig = {
         CapacitorUpdater: {
             appId: 'com.learncard.app',
             autoUpdate: true,
-            defaultChannel: '1.0.2',
+            defaultChannel: '1.0.3', // bumped here -> https://github.com/learningeconomy/LearnCard/pull/938
         },
     },
 };
