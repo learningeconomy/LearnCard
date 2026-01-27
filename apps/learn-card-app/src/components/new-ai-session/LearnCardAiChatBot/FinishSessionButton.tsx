4aa78d2ec820e7a1848affe14144387a0e25cfce
Gerardo Paredes <gerardparedes23@gmail.com>
2026-01-27 13:18:06 -0800

    add min height

--- apps/learn-card-app/src/components/new-ai-session/LearnCardAiChatBot/FinishSessionButton.tsx
+++ apps/learn-card-app/src/components/new-ai-session/LearnCardAiChatBot/FinishSessionButton.tsx
@@ -45,7 +45,7 @@ const FinishSessionButton: React.FC = () => {
 
     if (mode === AiSessionMode.insights) {
         return (
-            <div className="flex justify-between items-center gap-[15px] p-[15px] absolute top-[0px] left-[0px] w-full bg-white shadow-box-bottom z-[100000] safe-area-inset-top">
+            <div className="min-h-[75px] flex justify-between items-center gap-[15px] p-[15px] absolute top-[0px] left-[0px] w-full bg-white shadow-box-bottom z-[100000] safe-area-inset-top">
                 <div className="flex items-center justify-center gap-1">
                     <AiInsightsIconMonochrome className="w-[24px] h-[24px] mb-1" />
                     <p className="text-sm flex items-center justify-center font-[600] leading-[24px] tracking-[0.25px] text-grayscale-600">
