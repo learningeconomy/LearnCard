de46d9f234ed970a5be0468dc04b94fd3313544a
Gerardo Paredes <gerardparedes23@gmail.com>
2026-01-30 10:07:47 -0800

    add min-w/min-h to prompt list item icons

--- apps/learn-card-app/src/pages/ai-insights/ai-inisghts-prompt/ai-insights-prompt-list/AiInsightsPromptListItem.tsx
+++ apps/learn-card-app/src/pages/ai-insights/ai-inisghts-prompt/ai-insights-prompt-list/AiInsightsPromptListItem.tsx
@@ -59,7 +59,7 @@ export const AiInsightsPromptListItem: React.FC<{ prompt: string }> = ({ prompt 
             className="w-full flex items-center justify-start bg-indigo-600 px-3 rounded-[10px] h-[62px]"
         >
             <div className="w-full flex items-center justify-start gap-2">
-                <AiInsightsIconOutline className="text-white w-[25px] h-[25px]" />
+                <AiInsightsIconOutline className="text-white w-[25px] h-[25px] min-w-[25px] min-h-[25px]" />
                 <p className="text-left text-sm text-white line-clamp-2 font-semibold">{prompt}</p>
             </div>
             <RightArrow className="text-white w-[20px] h-[20px]" />
