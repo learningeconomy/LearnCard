64a6cf2d65a000ecaa1ad4f0296680cf159a0873
Gerardo Paredes <gerardparedes23@gmail.com>
2026-01-30 12:13:20 -0800

    add enableChatBubbles ld flag

--- apps/learn-card-app/src/components/new-ai-session/LearnCardAiChatBot/MessageWithQuestions.tsx
+++ apps/learn-card-app/src/components/new-ai-session/LearnCardAiChatBot/MessageWithQuestions.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useStore } from '@nanostores/react';
+import { useFlags } from 'launchdarkly-react-client-sdk';
 
 import { ProfilePicture } from 'learn-card-base';
 import MessageWithArtifact from './MessageWithArtifact';
@@ -19,12 +20,17 @@ interface MessageProps {
 
 export const MessageWithQuestions: React.FC<MessageProps> = React.memo(
     function MessageWithQuestions({ message }) {
+        const flags = useFlags();
         const $isTyping = useStore(isTyping);
+
+        const enableChatBubbles = flags.enableChatBubbles;
 
         const mode = chatBotStore.useTracked.mode();
 
-        let assistantBubbleStyles = 'bg-grayscale-100 mr-auto !max-w-full px-4';
-        const userBubbleStyles = 'bg-cyan-50 px-4';
+        let assistantBubbleStyles = enableChatBubbles
+            ? 'bg-grayscale-100 mr-auto !max-w-full px-4'
+            : 'bg-white mr-auto !max-w-full';
+        const userBubbleStyles = enableChatBubbles ? 'bg-cyan-50 px-4' : 'bg-grayscale-100 px-4';
 
         if (!message.content && !message?.artifact) return null;
 
@@ -37,7 +43,7 @@ export const MessageWithQuestions: React.FC<MessageProps> = React.memo(
 
         return (
             <div className="w-full flex items-center justify-end">
-                {message.role === 'assistant' && !$isTyping && (
+                {message.role === 'assistant' && !$isTyping && enableChatBubbles && (
                     <div className="mr-2 self-stretch flex items-end pb-5">
                         {mode === AiSessionMode.insights ? (
                             <AiInsightsIconWithShape className="text-grayscale-900 h-[35px] w-[35px] min-h-[35px] min-w-[35px] max-h-[35px] max-w-[35px] mt-[0px] mb-0" />
@@ -53,7 +59,7 @@ export const MessageWithQuestions: React.FC<MessageProps> = React.memo(
                 >
                     {text}
                 </div>
-                {message.role !== 'assistant' && !$isTyping && (
+                {message.role !== 'assistant' && !$isTyping && enableChatBubbles && (
                     <div className="ml-2 self-stretch flex items-end pb-5">
                         <ProfilePicture
                             customContainerClass="text-grayscale-900 h-[35px] w-[35px] min-h-[35px] min-w-[35px] max-h-[35px] max-w-[35px] mt-[0px] mb-0"
