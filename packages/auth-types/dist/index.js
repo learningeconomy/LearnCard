310755d319e7d856c77104f0fa2a40c198430fdb
Jacksón Smith <jackson@mac.myfiosgateway.com>
2026-02-13 17:46:37 -0500

    REmove auth types dist/modules

--- packages/auth-types/dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./auth-types.cjs.production.min.js');
-} else {
-    module.exports = require('./auth-types.cjs.development.js');
-}
