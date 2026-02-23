16e891c1da6128d54c9b8abc051ae11141a0b644
Jacksón Smith <custard7@gmail.com>
2026-02-23 16:12:32 -0500

    fix: eth build plugin (#1022)
    * Fix eth build
    
    * Create thick-seals-happen.md
    
    ---------
    
    Co-authored-by: Jacksón Smith <jackson@mac.myfiosgateway.com>

--- packages/plugins/ethereum/scripts/mixedEntypoint.js
+++ packages/plugins/ethereum/scripts/mixedEntypoint.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./ethereum-plugin.cjs.production.min.js');
+    module.exports = require('./ethereum-plugin.cjs.production.min.cjs');
 } else {
-    module.exports = require('./ethereum-plugin.cjs.development.js');
+    module.exports = require('./ethereum-plugin.cjs.development.cjs');
 }
