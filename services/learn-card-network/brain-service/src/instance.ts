6722fa435767fc633a24d8b4feb904ab27e26bb7
Jacksón Smith <jackson@mac.myfiosgateway.com>
2026-03-03 23:05:51 -0500

    Remove neogma config

--- services/learn-card-network/brain-service/src/instance.ts
+++ services/learn-card-network/brain-service/src/instance.ts
@@ -11,25 +11,7 @@ if (!uri) throw new Error('Whoops! No URI found');
 if (!username) throw new Error('Whoops! No Username found');
 if (!password) throw new Error('Whoops! No Password found');
 
-const encryptedInScheme = uri.startsWith('neo4j+s') || uri.startsWith('bolt+s');
-
-export const neogma = new Neogma(
-    { url: uri, username, password },
-    {
-        // When encryption is specified in the URI scheme (+s / +ssc), the driver
-        // forbids also setting encrypted/trust in config.
-        ...(encryptedInScheme
-            ? {}
-            : {
-                  encrypted: 'ENCRYPTION_OFF' as const,
-                  trust: 'TRUST_ALL_CERTIFICATES' as const,
-              }),
-        maxConnectionLifetime: 8 * 60 * 1000,
-        connectionAcquisitionTimeout: 30_000,
-        connectionTimeout: 30_000,
-        maxConnectionPoolSize: 10,
-    }
-);
+export const neogma = new Neogma({ url: uri, username, password });
 
 QueryBuilder.queryRunner = neogma.queryRunner;
 
