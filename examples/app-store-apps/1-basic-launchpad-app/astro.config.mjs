fb3031cf7b6f92dc750b2a486dd76d7562b81989
Taylor Beeston <beeston.taylor@gmail.com>
2026-03-17 17:19:25 -0700

    🐛 Don't use the tailwind CDN lol

--- examples/app-store-apps/1-basic-launchpad-app/astro.config.mjs
+++ examples/app-store-apps/1-basic-launchpad-app/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify()
+    output: 'server',
+    adapter: netlify(),
+    integrations: [tailwind()],
 });
