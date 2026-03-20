04ff38dbec7bf9248505d56313515f553470f4e3
Gerardo Paredes <gerardparedes23@gmail.com>
2026-03-20 11:15:09 -0700

    prevent duplicate ai topics selector from showing up on desktop

--- apps/learn-card-app/src/components/new-ai-session/NewAiSessionButton/NewAiSessionSideMenuButton.tsx
+++ apps/learn-card-app/src/components/new-ai-session/NewAiSessionButton/NewAiSessionSideMenuButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 import NewAiSessionIcon from 'learn-card-base/svgs/NewAiSessionIcon';
 
@@ -6,12 +7,28 @@ import { IonMenuToggle } from '@ionic/react';
 
 import useTheme from '../../../theme/hooks/useTheme';
 import { ColorSetEnum } from '../../../theme/colors';
+import { useDeviceTypeByWidth } from 'learn-card-base';
+import { chatBotStore } from 'src/stores/chatBotStore';
 
 const NewAiSessionSideMenuButton: React.FC<{ handleNewSession?: () => void }> = ({
     handleNewSession,
 }) => {
+    const location = useLocation();
     const { getColorSet } = useTheme();
+    const { isDesktop } = useDeviceTypeByWidth();
+
     const colors = getColorSet(ColorSetEnum.sideMenu);
+
+    const isOnTopicsRoute = location.pathname === '/ai/topics';
+
+    const onHandleNewSession = () => {
+        if (isOnTopicsRoute && isDesktop) {
+            chatBotStore.set.resetStore();
+            return;
+        }
+
+        handleNewSession?.();
+    };
 
     return (
         <IonMenuToggle
@@ -19,7 +36,7 @@ const NewAiSessionSideMenuButton: React.FC<{ handleNewSession?: () => void }> = 
             autoHide={false}
             className={`text-[17px] flex items-center justify-center font-semibold py-[5px] rounded-full w-full max-w-[90%]  h-[45px] max-h-[45px] shadow-soft-bottom ${colors.primaryButtonColor}`}
             onClick={() => {
-                handleNewSession?.();
+                onHandleNewSession();
             }}
         >
             New AI Session
