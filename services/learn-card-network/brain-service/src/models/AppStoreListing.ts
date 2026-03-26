19569b3b078c3de2b5882b5c68a238a0321b8e9f
Rachel Hendricks <rachelhen92@gmail.com>
2026-03-26 16:46:15 -0400

    Remove submitted_at on node schema

--- services/learn-card-network/brain-service/src/models/AppStoreListing.ts
+++ services/learn-card-network/brain-service/src/models/AppStoreListing.ts
@@ -99,7 +99,6 @@ export const AppStoreListing = ModelFactory<FlatAppStoreListingType, AppStoreLis
             hero_background_color: { type: 'string', required: false },
             min_age: { type: 'number', required: false },
             age_rating: { type: 'string', enum: AgeRating.options, required: false },
-            submitted_at: { type: 'string', required: false },
             contact_email: { type: 'string', required: false },
         } as any,
         relationships: {
