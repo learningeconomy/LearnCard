b62ebf5ecab0b1a00cdcf572579e794341fe68b3
Rachel Hendricks <rachelhen92@gmail.com>
2026-04-03 13:38:23 -0400

    Add new notification icon to navbaricons

--- packages/learn-card-base/src/svgs/navbar/navbarIcons.tsx
+++ packages/learn-card-base/src/svgs/navbar/navbarIcons.tsx
@@ -1,9 +1,11 @@
 import NavBarPassportIcon from './NavBarPassportIcon';
 import NavBarCircleIcon from './NavBarCircleIcon';
 import NavBarLaunchPadIcon from './NavBarLaunchPadIcon';
+import NavBarBellIcon from './NavBarBellIcon';
 
 export const NavBarIcons = {
     wallet: NavBarPassportIcon,
     circle: NavBarCircleIcon,
     launchPad: NavBarLaunchPadIcon,
+    notification: NavBarBellIcon,
 };
