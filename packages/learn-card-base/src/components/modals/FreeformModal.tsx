38738cfe17d87ab647e72c3878db5c2ff2aabe04
Rachel Hendricks <rachelhen92@gmail.com>
2026-04-22 11:56:58 -0400

    Allow freeform modal to close when clicking outside of the modal

--- packages/learn-card-base/src/components/modals/FreeformModal.tsx
+++ packages/learn-card-base/src/components/modals/FreeformModal.tsx
@@ -2,14 +2,32 @@ import React from 'react';
 
 import { ModalContainer } from './types/Modals';
 import GenericErrorBoundary from '../generic/GenericErrorBoundary';
+import { useModal } from './useModal';
 
 export const FreeformModal: ModalContainer = ({ component, options, open }) => {
+    const { closeModal } = useModal();
+
+    const handleBackdropClick = () => {
+        if (options?.disableCloseHandlers) return;
+        options?.onClose?.();
+        closeModal();
+    };
+
     return (
         <aside
             className={`freeform-modal ${options?.className ?? ''} ${open ? 'open' : 'closed'} ${
                 options?.hideDimmer ? 'hide-dimmer' : ''
             }`}
         >
+            {!options?.hideDimmer && (
+                <button
+                    className="freeform-modal-dimmer"
+                    type="button"
+                    onClick={handleBackdropClick}
+                    aria-label="modal-dimmer"
+                    aria-hidden
+                />
+            )}
             <div
                 className={`freeform-section ${options?.widen ? 'widen' : ''} ${
                     options?.addShadow ? 'add-shadow' : ''
