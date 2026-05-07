d952b83aca24475082ccaedff61bc8e9ac0a1e91
Gerardo Paredes <gerardparedes23@gmail.com>
2026-05-07 10:58:34 -0700

    define initial render method

---
+++ packages/plugins/render-method/src/plugin.ts
@@ -0,0 +1,61 @@
+import { UnsignedVC, TemplateRenderMethod } from '@learncard/types';
+
+import {
+    AttachRenderMethodConfig,
+    RenderMethodDependentLearnCard,
+    RenderMethodPlugin,
+    RENDER_METHOD_CONTEXT,
+} from './types';
+
+// ! for now we only support svg-mustache
+// TODO: Add other render suites and media types
+
+const buildTemplateRenderMethod = (config: AttachRenderMethodConfig): TemplateRenderMethod => ({
+    type: 'TemplateRenderMethod',
+    renderSuite: 'svg-mustache', // TODO: Add other render suites
+    template: {
+        id: config.templateId,
+        mediaType: 'image/svg+xml', // TODO: Add other media types
+        ...(config.digestMultibase ? { digestMultibase: config.digestMultibase } : {}),
+        ...(config.renderProperty ? { renderProperty: config.renderProperty } : {}),
+    },
+});
+
+const attachRenderMethod = (vc: UnsignedVC, config: AttachRenderMethodConfig): UnsignedVC => {
+    const renderMethod = buildTemplateRenderMethod(config);
+
+    const context = Array.isArray(vc['@context']) ? vc['@context'] : [vc['@context']];
+    const updatedContext = context.includes(RENDER_METHOD_CONTEXT)
+        ? context
+        : [...context, RENDER_METHOD_CONTEXT];
+
+    const existingRenderMethod = vc.renderMethod;
+    let updatedRenderMethod: TemplateRenderMethod | TemplateRenderMethod[];
+
+    if (!existingRenderMethod) {
+        updatedRenderMethod = renderMethod;
+    } else if (Array.isArray(existingRenderMethod)) {
+        updatedRenderMethod = [...(existingRenderMethod as TemplateRenderMethod[]), renderMethod];
+    } else {
+        updatedRenderMethod = [existingRenderMethod as TemplateRenderMethod, renderMethod];
+    }
+
+    return {
+        ...vc,
+        '@context': updatedContext,
+        renderMethod: updatedRenderMethod,
+    };
+};
+
+export const getRenderMethodPlugin = (
+    _learnCard: RenderMethodDependentLearnCard
+): RenderMethodPlugin => ({
+    name: 'Render Method',
+    displayName: 'Render Method Plugin',
+    description:
+        'Attaches W3C renderMethod to Verifiable Credentials for standards-based rendering',
+    methods: {
+        attachRenderMethod: (_lc, vc, config) => attachRenderMethod(vc, config),
+        buildTemplateRenderMethod: (_lc, config) => buildTemplateRenderMethod(config),
+    },
+});
