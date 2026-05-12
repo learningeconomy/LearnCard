89b12014c929ded019a6fa36f7a9e28790a878ec
Kai <Smurflo2@gmail.com>
2026-05-12 16:09:25 -0600

    Revert "fix storybook for react-learn-card"
    This reverts commit 7aa9cd258aa57a55f195d17f0392fac1ff391866.

--- packages/react-learn-card/.storybook/main.ts
+++ packages/react-learn-card/.storybook/main.ts
@@ -3,13 +3,5 @@ const config: StorybookConfig = {
     stories: ['../src/**/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-styling'],
     framework: { name: '@storybook/react-vite', options: {} },
-    core: {
-        builder: {
-            name: '@storybook/builder-vite',
-            options: {
-                viteConfigPath: './storybook-vite.config.ts',
-            },
-        },
-    },
 };
 export default config;
--- packages/react-learn-card/vite.config.ts
+++ packages/react-learn-card/vite.config.ts
@@ -1,4 +1,8 @@
-export default {
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+    plugins: [tsconfigPaths({ root: '.' }) as any],
     test: {
         environment: 'happy-dom',
         globals: true,
@@ -11,4 +15,4 @@ export default {
             '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
         ],
     },
-};
+});
