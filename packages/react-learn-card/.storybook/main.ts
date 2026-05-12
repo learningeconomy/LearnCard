1f7019d6c74778348c81350abafad9e462031d9c
Kai <Smurflo2@gmail.com>
2026-05-12 16:39:38 -0600

    finish fixing storybook

--- packages/react-learn-card/.storybook/main.ts
+++ packages/react-learn-card/.storybook/main.ts
@@ -3,6 +3,9 @@ const config: StorybookConfig = {
     stories: ['../src/**/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-styling'],
     framework: { name: '@storybook/react-vite', options: {} },
+    typescript: {
+        reactDocgen: false,
+    },
     core: {
         builder: {
             name: '@storybook/builder-vite',
