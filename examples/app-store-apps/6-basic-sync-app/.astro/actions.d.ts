c1e696fcbd2659f7d5bd8fa3219532f716a92834
Gerardo Paredes <gerardparedes23@gmail.com>
2026-06-11 17:03:49 -0700

    udpdate gitignore for astro app

--- examples/app-store-apps/6-basic-sync-app/.astro/actions.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'astro:actions' {
-    type Actions =
-        typeof import('/Users/gerardoparedes/Documents/work/LearnCard/examples/app-store-apps/6-basic-sync-app/src/actions/index.ts')['server'];
-
-    export const actions: Actions;
-}
