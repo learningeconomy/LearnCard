965eb069b3b734dced89d040c1505a6e3cfc1d09
Taylor Beeston <beeston.taylor@gmail.com>
2026-07-01 22:49:30 -0700

    💚 Fix libsodium import

--- services/learn-card-network/simple-signing-service/esbuildPlugins.cjs
+++ services/learn-card-network/simple-signing-service/esbuildPlugins.cjs
@@ -1,6 +1,16 @@
 const { sentryEsbuildPlugin } = require('@sentry/esbuild-plugin');
 
+const forceCjsSodiumWrapperPlugin = {
+    name: 'force-cjs-sodium-wrapper',
+    setup(build) {
+        build.onResolve({ filter: /^libsodium-wrappers$/ }, () => ({
+            path: require.resolve('libsodium-wrappers'),
+        }));
+    },
+};
+
 module.exports = [
+    forceCjsSodiumWrapperPlugin,
     sentryEsbuildPlugin({
         authToken: process.env.SENTRY_AUTH_TOKEN,
         org: process.env.SENTRY_ORG,
