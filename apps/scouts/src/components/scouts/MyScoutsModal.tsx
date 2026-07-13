b5a546fee574d5b39ca53dfe040ddfd762569d32
Kai <Smurflo2@gmail.com>
2026-07-13 16:42:56 -0600

    add complete profile button to  MyLearnCard modal

--- apps/scouts/src/components/scouts/MyScoutsModal.tsx
+++ apps/scouts/src/components/scouts/MyScoutsModal.tsx
@@ -38,6 +38,7 @@ import {
 import useLogout from '../../hooks/useLogout';
 import { useAppAuth } from '../../providers/AuthCoordinatorProvider';
 import { getSigningLearnCard } from 'learn-card-base/helpers/walletHelpers';
+import useJoinLCNetworkModal from '../network-prompts/hooks/useJoinLCNetworkModal';
 
 import {
     DEFAULT_COLOR_LIGHT,
@@ -89,6 +90,7 @@ const MyScoutsModal: React.FC<MyScoutsModalProps> = ({
 
     const { newModal, closeModal } = useModal();
     const { handleLogout, isLoggingOut } = useLogout();
+    const { handlePresentJoinNetworkModal } = useJoinLCNetworkModal();
 
     const { data: isNetworkUser } = useIsCurrentUserLCNUser();
     const notInNetwork = isNetworkUser === false;
@@ -571,6 +573,16 @@ const MyScoutsModal: React.FC<MyScoutsModalProps> = ({
                         <span className="py-[10px] text-grayscale-900 font-notoSans text-[17px] text-center line-clamp-4">
                             {description}
                         </span>
+                        {notInNetwork && (
+                            <button
+                                onClick={() => {
+                                    void handlePresentJoinNetworkModal();
+                                }}
+                                className="bg-grayscale-800 text-white font-notoSans text-[17px] font-semibold px-[20px] py-[7px] rounded-[10px] mb-[10px]"
+                            >
+                                Complete Profile
+                            </button>
+                        )}
 
                         <div>
                             {rows.map((r, index) => {
