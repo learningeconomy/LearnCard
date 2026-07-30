0eefb20a36f629359381c7cb977e77a9c9c65de5
Taylor Beeston <beeston.taylor@gmail.com>
2026-07-30 14:25:49 -0700

    [LC-1958] Rebuild and publish DIDKit WASM

--- packages/plugins/didkit/src/didkit/index.ts
+++ packages/plugins/didkit/src/didkit/index.ts
@@ -6,7 +6,9 @@ let initialized = false;
 let generating = false; // Mutex flag to allow first init call to acquire a lock
 
 export const init = async (
-    arg: InitInput | Promise<InitInput> = 'https://cdn.filestackcontent.com/6v059q3KQlmXd6XoVPyC'
+    arg:
+        | InitInput
+        | Promise<InitInput> = 'https://assets.learncard.ai/didkit_wasm_bg-3fca0a344286b5ed954e8df19c05dc2dd989893a8b6542b722dc2fed7e5dc52a.wasm'
 ) => {
     // Do not return until we are done generating!
     while (generating) await new Promise(res => setTimeout(res, 250));
