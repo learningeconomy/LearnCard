fcf1246b61eae282c6577a5b2fbc84c23817d990
Taylor Beeston <beeston.taylor@gmail.com>
2026-08-04 11:38:47 -0700

    Point DIDKit at issuer authorization WASM

--- packages/plugins/didkit/src/didkit/index.ts
+++ packages/plugins/didkit/src/didkit/index.ts
@@ -8,7 +8,7 @@ let generating = false; // Mutex flag to allow first init call to acquire a lock
 export const init = async (
     arg:
         | InitInput
-        | Promise<InitInput> = 'https://assets.learncard.ai/didkit_wasm_bg-7fec99f61754f80762b56b9a4a6bd1ed2a36d26266b9f7c44f735d26b4dee3c7.wasm'
+        | Promise<InitInput> = 'https://assets.learncard.ai/didkit_wasm_bg-da82d7c815e8a3bd14ed9d8b866918a67cc3f03d34daca22ef41e4b9febe4be7.wasm'
 ) => {
     // Do not return until we are done generating!
     while (generating) await new Promise(res => setTimeout(res, 250));
