6a494ce51aa8432d18336c70d8db1fa0a2d63cc3
Gerardo Paredes <gerardparedes23@gmail.com>
2026-09-09 14:13:03 -0700

    fix(launchpad): correct header component imports

--- apps/learn-card-app/src/pages/launchPad/LaunchPadHeader/LaunchPadHeaderUserGreeting.tsx
+++ apps/learn-card-app/src/pages/launchPad/LaunchPadHeader/LaunchPadHeaderUserGreeting.tsx
@@ -5,10 +5,10 @@ import { getFirstName, useCurrentUser, useModal, ModalTypes, useGetProfile } fro
 import { useTheme } from '../../../theme/hooks/useTheme';
 import { getGreetingAndEmoji } from './launchPadHeader.helpers';
 import LaunchPadActionModal from './LaunchPadActionModal';
-import useLCNGatedAction from '../../components/network-prompts/hooks/useLCNGatedAction';
-import CaretRightFilled from '../../components/svgs/CaretRightFilled';
-import { roleIcons } from '../../components/onboarding/onboardingRoles/OnboardingRoleItem';
-import { LearnCardRolesEnum } from '../../components/onboarding/onboarding.helpers';
+import useLCNGatedAction from '../../../components/network-prompts/hooks/useLCNGatedAction';
+import CaretRightFilled from '../../../components/svgs/CaretRightFilled';
+import { roleIcons } from '../../../components/onboarding/onboardingRoles/OnboardingRoleItem';
+import { LearnCardRolesEnum } from '../../../components/onboarding/onboarding.helpers';
 
 export const LaunchPadHeaderUserGreeting: React.FC = () => {
     const currentUser = useCurrentUser();
